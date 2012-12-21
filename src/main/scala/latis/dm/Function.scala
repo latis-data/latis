17f8a35ee4e6163d9fe24a5834a8828b9f1f98cd
Doug Lindholm <doug.lindholm@gmail.com>
2012-12-21 11:37:45 -0700

    Expose the domain and range of a Function with unapply for pattern matching.

--- src/main/scala/latis/dm/Function.scala
+++ src/main/scala/latis/dm/Function.scala
@@ -1,5 +1,17 @@
 package latis.dm
 
-class Function(domain: Variable, range: Variable) extends Variable {
+/**
+ * A Variable that represents a mapping from an independent (domain) Variable
+ * to a dependent (range) Variable.
+ */
+class Function(val domain: Variable, val range: Variable) extends Variable {
 
+}
+
+object Function {
+  
+  /**
+   * Expose a Functions domain and range Variables as a pair.
+   */
+  def unapply(f: Function) = (f.domain, f.range)
 }
