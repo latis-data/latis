b2b5e532463ffe2d2ea810889e69dca00e18a741
Doug Lindholm <doug.lindholm@gmail.com>
2012-12-21 11:30:22 -0700

    Add base class for making Datasets and providing access to their data.

---
+++ src/main/scala/latis/reader/DatasetAccessor.scala
@@ -0,0 +1,22 @@
+package latis.reader
+
+import latis.dm.Dataset
+
+/**
+ * Base for classes that provide data access for a Dataset.
+ * A DatasetAccessor is responsible for constructing a Dataset
+ * and returning data for the Variables it contains.
+ */
+abstract class DatasetAccessor {
+
+  /**
+   * Return the Dataset that this accessor is responsible for.
+   * This may involve considerable construction.
+   */
+  def getDataset(): Dataset
+  
+  /**
+   * Release any resources that this accessor acquired.
+   */
+  def close()
+}
