eded0580a25511990cc17f24a800785e588c6909
Doug Lindholm <doug.lindholm@gmail.com>
2012-12-21 11:27:26 -0700

    Add unapply to expose the datum represented by this Scalar. Just return a random number for now.

--- src/main/scala/latis/dm/Scalar.scala
+++ src/main/scala/latis/dm/Scalar.scala
@@ -1,5 +1,18 @@
 package latis.dm
 
+/**
+ * A Variable that represents a single datum.
+ */
 abstract class Scalar extends Variable {
 
+}
+
+object Scalar {
+  
+  /**
+   * Expose the datum value that this Scalar represents.
+   * For now, return a random int from 0 until 100.
+   * TODO: delegate to parent Dataset's Accessor to get data values
+   */
+  def unapply(scalar: Scalar): Option[_] = Some(scala.util.Random.nextInt(100))
 }
