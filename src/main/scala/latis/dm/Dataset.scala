f6d9fed3b0a958e69c20942489b781b51f162e1e
Doug Lindholm <doug.lindholm@gmail.com>
2012-12-21 11:28:43 -0700

    Add a Dataset, the ultimate parent of Variables, also responsible for data access.

---
+++ src/main/scala/latis/dm/Dataset.scala
@@ -0,0 +1,14 @@
+package latis.dm
+
+import latis.reader.DatasetAccessor
+
+import scala.collection.immutable._
+
+/**
+ * A Dataset is the ultimate parent of a collection of Variables.
+ * Unlike other Variables, a Dataset is responsible for data access.
+ * A Variable must get at its data via its parent Dataset.
+ */
+class Dataset(accessor: DatasetAccessor, vars: Seq[Variable]) extends Tuple(vars) {
+
+}
