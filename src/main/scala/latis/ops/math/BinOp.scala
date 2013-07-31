be1b9daf3a079f386c2b705b729acbd965d5d579
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2013-07-31 15:53:19 -0600

    case classes without args deprecated

--- src/main/scala/latis/ops/math/BinOp.scala
+++ src/main/scala/latis/ops/math/BinOp.scala
@@ -4,18 +4,18 @@ abstract class BinOp extends Function2[Double, Double, Double]
 
 //TODO: see Numeric.Ops
 
-case class Add      extends BinOp {def apply(a: Double, b: Double): Double = a + b}
-case class Subtract extends BinOp {def apply(a: Double, b: Double): Double = a - b}
-case class Multiply extends BinOp {def apply(a: Double, b: Double): Double = a * b}
-case class Divide   extends BinOp {def apply(a: Double, b: Double): Double = a / b}
-case class Modulo   extends BinOp {def apply(a: Double, b: Double): Double = a % b}
-case class Power    extends BinOp {def apply(a: Double, b: Double): Double = Math.pow(a, b)}
+class Add      extends BinOp {def apply(a: Double, b: Double): Double = a + b}
+class Subtract extends BinOp {def apply(a: Double, b: Double): Double = a - b}
+class Multiply extends BinOp {def apply(a: Double, b: Double): Double = a * b}
+class Divide   extends BinOp {def apply(a: Double, b: Double): Double = a / b}
+class Modulo   extends BinOp {def apply(a: Double, b: Double): Double = a % b}
+class Power    extends BinOp {def apply(a: Double, b: Double): Double = Math.pow(a, b)}
 
 object BinOp {
-  val ADD      = Add()
-  val SUBTRACT = Subtract()
-  val MULTIPLY = Multiply()
-  val DIVIDE   = Divide()
-  val MODULO   = Modulo()
-  val POWER    = Power()
+  val ADD      = new Add()
+  val SUBTRACT = new Subtract()
+  val MULTIPLY = new Multiply()
+  val DIVIDE   = new Divide()
+  val MODULO   = new Modulo()
+  val POWER    = new Power()
 }
