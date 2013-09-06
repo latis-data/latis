bfe313ba2a959d933a193f05978f88753702784f
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2013-09-06 14:57:59 -0600

    send 500 for unhandled errors

--- src/main/scala/latis/server/LatisServer.scala
+++ src/main/scala/latis/server/LatisServer.scala
@@ -1,6 +1,6 @@
 package latis.server
 
-import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import javax.servlet.http.{ HttpServlet, HttpServletRequest, HttpServletResponse }
 import latis.util.LatisProperties
 import latis.reader.tsml.TsmlReader
 import latis.metadata.Registry
@@ -11,50 +11,55 @@ import com.typesafe.scalalogging.slf4j.Logging
 class LatisServer extends HttpServlet with Logging {
 
   override def init() {
+    logger.info("Initializing LatisServer.")
     LatisProperties.init(getServletConfig)
   }
-  
-  override def doGet(request : HttpServletRequest, response : HttpServletResponse) {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+    var reader: TsmlReader = null //hack so it's visible to finally
+
     try {
-    
-    //Get the request not including the constraints
-    val path = request.getPathInfo()
-                    
-    //Get the type of the output request from the dataset suffix.
-    val index = path.lastIndexOf(".");
-    val suffix = path.substring(index+1); //suffix
-    val dsname = path.substring(1,index); //dataset name, drop leading "/"
-    
-    
-    //Get Dataset
-    val url = Registry.getTsmlUrl(dsname)
-    val reader = TsmlReader(url)
-    
-    
-    
-    /*
-     * TODO: apply constraints
-     * Map with type so we can be api agnostic?
-     *   DAP or SQL...
-     *   projection, selection, operation?
-     * delegate to DapHandler... to build a Seq of Ops?
-     * add keyword to URL: latis/dap/dataset.json...?
-     */
-    val constraints = DapConstraintParser.parseQuery(request.getQueryString)
-    val dataset = reader.getDataset(constraints)
+      logger.debug("Processing request: " + request.getRequestURL())
 
+      //Get the request not including the constraints
+      val path = request.getPathInfo()
 
-    val writer = HttpServletWriter(response, suffix)
-    
-    writer.write(dataset)
-    
-    //Let Reader know that it can release resource (e.g. open files, database connections).
-    reader.close()
-    
+      //Get the type of the output request from the dataset suffix.
+      val index = path.lastIndexOf(".");
+      val suffix = path.substring(index + 1); //suffix
+      val dsname = path.substring(1, index); //dataset name, drop leading "/"
+
+      logger.debug("Locating dataset: " + dsname)
+      
+      //Get Dataset
+      val url = Registry.getTsmlUrl(dsname)
+      
+      logger.debug("Reading dataset from TSML: " + url)
+      reader = TsmlReader(url)
+
+      val constraints = DapConstraintParser.parseQuery(request.getQueryString)
+      val dataset = reader.getDataset(constraints)
+
+      logger.debug("Writing " + suffix + " dataset.")
+      val writer = HttpServletWriter(response, suffix)
+
+      writer.write(dataset)
+      logger.debug("Request complete.")
+
     } catch {
-      case e: Exception => println("Exception in LatisServer: " + e.getMessage())
+      
+      case e: Exception => {
+        logger.error("Exception in LatisServer", e)
+        //Return error status 500, if all else fails
+        response.reset
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "LaTiS was not able to fulfill this request.")
+      }
+      
+    } finally {
+      //Let Reader know that it can release resource (e.g. open files, database connections).
+      try{ reader.close } catch { case e: Exception => }
     }
-    //TODO: finally, close resources: reader, dataset?
+
   }
 
 }
