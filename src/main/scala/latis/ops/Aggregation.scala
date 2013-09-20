02716764e6a61a9f4706dd105e35637b9b0576fa
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2013-09-20 17:00:36 -0600

    start aggregation operations

---
+++ src/main/scala/latis/ops/Aggregation.scala
@@ -0,0 +1,17 @@
+package latis.ops
+
+import latis.dm.Dataset
+
+trait Aggregation {
+
+  def apply(datasets: Seq[Dataset]): Dataset
+}
+
+object Aggregation {
+  
+  def apply(aggType: String): Aggregation = aggType match {
+    case "basic" => ???
+    case "union" => ???
+    case "join" => ???
+  }
+}
