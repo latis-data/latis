218bd9e3f4b52cf2b0c9dddbb719882cad211e2c
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2013-10-16 16:56:32 -0600

    notes

--- src/main/scala/latis/reader/tsml/ColumnarAdapter.scala
+++ src/main/scala/latis/reader/tsml/ColumnarAdapter.scala
@@ -8,11 +8,33 @@ import scala.collection._
  */
 class ColumnarAdapter(tsml: Tsml) extends AsciiAdapter(tsml) {
   //TODO: any reason this couldn't be used with iterative adapter?
+  
+  /*
+   * TODO: 2013-10-16 with Columns trait?
+   * override parseRecord, delegate to super then reorder?
+   * *reorder variableNames
+   * what about time vars in mult cols?
+   *   time as tuple? time.year, time.month...
+   *   <time> 
+   *     <real name="year"/>
+   *     <real name="month"/>
+   *   
+   * 
+   * Keep in mind principle of describing source dataset
+   * PIs to reorder, drop,...
+   *   "projection" PI?
+   * complicates reordering data if dome after?
+   * not as much for granule = each scalar has all data
+   * some cases (e.g. time in multiple cols) better applied as parsed
+   * 
+   * 
+   */
 
   //Get column numbers
-  //TODO: 0 or 1 based? java is 0 but jdbc is 1
+  //TODO: 0 or 1 based? java is 0 but jdbc is 1, probably 0
   //TODO: from single adapter attribute, e.g. "1,2,3;5;7;9"
-  //TODO: from tsml attributes, e.g. column="1,2,3"
+  //TODO: from tsml attributes, e.g. columns="1,2,3"
+  //TODO: use for Vector?
   
   val indexMap: Map[String, String] = getProperty("columns") match {
     case Some(s) => (variableNames zip s.split(";")).toMap
