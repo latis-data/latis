6ff2f56caf8c74d8bdbbcd0f0d8928611d4ad111
Doug Lindholm <doug.lindholm@gmail.com>
2013-12-11 23:46:10 -0700

    add binary writer

---
+++ src/main/scala/latis/writer/BinaryWriter.scala
@@ -0,0 +1,40 @@
+package latis.writer
+
+import java.io.OutputStream
+import java.io.DataOutputStream
+import latis.dm._
+
+class BinaryWriter extends Writer {
+
+  private[this] lazy val writer = new DataOutputStream(outputStream)
+  
+  def write(dataset: Dataset) {
+    dataset.getVariables.map(writeVariable(_))
+    writer.flush()
+  }
+  
+  def writeVariable(variable: Variable) {
+    val data = variable.getData
+    if (data.isEmpty) variable match {
+      case Tuple(vars) => vars.map(writeVariable(_))
+      case f: Function => f.iterator.map(writeVariable(_))
+      case _ => throw new Error("No Data found.")
+    } else {
+      val bs = data.getByteBuffer.array
+      writer.write(bs)
+    }
+  }
+  
+  override def mimeType: String = "application/octet-stream"
+}
+
+object BinaryWriter {
+  
+  def apply(out: OutputStream): BinaryWriter = {
+    val writer = new BinaryWriter()
+    writer._out = out
+    writer
+  }
+  
+  def apply(): BinaryWriter = BinaryWriter(System.out)
+}
