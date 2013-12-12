f622e168fec295d8e681464d3e14f6684fbcff6a
Doug Lindholm <doug.lindholm@gmail.com>
2013-12-11 23:40:06 -0700

    add Binary Variable to support arbitrary blobs

---
+++ src/main/scala/latis/dm/Binary.scala
@@ -0,0 +1,31 @@
+package latis.dm
+
+import latis.data.Data
+import latis.data.value.DoubleValue
+
+import java.nio.Buffer
+
+/**
+ * Binary blob.
+ */
+trait Binary extends Scalar {
+  
+  def length: Int = {
+    getMetadata.get("length") match {
+      case Some(l) => l.toInt
+      case None => throw new Error("Must declare length of Binary Variable.")
+    }
+  }
+  
+}
+
+object Binary {
+  
+  def apply(buffer: Buffer) = new Variable2(data = Data(buffer)) with Binary
+  
+//  def apply(v: Any) = v match {
+//    case d: Double => new Variable2(data = Data(Data(d).getByteBuffer)) with Binary
+//  }
+  
+  def unapply(b: Binary): Option[Buffer] = Some(b.getData.getByteBuffer)
+}
