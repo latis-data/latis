8fc82a5261f6d452aface0d37068e070321d7046
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2013-12-30 16:37:37 -0700

    fix metadata writer to write labels and metadata only for scalars

--- src/main/scala/latis/writer/MetadataWriter.scala
+++ src/main/scala/latis/writer/MetadataWriter.scala
@@ -9,11 +9,6 @@ import latis.util.FirstThenOther
 class MetadataWriter extends JsonWriter {
   //TODO: expose the metadata as a Dataset so we can count on parent's logic  
   
-  //The Dataset may have one variable (the function) but we only care about the 
-  //  set of scalars here, for now. //TODO: review for more general case.
-//  override def makeHeader(dataset: Dataset) = "{\"" + dataset.getName + "\": {\n"
-//  override def makeFooter(dataset: Dataset) = "}}"
-  
   //override to write only domain and range info, not all samples
   override def writeFunction(function: Function) {
     printWriter.print(varToString(Sample(function.getDomain, function.getRange)))
@@ -22,13 +17,14 @@ class MetadataWriter extends JsonWriter {
   //assume only scalars have metadata, for now
   override def varToString(variable: Variable): String = {
     val sb = new StringBuilder()
-    sb append makeLabel(variable)
-    sb append makeMetadata(variable)
     
     variable match {
+      case s: Scalar => {
+        sb append makeLabel(variable)
+        sb append makeMetadata(variable)
+      }
       case Tuple(vars) => sb append vars.map(varToString(_)).mkString(",\n")
       case Function(d,r) => sb append varToString(Sample(d,r))
-      case _ => //nothing to add for scalars
     }
     
     sb.toString
