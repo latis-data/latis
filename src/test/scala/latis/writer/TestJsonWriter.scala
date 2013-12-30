2fd330bfa569fd1fbb207f88cbcf3ea47bc2d762
Doug Lindholm <doug.lindholm@gmail.com>
2013-12-30 09:32:04 -0700

    start writing more systematic json tests

---
+++ src/test/scala/latis/writer/TestJsonWriter.scala
@@ -0,0 +1,20 @@
+package latis.writer
+
+import latis.dm.implicits._
+import org.junit._
+import Assert._
+import com.typesafe.scalalogging.slf4j.Logging
+import latis.dm._
+import latis.metadata.Metadata
+import latis.time.Time
+import latis.dm.TestDataset
+
+class TestJsonWriter {
+  
+  def real = Writer("json").write(TestDataset.real)
+  def integer = Writer("json").write(TestDataset.integer)
+  def text = Writer("json").write(TestDataset.text)
+  
+  @Test
+  def test = Writer("json").write(TestDataset.scalars)
+}
