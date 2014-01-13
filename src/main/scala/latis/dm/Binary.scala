3249a9c64fac5cc78e92d2efb240b162774601b3
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-01-13 16:59:55 -0700

    add length property to Binary variable

--- src/main/scala/latis/dm/Binary.scala
+++ src/main/scala/latis/dm/Binary.scala
@@ -23,11 +23,22 @@ object Binary {
   
 //TODO: set length if not already set (see Text)
   
-  def apply(buffer: Buffer) = new AbstractScalar(data = Data(buffer)) with Binary
+  def apply(buffer: Buffer) = {
+    val size = buffer.limit
+    val md = Metadata(Map("length" -> size.toString))
+    new AbstractScalar(md, Data(buffer)) with Binary
+  }
   
   def apply(md: Metadata): Binary = new AbstractScalar(md) with Binary
   
-  def apply(md: Metadata, buffer: Buffer): Binary = new AbstractScalar(md, Data(buffer)) with Binary
+  def apply(md: Metadata, buffer: Buffer): Binary = {
+    //add length if not already defined
+    val md2 = md.get("length") match {
+      case Some(_) => md
+      case None => Metadata(md.getProperties ++ Map("length" -> buffer.limit.toString))
+    }
+    new AbstractScalar(md2, Data(buffer)) with Binary
+  }
   
 //  def apply(v: Any) = v match {
 //    case d: Double => new Variable2(data = Data(Data(d).getByteBuffer)) with Binary
