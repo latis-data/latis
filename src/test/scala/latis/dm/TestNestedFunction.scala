d8cc2e2499fe2b6d73e2f4e17d26a3229a950df6
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-01-23 15:31:52 -0700

    refactor tests

--- src/test/scala/latis/dm/TestNestedFunction.scala
+++ src/test/scala/latis/dm/TestNestedFunction.scala
@@ -5,23 +5,38 @@ import Assert._
 import scala.collection.mutable.ArrayBuffer
 import latis.writer.AsciiWriter
 import latis.writer.Writer
+import latis.metadata.Metadata
 
 class TestNestedFunction {
 
-  //@Test
+  @Test
   def function_of_functions {
-    val n = 4
-    val domain  = (0 until n).map(Integer(_))
-    val range = for (i <- 0 until n) yield Function((0 until 3).map(j => Sample(Integer(10 + j), Real(10 * i + j))))
-    val ds = Dataset(Function(domain, range))
+    val ds = Dataset(TestNestedFunction.function_of_functions)
     AsciiWriter.write(ds)
     //Writer("json").write(ds)
   }
   
-  @Test
+  //@Test
   def tuple_of_functions {
-    val fs = for (i <- 0 until 4) yield Function((0 until 3).map(j => Sample(Integer(10 + j), Real(10 * i + j))))
-    val ds = Dataset(Tuple(fs))
+    val ds = Dataset(TestNestedFunction.tuple_of_functions)
     AsciiWriter.write(ds)
   }
+}
+
+object TestNestedFunction {
+    
+  def function_of_functions = {
+    val n = 4
+    val domain  = (0 until n).map(Integer(_))
+    val range = for (i <- 0 until n) yield Function((0 until 3).map(j => Sample(Integer(10 + j), Real(10 * i + j))))
+    Function(domain, range)
+  }
+    
+  def tuple_of_functions = {
+    val fs = for (i <- 0 until 4) yield Function((0 until 3).map(j =>
+      Sample(Integer(Metadata("myInt"+i), 10 + j), Real(Metadata("myReal"+i), 10 * i + j))))
+    Tuple(fs)
+  }
+  
+  
 }
