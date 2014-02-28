3227ea13030769b7ac62fa7f08ca3a694fec8ae4
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-02-28 13:07:03 -0700

    test RegexAdapter with AdapterTests

--- src/test/scala/latis/reader/TestRegexAdapter.scala
+++ src/test/scala/latis/reader/TestRegexAdapter.scala
@@ -6,14 +6,10 @@ import latis.reader.tsml.TsmlReader
 import latis.writer.Writer
 import latis.ops._
 
-class TestRegexAdapter {
-  
-  @Test
-  def log {
-    val ops = List(Operation("last"))
-    //val ops = List(Selection("time>2014-02-12T18"))
-    val ds = TsmlReader("datasets/test/log.tsml").getDataset(ops)
-    Writer("txt").write(ds)
-  }
+class TestRegexAdapter extends AdapterTests {
 
+  def datasetName = "regex"
+  
+  //@Test
+  def test = writeDataset
 }
