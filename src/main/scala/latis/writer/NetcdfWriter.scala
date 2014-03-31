34523d7ca46d68e53a34f61519f91e20d0e6bac0
Doug Lindholm <doug.lindholm@gmail.com>
2014-03-31 09:03:02 -0600

    start a NetcdfWriter based on nujan

--- src/main/scala/latis/writer/NetcdfWriter.scala
+++ src/main/scala/latis/writer/NetcdfWriter.scala
@@ -3,13 +3,22 @@ package latis.writer
 import java.io.OutputStream
 import latis.dm.Dataset
 import java.io.File
+import edu.ucar.ral.nujan.netcdf.NhFileWriter
 
 class NetcdfWriter extends FileWriter {
 
   override def writeFile(dataset: Dataset, file: File) = {
-    val file = getFile
+    val ncfile = new NhFileWriter(file.getName, NhFileWriter.OPT_OVERWRITE)
     
-    //TODO
+    val rootGroup = ncfile.getRootGroup
+    
+    //assume dataset has one Function, for now
+    val f = dataset.findFunction.get
+    val domain = f.getDomain
+    val range = f.getRange
+    
+    
+    
   }
   
 }
