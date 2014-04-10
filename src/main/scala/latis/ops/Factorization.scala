a5ab16acfdededb13b57c19cb7b4aaff8daca245
Doug Lindholm <doug.lindholm@gmail.com>
2014-04-10 16:24:35 -0600

    snapshot

--- src/main/scala/latis/ops/Factorization.scala
+++ src/main/scala/latis/ops/Factorization.scala
@@ -12,7 +12,7 @@ class Factorization extends Operation {
     Dataset(dataset.getVariables.map(op(_)))
   }
   
-  private def op(v: Variable): Variable = v match {
+  private def op(variable: Variable): Variable = variable match {
     case f: Function => {
       /*
        * TODO: require Index Function, for now
@@ -24,13 +24,43 @@ class Factorization extends Operation {
        * .reduce
        *   t -> a (if length of every nested function is 1)
        * 
+       * 
        */
+      if (! f.getDomain.isInstanceOf[Index]) ???
       
+      f.getRange match {
+        case t: Tuple => {
+          
+        }
+      }
       
       ???
     }
 
-    case _ => v
+    case Sample(d,r) => {
+      ???
+    }
+    
+    /*
+     * factor var out of tuple
+     * TODO: handle interleaved Data
+     * (a,b,c) factorOut("b") => (b,(a,c))
+     * does returning a Sample make sense?
+     */
+    case Tuple(vars) => {
+      val (dvars, rvars) = vars.partition(_.hasName(factor))
+      if (dvars.length == 0) throw new Error("Factor not found in Tuple.")
+      if (dvars.length > 1) throw new Error("Factor found more than once in Tuple.")
+      val dvar = dvars.head
+      
+      rvars.length match {
+        case 0 => variable //only had one element? no change, Sample with Index?
+        case 1 => Tuple(dvar, rvars.head)
+        case _ => Tuple(dvar, Tuple(rvars))
+      }
+    }
+    
+    case _ => ???
   }
 }
 
