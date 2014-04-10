cf31637de2e46b9d46876abdf7e7c403e9c35b02
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-10 17:12:30 -0600

    notes on operations

--- src/main/scala/latis/ops/Operation.scala
+++ src/main/scala/latis/ops/Operation.scala
@@ -4,7 +4,25 @@ import latis.dm._
 import latis.util.LatisProperties
 
 trait Operation {
-
+  
+  /*
+   * Like scala, we should support something like 
+   *   ds.filter(p: Variable => Boolean) 
+   *   ds.map(f: Variable => Variable)
+   *   
+   * Do we need Transformation or just use Operation with Filter as special kind?
+   * 
+   * Filter super (trait?)
+   * ultimately support dataset.filter(predicate)?
+   * currently myFilter(dataset)
+   * filter(var): Boolean?
+   * 
+   * bothered by semantics:
+   *   operation "operates on" a dataset
+   *   apply an operation to a dataset
+   *     consistent with operation.apply(ds) or operation(ds)?
+   */
+  
   def apply(dataset: Dataset): Dataset
 
   //  protected def applyToVariable(variable: Variable): Variable = variable match {
@@ -23,119 +41,17 @@ trait Operation {
   //  protected def applyToSample(sample: Sample): Variable = 
   //    Sample(applyToVariable(sample.domain), applyToVariable(sample.range))
 
-  /*
-     * TODO: if we are operating on function samples is it ever reasonable to NOT return another Sample?
-     * domain and range types could change
-     * but don't we still need to maintain the same number of samples (unless they are filtered out)?
-     * 
-     * but may be some operations that can't be applied to each sample
-     * ++need to keep Filter and Operation unique?
-     *   but still have common superclass - Trait
-     *   apply(ds):ds
-     * Transformation?
-     */
-
-  /*
-   * TODO: 2013-07-01
-   * apply operations, as wrapper on orig Variables?
-   * wrapper would allow adding ops after construction
-   * wrapper might simplify immutability - new Dataset points to orig vars, replacing morphed one with wrapper
-   * consider issues of who has the Data
-   * +assume we get everything via a Function Iterator, just use MorphedFunctions, morphSample
-   * 
-   * *general morphing must happen at dataset level, Function may become something completely different
-   *  only makes sense to wrap Function for Filters, act on each sample, or group like "sliding"
-   *    FilteredFunction (akin to scala collections WithFilter)
-   *  could also wrap Tuple with Filter for projected vars
-   *    ProjectedTuple?
-   *  wrap scalar?
-   *  
-   * 
-   * Like scala, we should support something like 
-   *   ds.filter(p: Variable => Boolean) 
-   *   ds.map(f: Variable => Variable)
-   */
-
-  /*
-     * relational algebra: projection, selection, rename
-     *   projection: list of var names, same as dap2
-     *     "project a,b,c"
-     *     belongs to Dataset
-     *     as opposed to using "include/exclude" in tsml for target var
-     *     but "project" is overloaded with "map projections"
-     *       or confused with noun "project"
-     *   selection: equivalent to true filters, only exclude, no changing of type
-     *     always a "filter"?
-     *     "filter time > 2" 
-     *     or "select ...", too much like SQL, limits thinking? also used for projection in sql
-     *   rename: simply give a var a new name, set name in metadata (leave orig id)
-     *     "rename foo as bar"?
-     * operation: for everything else, typically changes type/metadata
-     *   do we need to distinguish between streamable vs operating on the whole thing?
-     *   use cases for the latter? 
-     *     FFT? appears to be streamable versions
-     *   even a single granule is a Stream with one (potentially infinite?) object
-     *   streamable doesn't have to mean one sample at a time
-     *   e.g. boxcar filter, use scala Seq.grouped, sliding
-     * verbs: project, select, rename, operate/transform/morph
-     * ?better to use words from theory or practical use?
-     *   theory in low level impl, expose common terms in DSL
-     *   
-     * 
-     */
-
     /*
      * Operations: subclass of Operation for each? easier to reason about
      *   Selection: exclude Function samples, affect only provenance metadata
-     *     needs to be given a predicate: Sample => Boolean
-     *     can we use: filterSample(samp) => Option ?
-     *     or is there a more idiomatic way?
-     *     Function as Seq[Sample], iterator is Iterator[Sample]
-     *     selection as function instead of impld in "filter" method
-     *     f.iterator.filter(selection)
-     *     but we are implementing the iterator using getNext, need to apply filter then?
-     *       want to know if there are more samples, so we need NextIterator to make sure there are more valid samples
-     *       other ops don't need to worry about this since they aren't filtering out samples
-     *       filter(v) => Option seems fine
-     *     Selection extend Function1, Sample => Boolean?
      *   Projection: exclude Variables from the model, affect only provenance metadata
-     *     do before reading data
-     *     may need to be clever if data not column oriented, exclude interleaved data values
-     *     predicate: Variable => Boolean
-     *     based entirely on Variable name
-     *     iterating over model components, not data samples
-     *     no need to provide Function wrapper?
-     *       what about extracting data from the orig sample
-     *       might work for col-oriented (only scalars have data) 
-     *       but record-oriented might need to apply to each sample
-     *       redundant but easier for interleaved data?
-     *       maybe wrap data? or just use name => index
-     *       would like to avoid parsing data we just throw away
-     *       adapter responsibility?
-     *       since sample is just a Tuple, the filtering should just work
-     *     filterFunction:
-     *       if f has data, use Wrapped Function
-     *         filter will be applied to each sample as read
-     *         still need to apply to model (domain, range)
-     *         can we do during construction so we can pass these to super constructor?
-     *         ugly, deal with Option...
-     *         replace all constructor args with defs for Function...?
-     *         ++seems like the only way if we want Wrappedfunction to BE-A Function
-     *           especially given potential complications of general Transromation
-     *       else, iterator will get data from kids (column-oriented)
-     *         could still just filter samples, but wasting effort parsing all data
-     *     ++is an empty Tuple allowed?
      *   Transformation
      *     ++need a type of op that doesn't change type/model?
      *       e.g. unit conversion, basic math
      *       change values (and metadata)
      *       do we need a subclass? easier to reason about?
      *     traits for ModelChanging, DataChanging, ...
-     *     
      *   ("Rename" is also part of relational algebra)
-     * 
-     * Keep in mind diffs between applying ops to existing dataset vs applied by adapter during construction
-     * consider function composition, pipe/decorate
      * 
      * Things that can change in a Dataset operation
      * - model
@@ -147,7 +63,7 @@ trait Operation {
      * - select/filter (exclude samples, same type)
      * - subset/resample (diff set of samples, same type)
      * - reduce dimension lengths (stride, bin, ...)
-     * - reduce dimension to length to 1, eliminate? (slice, first, last, min, max, integrate)
+     * - reduce dimension to length to 1, eliminate? (slice, first, last, min, max, integrate) "aggregation" in some circles
      * - convert units (change values, change metadata, same model/type)
      * - replace values (change values, e.g. missing value, may change metadata, same model)
      * - compute new variable (e.g. B mag, same domain, add to model)
@@ -155,11 +71,6 @@ trait Operation {
      * - rename (metadata only)
      * - enhance/repair metadata
      * 
-     * can we always change model or metadata without having to read data?
-     *   "length" is often handy, but ok if that requires processing the data
-     * 
-     * Do we ever need to extend Selection or Projection?
-     * probably not
      */
 
 }
--- src/main/scala/latis/ops/xform/Transformation.scala
+++ src/main/scala/latis/ops/xform/Transformation.scala
@@ -27,68 +27,8 @@ abstract class Transformation extends Operation {
   //sample should be case where we just act on range, else use tuple
   def transformSample(sample: Sample): Sample = Sample(sample.domain, transform(sample.range))
   
-    /*
-     * TODO: if we are operating on function samples is it ever reasonable to NOT return another Sample?
-     * domain and range types could change
-     * but don't we still need to maintain the same number of samples (unless they are filtered out)?
-     * 
-     * but may be some operations that can't be applied to each sample
-     * boxcar, integration
-     * need to use "sliding" on iterator
-     * xform must supply that info, when wrapping function
-     * 
-     * 
-     */
-    
-  /*
-   * TODO: 2013-07-01
-   * apply operations, as wrapper on orig Variables?
-   * wrapper would allow adding ops after construction
-   * wrapper might simplify immutability - new Dataset points to orig vars, replacing morphed one with wrapper
-   * consider issues of who has the Data
-   * +assume we get everything via a Function Iterator, just use MorphedFunctions, morphSample
-   * 
-   * *general morphing must happen at dataset level, Function may become something completely different
-   *  only makes sense to wrap Function for Filters, act on each sample, or group like "sliding"
-   *    FilteredFunction (akin to scala collections WithFilter)
-   *  could also wrap Tuple with Filter for projected vars
-   *    ProjectedTuple?
-   *  wrap scalar?
-   *  
-   * 
-   * Like scala, we should support something like 
-   *   ds.filter(p: Variable => Boolean) 
-   *   ds.map(f: Variable => Variable)
-   */
-  
-    /*
-     * relational algebra: projection, selection, rename
-     *   projection: list of var names, same as dap2
-     *     "project a,b,c"
-     *     belongs to Dataset
-     *     as opposed to using "include/exclude" in tsml for target var
-     *     but "project" is overloaded with "map projections"
-     *       or confused with noun "project"
-     *   selection: equivalent to true filters, only exclude, no changing of type
-     *     always a "filter"?
-     *     "filter time > 2" 
-     *     or "select ...", too much like SQL, limits thinking? also used for projection in sql
-     *   rename: simply give a var a new name, set name in metadata (leave orig id)
-     *     "rename foo as bar"?
-     * operation: for everything else, typically changes type/metadata
-     *   do we need to distinguish between streamable vs operating on the whole thing?
-     *   use cases for the latter? 
-     *     FFT? appears to be streamable versions
-     *   even a single granule is a Stream with one (potentially infinite?) object
-     *   streamable doesn't have to mean one sample at a time
-     *   e.g. boxcar filter, use scala Seq.grouped, sliding
-     * verbs: project, select, rename, operate/transform/morph
-     * ?better to use words from theory or practical use?
-     *   theory in low level impl, expose common terms in DSL
-     *   
-     * 
-     */
-  
+
+ 
 }
 
 
