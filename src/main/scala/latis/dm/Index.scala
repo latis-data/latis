4af16e148d8f4acab24ec7e0098d595796762134
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-29 17:55:15 -0600

    clean up

--- src/main/scala/latis/dm/Index.scala
+++ src/main/scala/latis/dm/Index.scala
@@ -14,12 +14,8 @@ trait Index extends Scalar with Number
 object Index {
   
   def apply(): Index = new AbstractScalar(metadata = Metadata("index")) with Index
-  
-  //def withLength(length: Int): Index = new AbstractScalar(metadata = Metadata("index"), data = IndexData(length)) with Index
 
   def apply(value: Int): Index = new AbstractScalar(metadata = Metadata("index"), data = IndexValue(value)) with Index
 
-  
   def unapply(index: Index): Option[Int] = Some(index.getNumberData.intValue)
-  //TODO: make sure we have a data value as opposed to IndexData...
 }
