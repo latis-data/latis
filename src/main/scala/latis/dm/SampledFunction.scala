3be051dedd5a9716b48bf6d808779aaff8f96af2
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-30 17:13:09 -0600

    clean up, add logging

--- src/main/scala/latis/dm/SampledFunction.scala
+++ src/main/scala/latis/dm/SampledFunction.scala
@@ -10,31 +10,63 @@ import latis.data.SampledData
 import latis.data.SampleData
 import latis.util.PeekIterator
 import latis.util.PeekIterator2
+import latis.data.EmptyData
+import com.typesafe.scalalogging.slf4j.Logging
 
 class SampledFunction(domain: Variable, range: Variable, metadata: Metadata = EmptyMetadata, data: Data = EmptyData) 
-    extends AbstractVariable(metadata, data) with Function {
+    extends AbstractVariable(metadata, data) with Function with Logging {
 
-  
-  //expose domain and range via defs only so we can override (e.g. ProjectedFunction)
+  //expose domain and range via defs only so we can override
   //TODO: no longer the case? require operation to make new SampledFunction if it changes the type instead of a wrapped function
   def getDomain: Variable = domain
   def getRange: Variable = range
   
-  //private var _iterator: Iterator[Sample] = null
-  //TODO: do we have a use case to construct with a set of Samples as opposed to data values?
+  /**
+   * Internal Iterator so we can construct a SampledFunction from an Iterator of Samples.
+   */
+  private var _iterator: Iterator[Sample] = null
   
-  def iterator: PeekIterator[Sample] = new PeekIterator2(getDataIterator, (d: Data) => Some(DataUtils.dataToSample(d, Sample(domain, range))))
+  /**
+   * If this SampledFunction was constructed with an Iterator, return it.
+   * Wrap it as a PeekIterator if it isn't one already.
+   * Otherwise, make the Iterator from the Data Iterator.
+   */
+  def iterator: PeekIterator[Sample] = _iterator match {
+    case null => {
+      logger.debug("Make Iterator from DataIterator: " + this)
+      new PeekIterator2(getDataIterator, (d: Data) => Some(DataUtils.dataToSample(d, Sample(domain, range))))
+    }
+    case pit: PeekIterator[Sample] => {
+      logger.debug("Return existing Iterator: " + this)
+      pit
+    }
+    case _ => {
+      logger.debug("Wrap existing Iterator: " + this)
+      new PeekIterator2(_iterator, (s: Sample) => Some(s))
+    }
+  }
   
-//  {
-////    if (_iterator != null) _iterator //TODO: do we use this? WrappedFunction may want to intervene at DataIterator (new SampledData)
-////    else if (getData.isEmpty) ??? //iterateFromKids
-////    else 
-//    getDataIterator.map(DataUtils.dataToSample(_, Sample(domain, range)))
-//    //TODO: use PeekIterator2(getDataIterator, (s: Sample) => DataUtils.dataToSample(_, Sample(domain, range))) ?
-//    //  then users could peek
-//  }
-  
-  //TODO: require SampledFunction to have SampledData?
-  def getDataIterator: Iterator[SampleData] = getData.asInstanceOf[SampledData].iterator
+  /**
+   * If this SampledFunction was constructed with SampledData, iterate on it.
+   * Otherwise, try to use the sample iterator and map back to the data.
+   */
+  def getDataIterator: Iterator[SampleData] = {
+    val d = getData
+    if (d.isEmpty) {
+      logger.debug("Make Data Iterator from existing Sample Iterator: " + this)
+      new PeekIterator2(_iterator, (s: Sample) => Some(DataUtils.sampleToData(s)))
+    } else {
+      logger.debug("Make Data Iterator from SampledData: " + this)
+      d.asInstanceOf[SampledData].iterator
+    }
+  }
+}
 
+object SampledFunction {
+  
+  def apply(domain: Variable, range: Variable, samples: Iterator[Sample], metadata: Metadata = EmptyMetadata) = {
+    val sf = new SampledFunction(domain, range, metadata=metadata)
+    sf._iterator = samples
+    sf
+  }
 }
