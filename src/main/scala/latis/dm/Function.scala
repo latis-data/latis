4729ac993cc1d0d503104a7b81bfe913a4d9b843
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-30 17:13:48 -0600

    clean up Function construction

--- src/main/scala/latis/dm/Function.scala
+++ src/main/scala/latis/dm/Function.scala
@@ -14,9 +14,8 @@ trait Function extends Variable {
   //TODO: delegate to Operation
   //def apply(v: Variable): Variable
   
-  def iterator: Iterator[Sample]
   //TODO: only applicable to SampledFunction, need to replace lots of pattern matches...
-  
+  def iterator: Iterator[Sample]
   def getDataIterator: Iterator[SampleData]
 }
   
@@ -29,59 +28,74 @@ trait Function extends Variable {
    */
 
 object Function {
+  //TODO: make sure samples are sorted!
   
-  def apply(domain: Variable, range: Variable, md: Metadata = EmptyMetadata, data: SampledData = EmptyData): Function = new SampledFunction(domain, range, md, data)
+  def apply(domain: Variable, range: Variable, md: Metadata = EmptyMetadata, data: SampledData = EmptyData): SampledFunction = new SampledFunction(domain, range, md, data)
   
-  //TODO: do we have a use case to construct with a set of Samples as opposed to data values?
-  //  LimitFilter
-//  def apply(domain: Variable, range: Variable, sampleIterator: Iterator[Sample], md: Metadata = EmptyMetadata): Function = {
-//    val data = SampledData(sampleIterator, Sample(domain, range))
-//    ???
-//  }
+  /**
+   * Construct from Iterator of Samples.
+   */
+  def apply(domain: Variable, range: Variable, sampleIterator: Iterator[Sample], md: Metadata): SampledFunction = {
+    SampledFunction(domain, range, sampleIterator, md)
+  }
+  def apply(domain: Variable, range: Variable, sampleIterator: Iterator[Sample]): SampledFunction = {
+    SampledFunction(domain, range, sampleIterator, EmptyMetadata)
+  }
   
-//TODO: make sure samples are sorted!
+  /**
+   * Construct from Seq of Variable which are assumed to contain their own data.
+   */
+  //def apply(vs: Seq[Variable], md: Metadata = EmptyMetadata): Function = vs.head match {
+  def apply(vs: Seq[Variable]): SampledFunction = vs.head match {
+    case sample: Sample => Function(sample.domain, sample.range, vs.asInstanceOf[Seq[Sample]].iterator)
+    case _ => {
+      //make Seq of samples where domain is index
+      //TODO: make sure every Variable in the Seq has the same type
+      //TODO: make from SampledData with IndexSet
+      val samples = vs.zipWithIndex.map(s => Sample(Index(s._2), s._1))
+      val sample = samples.head
+      Function(sample.domain, sample.range, samples.iterator)
+    }
+  }
   
-//  def apply(vs: Seq[Variable], metadata: Metadata = EmptyMetadata): Function = vs.head match {
-//    case sample: Sample => Function(sample.domain, sample.range, vs.asInstanceOf[Seq[Sample]].iterator, metadata)
-//    case _ => {
-//      //make Seq of samples where domain is index
-//      val samples = vs.zipWithIndex.map(s => Sample(Index(s._2), s._1))
-//      val sample = samples.head
-//      Function(sample.domain, sample.range, samples.iterator, metadata)
-//    }
-//  }
+  /**
+   * Construct from a Seq of domain Variables and a Seq of range Variables.
+   */
+  def apply(ds: Seq[Variable], rs: Seq[Variable]): SampledFunction = {
+    if (ds.length != rs.length) throw new Error("Domain and range sequences must have the same length.")
+    Function((ds zip rs).map(s => Sample(s._1, s._2)))
+  }
   
-//  def apply(ds: Seq[Variable], rs: Seq[Variable]): Function = {
-//    //TODO: assert same length?
-//    Function((ds zip rs).map(s => Sample(s._1, s._2)))
-//  }
-  
-  //TODO: names arg
-  def fromValues(vals: Seq[Seq[Double]]): Function = Function.fromValues(vals.head, vals.tail: _*)
-  
-  def fromValues(dvals: Seq[Double], vals: Seq[Double]*): Function = {
+  /**
+   * Construct from a 2D sequence of double values. Assume the first is for a 1D domain.
+   */
+  def fromValues(vals: Seq[Seq[Double]]): SampledFunction = Function.fromValues(vals.head, vals.tail: _*)
+    
+  /**
+   * Construct from a sequence of double values and a sequence of range values for multiple variables.
+   */
+  def fromValues(dvals: Seq[Double], vals: Seq[Double]*): SampledFunction = {
     val domain = Real(Metadata("domain"))
     val range = vals.length match {
       case 1 => Real(Metadata("range"))
       case n: Int => Tuple((0 until n).map(i => Real(Metadata("real"+i)))) //auto-gen names
     }
     val data: SampledData = {
-      //assert that all Seq are same length
+      //assert that all Seq are same length as the domain
       if (vals.exists(_.length != dvals.length)) throw new Error("Value sequences must be the same length.")
-      //vals.foldLeft(Data.fromDoubles(dvals: _*))(_ zip Data.fromDoubles(_: _*))
       val rdata = vals.tail.foldLeft(Data.fromDoubles(vals.head: _*))(_ zip Data.fromDoubles(_: _*))
       SampledData(DomainSet(Data.fromDoubles(dvals: _*)), rdata)
       //TODO: SampledData.fromValues?
     }
     
-    //make Real if vals.length == 1
-    //val range = if (vals.length == 1) Real(Metadata("range"), vals(0)) else Tuple(vals)(0d) //hack to get around type erasure ambiguity
     Function(domain, range, data = data)
   }
   
-  //def apply(samples: Seq[Sample]): Function = 
-  
-  def unapply(f: Function): Option[Iterator[Sample]] = Some(f.iterator)
+  /**
+   * Expose the Sample Iterator.
+   */
+  def unapply(f: SampledFunction): Option[Iterator[Sample]] = Some(f.iterator)
+  //TODO: only applies to SampledFunction, define it there
   
 }
 
