dc76b989e4673b4653b9721a77c421ea869a00a3
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-29 18:54:19 -0600

    reduce to Sample

--- src/main/scala/latis/ops/filter/FirstFilter.scala
+++ src/main/scala/latis/ops/filter/FirstFilter.scala
@@ -7,8 +7,7 @@ import latis.ops.OperationFactory
  * Keep only the first sample of any outer Function in the Dataset.
  */
 class FirstFilter extends Filter {
-  //TODO: could we return the Sample not nested within the Function (i.e. reduced)?
-  override def applyToFunction(function: Function) = Some(Function(Seq(function.iterator.next)))
+  override def applyToFunction(function: Function) = Some(function.iterator.next)
 }
 
 object FirstFilter extends OperationFactory {
--- src/main/scala/latis/ops/filter/LastFilter.scala
+++ src/main/scala/latis/ops/filter/LastFilter.scala
@@ -7,10 +7,10 @@ import latis.ops.OperationFactory
  * Keep only the last sample of any outer Function in the Dataset.
  */
 class LastFilter extends Filter {
-  //TODO: could we return the Sample not nested within the Function (i.e. reduced)?
+
   override def applyToFunction(function: Function) = {
     val s = function.iterator.drop(function.getLength-1).next
-    Some(Function(Seq(s)))
+    Some(s)
   }
 }
 
