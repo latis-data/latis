c2aea7a59b2edfb3b36aeda0d28fe1ffdd710fe7
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-29 18:56:27 -0600

    start simplifying Function construction

--- src/main/scala/latis/dm/Function.scala
+++ src/main/scala/latis/dm/Function.scala
@@ -6,72 +6,55 @@ import latis.data.set.DomainSet
 import latis.metadata._
 import java.nio.ByteBuffer
 
-trait Function extends Variable { //this: Variable =>
+trait Function extends Variable {
   def getDomain: Variable
   def getRange: Variable
   
   //evaluate for given domain sample
+  //TODO: delegate to Operation
   //def apply(v: Variable): Variable
   
-  //TODO: reconsider when we add ContinuousFunction
-  //def getFirstSample: Sample
-  //def getLastSample: Sample
-  //def getSample(index: Int): Sample
-  
-  //TODO: put in Variable?
-  //def length: Int  getLength?
   def iterator: Iterator[Sample]
-  //TODO: only applicable to SampledFunction
+  //TODO: only applicable to SampledFunction, need to replace lots of pattern matches...
   
-  /**
-   * Function should always have SampledData, for now
-   */
-  //def getData: SampledData
   def getDataIterator: Iterator[SampleData]
 }
-
+  
+  /*
+   * TODO: ContinuousFunction:
+   *   apply(domainVal: Variable) => range val
+   *   apply(domainSet: Seq[Variable] or Var with SeqData) => SampledFunction
+   *   length = -1?
+   *   iterator => error
+   */
 
 object Function {
-  //TODO: used named args for data, md?
   
+  def apply(domain: Variable, range: Variable, md: Metadata = EmptyMetadata, data: SampledData = EmptyData): Function = new SampledFunction(domain, range, md, data)
   
-  def apply(domain: Variable, range: Variable): Function = new SampledFunction(domain, range)
-  
-  def apply(domain: Variable, range: Variable, md: Metadata): Function = new SampledFunction(domain, range, metadata = md)
-  
-  def apply(domain: Variable, range: Variable, data: SampledData): Function = new SampledFunction(domain, range, data = data)
-
-  
-  //build from Iterator[Sample]
-  //TODO: could we set data to something?
-  def apply(domain: Variable, range: Variable, sampleIterator: Iterator[Sample], md: Metadata): Function = {
-    //Note, wouldn't need domain and range, but would have to trigger iterator, or use peek?
-    new SampledFunction(domain, range, sampleIterator, metadata = md)
-  }
-  
-  def apply(domain: Variable, range: Variable, sampleIterator: Iterator[Sample]): Function = 
-    Function(domain, range, sampleIterator, EmptyMetadata)
-  
-  def apply(domain: Variable, range: Variable, md: Metadata, data: SampledData): Function = {
-    new SampledFunction(domain, range, metadata = md, data = data)
-  }
+  //TODO: do we have a use case to construct with a set of Samples as opposed to data values?
+  //  LimitFilter
+//  def apply(domain: Variable, range: Variable, sampleIterator: Iterator[Sample], md: Metadata = EmptyMetadata): Function = {
+//    val data = SampledData(sampleIterator, Sample(domain, range))
+//    ???
+//  }
   
 //TODO: make sure samples are sorted!
   
-  def apply(vs: Seq[Variable], metadata: Metadata = EmptyMetadata): Function = vs.head match {
-    case sample: Sample => Function(sample.domain, sample.range, vs.asInstanceOf[Seq[Sample]].iterator, metadata)
-    case _ => {
-      //make Seq of samples where domain is index
-      val samples = vs.zipWithIndex.map(s => Sample(Index(s._2), s._1))
-      val sample = samples.head
-      Function(sample.domain, sample.range, samples.iterator, metadata)
-    }
-  }
+//  def apply(vs: Seq[Variable], metadata: Metadata = EmptyMetadata): Function = vs.head match {
+//    case sample: Sample => Function(sample.domain, sample.range, vs.asInstanceOf[Seq[Sample]].iterator, metadata)
+//    case _ => {
+//      //make Seq of samples where domain is index
+//      val samples = vs.zipWithIndex.map(s => Sample(Index(s._2), s._1))
+//      val sample = samples.head
+//      Function(sample.domain, sample.range, samples.iterator, metadata)
+//    }
+//  }
   
-  def apply(ds: Seq[Variable], rs: Seq[Variable]): Function = {
-    //TODO: assert same length?
-    Function((ds zip rs).map(s => Sample(s._1, s._2)))
-  }
+//  def apply(ds: Seq[Variable], rs: Seq[Variable]): Function = {
+//    //TODO: assert same length?
+//    Function((ds zip rs).map(s => Sample(s._1, s._2)))
+//  }
   
   //TODO: names arg
   def fromValues(vals: Seq[Seq[Double]]): Function = Function.fromValues(vals.head, vals.tail: _*)
@@ -93,64 +76,15 @@ object Function {
     
     //make Real if vals.length == 1
     //val range = if (vals.length == 1) Real(Metadata("range"), vals(0)) else Tuple(vals)(0d) //hack to get around type erasure ambiguity
-    Function(domain, range, data)
+    Function(domain, range, data = data)
   }
   
   //def apply(samples: Seq[Sample]): Function = 
   
-  def unapply(f: Function): Option[Iterator[Sample]] = Some((f.iterator))
+  def unapply(f: Function): Option[Iterator[Sample]] = Some(f.iterator)
   
 }
 
-//  def apply(dvals: Seq[Double], rvals: Seq[Double]): Function = {
-//    /*
-//     * TODO: store in Data
-//     * expose with iterator, instead of delegating to kids
-//     * do we need FunctionData? 2 arrays, or interleave like tuple?
-//     *   otherwise each can be an IndexFunction
-//     * Seq of pairs, zip, facilitate iteration over pairs
-//     *   generalize beyond a pair of doubles, (or special case?)
-//     *   but then need structure to make sense of numbers
-//     *   use byte buffer?
-//     *   don't forget unzip
-//     * 
-//     * Is there a case where we can delegate to kids' data?
-//     * iterate into nested functions...
-//     * only if we can let kids have multiple samples?
-//     * or split Function into 2 IndexFunctions
-//     *   not conducive for function iterator
-//     * If domain and range scalars can have multiple values in data
-//     *   should we expose IndexFunctions in unapply?
-//     * 
-//     * DomainSet
-//     *   as Scalar with multiple values?
-//     *   as IndexFunction?
-//     *   *as Data?
-//     *   could be linear, index math
-//     *   RealSet?
-//     * RangeSeq, VariableSeq
-//     *   could be nested Fs..., can't be Data?
-//     *   are these structures needed?
-//     *   just a Variable with Seq of Data?
-//     * 
-//     * Seems like we need to be able to store multiple values in the Data of a Scalar type
-//     * In the context of iterating (e.g. math), need to assume that a Scalar has a single value.
-//     * This seems to be the same old problem of mixed abstractions.
-//     * Can we just use IndexFunction interchangeably when we need such behavior?
-//     * 
-//     * just put into function data for now
-//     * if math see f data, iterate
-//     */
-//    val domain = Real(dvals)
-//    val range = Real(rvals)
-//    Function(domain, range)
-//  }
+
+
   
-//
-//object IndexFunction {
-//  
-//  def apply(ds: Array[Double]): IndexFunction = {
-//    //TODO: add length to metadata?
-//    
-//  }
-//}
--- src/main/scala/latis/dm/SampledFunction.scala
+++ src/main/scala/latis/dm/SampledFunction.scala
@@ -8,81 +8,33 @@ import latis.util.DataUtils
 import latis.data.IterableData
 import latis.data.SampledData
 import latis.data.SampleData
+import latis.util.PeekIterator
+import latis.util.PeekIterator2
 
-class SampledFunction(domain: Variable, range: Variable, _iterator: Iterator[Sample] = null,
-    metadata: Metadata = EmptyMetadata, data: Data = EmptyData) 
+class SampledFunction(domain: Variable, range: Variable, metadata: Metadata = EmptyMetadata, data: Data = EmptyData) 
     extends AbstractVariable(metadata, data) with Function {
-  
-  /*
-   * 2013-08-07
-   * TODO: ContinuousFunction
-   * now that Data is separable, how can we support continuous function (e.g. exp model)
-   * ContinuousFunction:
-   *   apply(domainVal: Variable) => range val
-   *   apply(domainSet: Seq[Variable] or Var with SeqData) => SampledFunction
-   *   length = -1?
-   *   iterator => error
-   */
-  
-  /*
-   * TODO: IterableOnce issues
-   * Use Stream? or cache?
-   * effectively becomes like a GranuleAdapter after the first pass
-   * but requires caching even if we don't need it, oh well
-   * responsibility of IterativeAdapter to cache as it iterates
-   * property of adapter for when we really can't cache?
-   * 
-   * iterator.toStream here?
-   * +or add 'stream' in place of 'iterate'?
-   * need hook to disable for really large datasets
-   */
+
   
   //expose domain and range via defs only so we can override (e.g. ProjectedFunction)
+  //TODO: no longer the case? require operation to make new SampledFunction if it changes the type instead of a wrapped function
   def getDomain: Variable = domain
   def getRange: Variable = range
   
-  //override def getData: SampledData = super.getData.asInstanceOf[SampledData]
-
-  
   //private var _iterator: Iterator[Sample] = null
+  //TODO: do we have a use case to construct with a set of Samples as opposed to data values?
   
-  def iterator: Iterator[Sample] = {
-//    if (_iterator != null) _iterator //TODO: do we use this? WrappedFunction may want to intervene at DataIterator (new SampledData)
-//    else if (getData.isEmpty) ??? //iterateFromKids
-//    else 
-    getDataIterator.map(DataUtils.dataToSample(_, Sample(domain, range)))
-    //TODO: use PeekIterator2(getDataIterator, (s: Sample) => DataUtils.dataToSample(_, Sample(domain, range))) ?
-    //  then users could peek
-  }
+  def iterator: PeekIterator[Sample] = PeekIterator2(getDataIterator, (s: Sample) => DataUtils.dataToSample(_, Sample(domain, range)))
+  
+//  {
+////    if (_iterator != null) _iterator //TODO: do we use this? WrappedFunction may want to intervene at DataIterator (new SampledData)
+////    else if (getData.isEmpty) ??? //iterateFromKids
+////    else 
+//    getDataIterator.map(DataUtils.dataToSample(_, Sample(domain, range)))
+//    //TODO: use PeekIterator2(getDataIterator, (s: Sample) => DataUtils.dataToSample(_, Sample(domain, range))) ?
+//    //  then users could peek
+//  }
   
   //TODO: require SampledFunction to have SampledData?
   def getDataIterator: Iterator[SampleData] = getData.asInstanceOf[SampledData].iterator
-//  {
-//    if (data.notEmpty) data.asInstanceOf[IterableData].iterator
-//    else ???
-//  }
-    
-//  private def iterateFromKids: Iterator[Sample] = {
-//    //val dit = domain.getDataIterator.map(data => DataUtils.dataToVariable(data, domain))
-//    val dit = domain.getDomainDataIterator.map(data => DataUtils.dataToVariable(data, domain))
-//    val rit = range.getDataIterator.map(data => DataUtils.dataToVariable(data, range))
-//    (dit zip rit).map(pair => Sample(pair._1, pair._2))
-//  }
-
-  
-  //Support first and last filters
-  //TODO: consider more optimal approaches
-  //TODO: consider immutability, iterator position
-//  def getFirstSample: Sample = iterator.next //TODO: peek
-//  def getLastSample: Sample = {
-//    //iterator.drop(length-1).next  //dataIterator is giving DataUtils.dataToSample null Data!?
-//    var sample: Sample = null
-//    for (s <- iterator) sample = s
-//    sample 
-//  }
-//  
-//  def getSample(index: Int): Sample = {
-//    iterator.drop(index-1).next
-//  }
 
 }
