e57a30aeedc50540c72cebd7b55832df89b36a49
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-30 17:12:24 -0600

    add logging

--- src/main/scala/latis/dm/WrappedFunction.scala
+++ src/main/scala/latis/dm/WrappedFunction.scala
@@ -4,23 +4,32 @@ import latis.data.SampleData
 import latis.ops.Operation
 import latis.util.DataUtils
 import latis.util.PeekIterator2
+import latis.util.PeekIterator
+import com.typesafe.scalalogging.slf4j.Logging
 
 //TODO: SampleMappedFunction?
 class WrappedFunction(function: Function, val operation: Operation) 
-  extends SampledFunction(function.getDomain, function.getRange) {
+  extends SampledFunction(function.getDomain, function.getRange) with Logging {
   
   /**
    * Override iterator to apply the Operation to each sample as it iterates (lazy).
    */
-  override def iterator: Iterator[Sample] = new PeekIterator2(function.iterator, (s: Sample) => operation.applyToSample(s))
+  override def iterator: PeekIterator[Sample] = {
+    logger.debug("Sample Iterating: " + this)
+    new PeekIterator2(function.iterator, (s: Sample) => operation.applyToSample(s))
+  }
   
   /**
    * Get an Iterator of SampleData for this Function. This is really a step backwards
    * since it uses the wrapped Function's iterator which has already put the original Data into Samples.
    */
   //only needed for projection?
-  override def getDataIterator: Iterator[SampleData] = iterator.map(DataUtils.sampleToData(_))
+  override def getDataIterator: Iterator[SampleData] = {
+    logger.debug("Data Iterating: " + this)
+    iterator.map(DataUtils.sampleToData(_))
+  }
 
+  override def toString = super.toString + " + " + operation
 }
 
 object WrappedFunction {
