8e6dfec2412e2171d3b190b53dfff01e9241d9df
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-29 18:37:00 -0600

    extend SampledData ao we can use it in Function construction

--- src/main/scala/latis/data/EmptyData.scala
+++ src/main/scala/latis/data/EmptyData.scala
@@ -2,11 +2,9 @@ package latis.data
 
 import java.nio.ByteBuffer
 
-object EmptyData extends IterableData {
-  override def getByteBuffer: ByteBuffer = ByteBuffer.allocate(0)
-  def iterator = Iterator.empty
+object EmptyData extends SampledData {
   override def length = 0
-  def recordSize = 0
-  
-  //def apply(index: Int): Data = throw new RuntimeException("EmptyData")
+  override def recordSize = 0
+  def iterator = Iterator.empty
+  override def getByteBuffer: ByteBuffer = ByteBuffer.allocate(0)
 }
