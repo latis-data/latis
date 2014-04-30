01776c89d6fe093df12ec2ad8db1993785588353
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-04-30 17:14:39 -0600

    override

--- src/main/scala/latis/data/EmptyData.scala
+++ src/main/scala/latis/data/EmptyData.scala
@@ -5,6 +5,6 @@ import java.nio.ByteBuffer
 object EmptyData extends SampledData {
   override def length = 0
   override def recordSize = 0
-  def iterator = Iterator.empty
+  override def iterator = Iterator.empty
   override def getByteBuffer: ByteBuffer = ByteBuffer.allocate(0)
 }
