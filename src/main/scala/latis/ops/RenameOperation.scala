58e7ea15c7e2bf8d4f5b5b16f9c33c47c4aff19b
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-05-22 08:34:26 -0600

    Add rename operation. Works for dataset name only, for now.

---
+++ src/main/scala/latis/ops/RenameOperation.scala
@@ -0,0 +1,28 @@
+package latis.ops
+
+import latis.dm.Dataset
+import latis.dm.Variable
+import latis.metadata.Metadata
+
+class RenameOperation(origName: String, newName: String) extends Operation {
+
+  /*
+   * TODO: apply to any Variable
+   */
+  
+  //make new Dataset with new metadata with the new name
+  override def apply(dataset: Dataset): Dataset = {
+    if (dataset.hasName(origName)) {
+      //TODO: deal with vars with same name or alias
+      val md = Metadata(dataset.getMetadata.getProperties + ("name" -> newName))
+      Dataset(dataset.getVariables, md) 
+    } else dataset
+  }
+  
+  override def applyToVariable(variable: Variable): Option[Variable] = ???
+}
+
+object RenameOperation {
+  
+  def apply(name1: String, name2: String) = new RenameOperation(name1, name2)
+}
