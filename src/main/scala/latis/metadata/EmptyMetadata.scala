c345f0f24e31ce33e8359f47af4ad8c965581c41
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-07-24 13:04:06 -0600

    add isEmpty, nonEmpty convenience methods

--- src/main/scala/latis/metadata/EmptyMetadata.scala
+++ src/main/scala/latis/metadata/EmptyMetadata.scala
@@ -4,4 +4,6 @@ object EmptyMetadata extends Metadata {
   def get(key: String) = None
   def getProperties: Map[String,String] = Map[String,String]() 
   def has(key: String): Boolean = false
+  override def isEmpty = true
+  override def nonEmpty = false
 }
