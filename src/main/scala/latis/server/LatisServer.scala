8354345399a1b6395be548edd05185de3c0297ff
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-08-20 16:33:36 -0600

    notes on catching client abort
    Former-commit-id: 826001ec1c791ac250061f87eaeb55d328a76770

--- src/main/scala/latis/server/LatisServer.scala
+++ src/main/scala/latis/server/LatisServer.scala
@@ -9,6 +9,7 @@ import com.typesafe.scalalogging.slf4j.Logging
 import java.net.URLDecoder
 import latis.metadata.Catalog
 import latis.dm.Dataset
+import java.io.IOException
 
 class LatisServer extends HttpServlet with Logging {
 
@@ -60,16 +61,32 @@ class LatisServer extends HttpServlet with Logging {
       //Make the Writer, wrapped for Servlet output.
       logger.debug("Writing " + suffix + " dataset.")
       val writer = HttpServletWriter(response, suffix)
+      //TODO: if suffix not supported, return "415 Unsupported Media Type"
 
       //Write the dataset. 
       //Note, data might not be read until the Writer asks for it.
       //  So don't blame the Writer if this seems slow.
-      writer.write(dataset)
+      //Handle IOException when user aborts request.
+      //TODO: are we at risk of catching/masking other sources of IOException?
+      //TODO: but reading is usually lazy so this would catch dataset reading errors too, which do need an error response
+      //try {
+        writer.write(dataset)
+      //} catch {
+      //  case e: IOException => logger.warn("IOException during write: " + e.getMessage)
+      //}
       
       logger.info("Request complete.") //TODO: "with status...", do in finally?
 
     } catch {
-      
+      //Handle client abort (broken pipe).
+      //This is not critical but we shouldn't log an error in this case.
+      //Glassfish and Tomcat throw org.apache.catalina.connector.ClientAbortException
+      //  which extends IOException with the message: "Broken pipe"
+      //TODO: need to be app server agnostic, don't want to add catalina.jar dependency
+      //Do in try/catch block around write above
+//      case cae: org.apache.catalina.connector.ClientAbortException => {
+//        logger.warn("ClientAbortException: " + cae.getMessage)
+//      }
       case e: Throwable => {
         //TODO: throw internal and external exceptions with messages to print here
         //TODO: safe to catch Thowable? e.g. get out of memory errors, we are on our way out anyway
