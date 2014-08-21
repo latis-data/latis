ec97f51cc1b73ed60d554d1b1af82b73c366c0eb
jast1399 <jast1399@lasp.colorado.edu>
2014-08-21 08:17:17 -0600

    remove sparql/lemr
    Former-commit-id: 5db81390814abfdfb4de61bf1ea60a56b9196545

--- src/test/scala/latis/writer/TestInfoWriter.scala
+++ src/test/scala/latis/writer/TestInfoWriter.scala
@@ -1,40 +1,40 @@
-package latis.writer
-
-import org.junit._
-import latis.reader.tsml.TsmlReader
-import latis.ops.filter.Selection
-
-class TestInfoWriter extends WriterTest{
-  
-  @Test
-  def test_dap2 {
-    test_writer(getDataset("dap2"),"info")
-  }
-  @Test
-  def test_fof {
-    test_writer(getDataset(fof),"info")
-  }
-  @Test
-  def test_scalar {
-    test_writer(getDataset("scalar"),"info")
-  }
-  @Test
-  def test_tsi {
-    test_writer(getDataset("tsi"),"info")
-  }
-  @Test
-  def test_tof {
-    test_writer(getDataset(tof),"info")
-  }
-  
-  //@Test
-  def print_info {
-    print(fof, "info")
-  }
-  
-  //@Test 
-  def write_info_file {
-    write_to_file(fof, "info")
-  }
-
-}
+//package latis.writer
+//
+//import org.junit._
+//import latis.reader.tsml.TsmlReader
+//import latis.ops.filter.Selection
+//
+//class TestInfoWriter extends WriterTest{
+//  
+//  @Test
+//  def test_dap2 {
+//    test_writer(getDataset("dap2"),"info")
+//  }
+//  @Test
+//  def test_fof {
+//    test_writer(getDataset(fof),"info")
+//  }
+//  @Test
+//  def test_scalar {
+//    test_writer(getDataset("scalar"),"info")
+//  }
+//  @Test
+//  def test_tsi {
+//    test_writer(getDataset("tsi"),"info")
+//  }
+//  @Test
+//  def test_tof {
+//    test_writer(getDataset(tof),"info")
+//  }
+//  
+//  //@Test
+//  def print_info {
+//    print(fof, "info")
+//  }
+//  
+//  //@Test 
+//  def write_info_file {
+//    write_to_file(fof, "info")
+//  }
+//
+//}
