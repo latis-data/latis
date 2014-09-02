16934c69090d207e0057053706122057d17347b2
Doug Lindholm <doug.lindholm@gmail.com>
2014-09-02 11:52:11 -0600

    start test for Time variable support in both domain and range
    Former-commit-id: d397c55ac92e97c16efc83f68827be13032b4e06

---
+++ src/test/scala/latis/time/TestTimeInFunctionRange.scala
@@ -0,0 +1,16 @@
+package latis.time
+
+import org.junit._
+import Assert._
+import latis.metadata.Metadata
+import latis.dm.Real
+import latis.dm.Text
+
+class TestTimeInFunctionRange {
+
+  @Test
+  def jsona {
+    
+  }
+  
+}
