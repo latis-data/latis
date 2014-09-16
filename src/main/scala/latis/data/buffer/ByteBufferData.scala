6f29e2cf280ffe8077db9745baf2ba694e24399f
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-09-16 17:03:07 -0600

    typo
    Former-commit-id: 3629b526b928a34f19ca922a71c80ee683f0d825

--- src/main/scala/latis/data/buffer/ByteBufferData.scala
+++ src/main/scala/latis/data/buffer/ByteBufferData.scala
@@ -12,7 +12,7 @@ class ByteBufferData(val buffer: ByteBuffer) extends Data {
   def size = buffer.limit //TODO: or limit-position since that is all it currently has to offer? and how ByteBuffer.equals works
   
   /**
-   * Return a duplcate that is backed by the same data but has independent position.
+   * Return a duplicate that is backed by the same data but has independent position.
    */
   def getByteBuffer = buffer.duplicate
 }
