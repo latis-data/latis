24f4fc2f43dda56acf87651f8dbda7ca87584828
jast1399 <jast1399@lasp.colorado.edu>
2014-10-09 09:13:44 -0600

    SsiAdapter4
    Former-commit-id: 737816c27c5e46275c4e1d4539ec4b5ce3f36cf6

--- src/main/scala/latis/data/buffer/ByteBufferData.scala
+++ src/main/scala/latis/data/buffer/ByteBufferData.scala
@@ -1,18 +1,36 @@
 package latis.data.buffer
 
 import latis.data.Data
-
 import java.nio.ByteBuffer
+import latis.data.IterableData
+import latis.dm.Variable
+import latis.util.BufferIterator
+import latis.dm.Binary
 
 /**
  * Implement Data with a ByteBuffer.
  */
-class ByteBufferData(val buffer: ByteBuffer) extends Data {
+abstract class ByteBufferData(val buffer: ByteBuffer) extends IterableData {
   
-  def size = buffer.limit //TODO: or limit-position since that is all it currently has to offer? and how ByteBuffer.equals works
+  override def size = buffer.limit //TODO: or limit-position since that is all it currently has to offer? and how ByteBuffer.equals works
   
   /**
    * Return a duplicate that is backed by the same data but has independent position.
    */
-  def getByteBuffer = buffer.duplicate
+  override def getByteBuffer = buffer.duplicate
+  
+}
+
+object ByteBufferData {
+  
+  def apply(buf: ByteBuffer) = new ByteBufferData(buf) {
+    def recordSize = buf.limit
+    def iterator = new BufferIterator(buf, Binary(buf))
+  }
+  
+  def apply(buf: ByteBuffer, v: Variable) = new ByteBufferData(buf) {
+    def recordSize = v.getSize
+    def iterator = new BufferIterator(buf, v)
+  }
+  
 }
---
+++ src/main/scala/latis/reader/tsml/SsiAdapter4.scala
@@ -0,0 +1,93 @@
+package latis.reader.tsml
+
+import latis.reader.tsml.ml.Tsml
+import latis.dm._
+import java.io.RandomAccessFile
+import java.nio.ByteOrder
+import java.nio.channels.FileChannel.MapMode
+import latis.data.Data
+import latis.data.buffer.ByteBufferData
+import latis.data.SampledData
+import latis.util.BufferIterator
+import latis.data.IterableData
+import latis.data.seq.DataSeq
+import latis.util.MappingIterator
+import latis.data.SampleData
+import latis.util.DataUtils
+import latis.util.PeekIterator
+
+class SsiAdapter4(tsml: Tsml) extends TsmlAdapter(tsml) {
+  
+  override def makeFunction(f: Function): Option[Function] = {
+    val template = Sample(f.getDomain, f.getRange)
+    makeSample(template) match {
+      case None => None
+      case Some(sample) => findFunctionData(f) match{
+        case Some(data) => Some(Function(template.domain, template.range, new MappingIterator[SampleData, Sample](data.iterator, sd => Some(DataUtils.dataToSample(sd, sample))),f.getMetadata))
+        case None => None
+      }
+    }
+  }
+  
+  private def findData(v: Variable): Option[IterableData] = v match {
+    case scalar:   Scalar   => findScalarData(scalar)
+//    case sample:   Sample   => findSampleData(sample)
+    case tuple:    Tuple    => findTupleData(tuple)
+    case function: Function => findFunctionData(function)
+  }
+  
+  private def findScalarData(s: Scalar): Option[IterableData] = {
+    val name = s.getName
+    val location = getUrl.getPath + name + ".bin"
+    val file = new RandomAccessFile(location, "r")
+    val order = this.getProperty("byteOrder", "big-endian") match {
+      case "big-endian" => ByteOrder.BIG_ENDIAN
+      case "little-endian" => ByteOrder.LITTLE_ENDIAN
+    }
+    val bb = file.getChannel.map(MapMode.READ_ONLY, 0, file.length)
+    bb.order(order)
+    
+    Some(ByteBufferData(bb, s))
+  }
+    
+  private def findTupleData(tuple: Tuple): Option[IterableData] = {
+    val data = tuple.getVariables.flatMap(findData(_)) 
+    data.length match {
+      case 0 => None
+      case n => Some(data.reduceLeft(_.zip(_)))
+    }
+  }
+  
+  private def findFunctionData(f: Function): Option[SampledData] = {
+    val ddata = findData(f.getDomain).get
+    val rdata = f.getRange match {
+      case nf: Function => {
+        val len = nf.getMetadata("length").get.toInt
+        val sdata = findFunctionData(nf).get
+        
+        val loopData = IterableData(new LoopIterator(sdata.domainSet.iterator), sdata.domainSet.recordSize)
+        
+        IterableData(SampledData(loopData,sdata.rangeData).iterator.grouped(len).map(s => DataSeq(s)), sdata.recordSize * len)
+      }
+      case e => findData(e).get
+    }
+    val sdata = SampledData(ddata, rdata)
+    Some(sdata)
+  }
+  
+  def close = {}
+
+  class LoopIterator[T >: Null](iterator: Iterator[T]) extends PeekIterator[T] {
+    
+    private var it = iterator.duplicate
+    
+    def getNext = {
+      if(it._1 hasNext) it._1.next
+      else {
+        it = it._2.duplicate
+        getNext
+      }
+    }
+  }
+  
+}
