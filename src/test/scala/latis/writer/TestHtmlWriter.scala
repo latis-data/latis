f0b32f173efb219d337af2a4c89b27270749caaf
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-10-16 14:11:08 -0600

    re-enable tests
    Former-commit-id: 32f49c9926cc805b9ce1025d13045de486fa6dcf

--- src/test/scala/latis/writer/TestHtmlWriter.scala
+++ src/test/scala/latis/writer/TestHtmlWriter.scala
@@ -4,26 +4,26 @@ import org.junit.Test
 
 class TestHtmlWriter extends WriterTest {
   
-//  @Test
-//  def test_dap2 {
-//    test_writer(getDataset("dap2"),"html")
-//  }
-//  @Test
-//  def test_fof {
-//    test_writer(getDataset(fof),"html")
-//  }
-//  @Test
-//  def test_scalar {
-//    test_writer(getDataset("scalar"),"html")
-//  }
-//  @Test
-//  def test_tsi {
-//    test_writer(getDataset("tsi"),"html")
-//  }
-//  @Test
-//  def test_tof {
-//    test_writer(getDataset(tof),"html")
-//  }
+  @Test
+  def test_dap2 {
+    test_writer(getDataset("dap2"),"html")
+  }
+  @Test
+  def test_fof {
+    test_writer(getDataset(fof),"html")
+  }
+  @Test
+  def test_scalar {
+    test_writer(getDataset("scalar"),"html")
+  }
+  @Test
+  def test_tsi {
+    test_writer(getDataset("tsi"),"html")
+  }
+  @Test
+  def test_tof {
+    test_writer(getDataset(tof),"html")
+  }
   
   //@Test
   def print_html {
