6241a6eafa89beabdb86034c60c6983c41f6c32a
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-11-03 12:59:24 -0700

    Truncate float values modeled at integers
    Former-commit-id: bb315fb38a23cc50850df74e27a8783a2b9ec100

--- src/test/scala/latis/reader/TestIterativeAsciiAdapter.scala
+++ src/test/scala/latis/reader/TestIterativeAsciiAdapter.scala
@@ -2,7 +2,16 @@ package latis.reader
 
 import org.junit._
 import Assert._
+import latis.reader.tsml.TsmlReader
+import latis.writer.AsciiWriter
 
 class TestIterativeAsciiAdapter extends AdapterTests {
   def datasetName = "ascii_iterative"
+    
+  @Test
+  def float_as_int {
+    val ds = TsmlReader("ascii_float_as_int.tsml").getDataset
+    val data = ds.toDoubleMap
+    assertEquals(1.0, data("myReal").head, 0.0)
+  }
 }
