fb84c39a2a3898e4b15672a322fe8255b33351d3
Doug Lindholm <doug.lindholm@gmail.com>
2014-11-07 16:13:09 -0700

    Add operation to exclude missing values
    Former-commit-id: 41d6e8acf0e0bfb2b4ded919cd641a1834eb11d9

--- src/main/scala/latis/ops/filter/ExcludeMissing.scala
+++ src/main/scala/latis/ops/filter/ExcludeMissing.scala
@@ -3,16 +3,28 @@ package latis.ops.filter
 import latis.dm.Scalar
 import latis.dm.Number
 import latis.dm.Variable
+import latis.ops.OperationFactory
+import latis.dm.Tuple
 
 class ExcludeMissing extends Filter {
 
   override def applyToScalar(scalar: Scalar): Option[Variable] = {
-    val missingValue = scalar.getMissingValue
-    
-    scalar match {
-      case Number(d) =>
-    }
-    
-    ???
+    if (scalar.isMissing) None
+    else Some(scalar)
   }
+  
+  /**
+   * Exclude a Tuple if any element in it is excluded.
+   */
+  override def applyToTuple(tuple: Tuple): Option[Variable] = {
+    val vars = tuple.getVariables.flatMap(applyToVariable(_))
+    if (vars.length == tuple.getElementCount) Some(tuple)
+    else None
+  }
+  
+  //TODO: Nested Function, exclude entire outer sample
+}
+
+object ExcludeMissing extends OperationFactory {
+  override def apply(): ExcludeMissing = new ExcludeMissing
 }
