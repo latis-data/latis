b201bfef13b0236fe6b4c77c6e9918cb4ae23a9f
jast1399 <jast1399@lasp.colorado.edu>
2014-12-02 08:33:36 -0700

    Integrate op tests
    Former-commit-id: 2a123918457542d45fb7ea46d5b544868e1be166

--- src/main/scala/latis/ops/Integrate.scala
+++ src/main/scala/latis/ops/Integrate.scala
@@ -6,6 +6,7 @@ import latis.dm.Real
 import latis.dm.Tuple
 import latis.dm.Variable
 import latis.dm.WrappedFunction
+import latis.time.Time
 
 /**
  * Integrates the innermost function of a dataset using a trapezoidal Riemann sum. 
@@ -24,6 +25,7 @@ class Integrate(start: Double = Double.NaN, end: Double = Double.NaN) extends Op
   def varToDouble(v: Variable): Double = v match {
     case n: Number => n.doubleValue
     case t: Tuple => t.getVariables.map(varToDouble(_)).head
+    case t: Time => t.getJavaTime
     case _ => Double.NaN
   }
   
@@ -43,4 +45,19 @@ class Integrate(start: Double = Double.NaN, end: Double = Double.NaN) extends Op
     sum/2
   }
 
+}
+
+object Integrate extends OperationFactory {
+  
+  override def apply(args: Seq[String]): Integrate = {
+    if (args.length > 2) throw new UnsupportedOperationException("The Integrate accepts up to 2 argument")
+    try {
+      Integrate(args(0).toDouble, args(1).toDouble)
+    } catch {
+      case e: NumberFormatException => throw new UnsupportedOperationException("The Integrate requires two or zero numeric arguments")
+    }
+  }
+    
+  def apply(start: Double, stop: Double): Integrate = new Integrate(start, stop)
+  override def apply(): Integrate = new Integrate()
 }
