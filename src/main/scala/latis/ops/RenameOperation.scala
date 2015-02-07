315b8afebd5b1b3ce91564ec991e441aa9ba4125
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-02-06 17:39:07 -0700

    start getting rename operation working for any variable
    Former-commit-id: 49f33b396d7b6faa2074c1edf3ea20cbc948fe2b

--- src/main/scala/latis/ops/RenameOperation.scala
+++ src/main/scala/latis/ops/RenameOperation.scala
@@ -12,17 +12,38 @@ class RenameOperation(val origName: String, val newName: String) extends Operati
   
   //make new Dataset with new metadata with the new name
   override def apply(dataset: Dataset): Dataset = {
+    val dsmd = dataset.getMetadata
+    
     if (dataset.hasName(origName)) {
-      //TODO: deal with vars with same name or alias
-      val md = Metadata(dataset.getMetadata.getProperties + ("name" -> newName))
+      val md = changeName(dsmd, newName)
       Dataset(dataset.getVariables, md) 
-    } else dataset
+    } else { //try the kids
+      val vars = dataset.getVariables.flatMap(applyToVariable(_))
+      Dataset(vars, dsmd)
+    }
   }
   
-  override def applyToVariable(variable: Variable): Option[Variable] = ???
+  override def applyToVariable(variable: Variable): Option[Variable] = Some(variable)
+//  {
+//    //TODO: deal with vars with same name or alias
+//    val md = variable.getMetadata
+//    val md2 = if (variable.hasName(origName)) changeName(md, newName) else md
+//    
+//    //recurse
+//    variable match {
+//      case 
+//    }
+//  }
+  
+  private def changeName(md: Metadata, name: String): Metadata = {
+    Metadata(md.getProperties + ("name" -> name))
+  }
 }
 
-object RenameOperation {
+object RenameOperation extends OperationFactory {
+  
+  override def apply(args: Seq[String]) = RenameOperation(args(0), args(1))
+  //TODO: error handling
   
   def apply(expression: String): RenameOperation = {
     //assume "name1,name2"
