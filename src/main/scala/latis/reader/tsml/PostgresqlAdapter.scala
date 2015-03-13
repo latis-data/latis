a8482371275241d650408cd48c84a699f0eb7149
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-03-13 08:37:16 -0600

    improve docs
    Former-commit-id: b214c78e37e65f7fcf1e3510879b0fa87f08192a

--- src/main/scala/latis/reader/tsml/PostgresqlAdapter.scala
+++ src/main/scala/latis/reader/tsml/PostgresqlAdapter.scala
@@ -5,7 +5,10 @@ import latis.reader.tsml.ml.Tsml
 
 /**
  * Extend JdbcAdapter to change how row limits are applied for
- * Postgresql database queries.
+ * Postgresql database queries. The Postgresql JDBC driver does
+ * not appear to use the java.sql.Statement's setMaxRows which 
+ * we use in the JdcbAdapter. This adapter will add "limit n" 
+ * to the sql (which isn't supported by Oracle).
  */
 class PostgresqlAdapter(tsml: Tsml) extends JdbcAdapter(tsml) {
 
@@ -14,8 +17,10 @@ class PostgresqlAdapter(tsml: Tsml) extends JdbcAdapter(tsml) {
    * Limit, First, and Last Filters use this property.
    */
   override def makeQuery = {
+    //Delegate to the superclass to construct the main query.
     val sql = super.makeQuery
     
+    //Append the limit to the query if the property is set.
     getProperty("limit") match {
       case Some(limit) => sql + " limit " + limit.toInt
       case None => sql
