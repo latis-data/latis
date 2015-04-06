602fc29e026d0d6b6cef39d2502fb88198c791a0
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-04-06 13:13:11 -0600

    Add the termination marker when converting Binary Variable Data to an array of bytes.
    Former-commit-id: 517e6e57b829ee97bea38e660f1e6c358213131c

--- src/main/scala/latis/dm/Binary.scala
+++ src/main/scala/latis/dm/Binary.scala
@@ -2,8 +2,8 @@ package latis.dm
 
 import latis.data.Data
 import latis.metadata.Metadata
-
 import java.nio.ByteBuffer
+import latis.util.DataUtils
 
 /**
  * A single variable (Scalar) that represents an arbitrary binary 'blob'.
@@ -27,13 +27,23 @@ object Binary {
     //add length if not already defined
     val md2 = md.get("length") match {
       case Some(_) => md
-      case None => Metadata(md.getProperties ++ Map("length" -> buffer.capacity.toString))
+      case None => md + ("length" -> buffer.capacity.toString)
     }
     new AbstractScalar(md2, Data(buffer)) with Binary
   }
   
   /**
    * Expose the data represented by this Variable as an array of bytes.
+   * Use the 'limit' of the Data's ByteBuffer to add a nullMark so we
+   * know where the useful bytes end.
    */
-  def unapply(b: Binary): Option[Array[Byte]] = Some(b.getData.getBytes)
+  def unapply(b: Binary): Option[Array[Byte]] = {
+    val bb = b.getData.getByteBuffer
+    bb.rewind //just to make sure we are starting at the beginning
+    val bytes = new Array[Byte](bb.capacity + 8) //allocate array for all bytes
+    bb.get(bytes, 0, bb.limit) //get the useful bytes
+    //insert null marker after useful bytes
+    for (i <- 0 until 8) bytes(bb.limit + i) = DataUtils.nullMark(i)
+    Some(bytes)
+  }
 }
