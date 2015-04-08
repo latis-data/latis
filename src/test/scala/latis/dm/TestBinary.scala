d578daac0e0eb48d06438e2c94a4c5bf50c1f097
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-04-07 18:02:31 -0600

    tests working with new byte array termination marker strategy
    Former-commit-id: 0cc0a60fbdf4cae9db4a9afa4a3ac821b58e1d2f

--- src/test/scala/latis/dm/TestBinary.scala
+++ src/test/scala/latis/dm/TestBinary.scala
@@ -8,6 +8,7 @@ import java.io.FileOutputStream
 import java.nio.DoubleBuffer
 import java.nio.ByteBuffer
 import latis.data.value.StringValue
+import latis.util.DataUtils
 
 class TestBinary {
 
@@ -20,23 +21,38 @@ class TestBinary {
 
   @Test
   def test_binary_string {
-    val bytes = TestBinary.binary_string.getData.getBytes
-    val s = ByteBuffer.wrap(bytes).asCharBuffer.toString
+    val bytes = TestBinary.binary_string match {case Binary(bs) => bs} //won't include termination marker
+    val s = ByteBuffer.wrap(DataUtils.trimBytes(bytes)).asCharBuffer.toString
     assertEquals("Hello", s)
   }
   
   @Test
   def underfilled_buffer {
-    val bb = ByteBuffer.allocate(10)
+    val bb = ByteBuffer.allocate(20)
+    bb.putDouble(3.14)
+    bb.put(DataUtils.nullMark)
+    val bv = Binary(bb)
+    val ds = Dataset(bv)
+    val bytes = ByteArrayWriter.getBytes(ds)
+    assertEquals(8, bytes.length) //write only to the end of the data
+    assertEquals(20, bv.getSize) //variable size is still full buffer capacity = limit + 8 byte marker
+    val d = ByteBuffer.wrap(bytes).getDouble
+    assertEquals(3.14, d, 0.0) //data value is preserved
+  }
+  
+  @Test
+  def no_termination_mark {
+    val bb = ByteBuffer.allocate(8)
     bb.putDouble(3.14)
     val bv = Binary(bb)
     val ds = Dataset(bv)
     val bytes = ByteArrayWriter.getBytes(ds)
     assertEquals(8, bytes.length) //write only to the end of the data
-    assertEquals(10, bv.getSize) //variable size is still full buffer capacity
+    assertEquals(8, bv.getSize) //variable size is still full buffer capacity = limit + 8 byte marker
     val d = ByteBuffer.wrap(bytes).getDouble
     assertEquals(3.14, d, 0.0) //data value is preserved
   }
+  
   
   /*
    * TODO: test overfilled buffer, but only the JdbcAdapter applies that now 
@@ -45,12 +61,15 @@ class TestBinary {
    * akin to StringUtils.padOrTruncate
    */
   
+  //TODO: test time series of underfilled Binary with a selection
+  //operations lead to a wrapped function that ends up using DataUtils.buildVarFromBuffer
+  
 }
 
 /**
  * Static Binary variables to use for testing.
  */
 object TestBinary {
-  def binary_double = Binary(DoubleValue(3.14).getByteBuffer)
-  def binary_string = Binary(StringValue("Hello").getByteBuffer)
+  def binary_double = Binary(DataUtils.terminateBytes(DoubleValue(3.14).getByteBuffer))
+  def binary_string = Binary(DataUtils.terminateBytes(StringValue("Hello").getByteBuffer))
 }
