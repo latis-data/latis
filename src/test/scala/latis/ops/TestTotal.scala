b8ac8e1c4a534b50964102242bf4818a83435b82
jast1399 <jast1399@lasp.colorado.edu>
2015-05-14 22:07:15 +0000

    Sum renamed Total, uses reduceLeft
    Former-commit-id: 3f3d46c35daa2eb4c15329b00152baac4bbd0edd

--- src/test/scala/latis/ops/TestTotal.scala
+++ src/test/scala/latis/ops/TestTotal.scala
@@ -1,25 +1,28 @@
 package latis.ops
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Test
 import latis.dm.TestDataset
 import latis.writer.AsciiWriter
+import latis.dm.Dataset
 
-class TestSum {
+class TestTotal {
   
   @Test
   def canonical {
-    val ds = Sum()(TestDataset.canonical)
-    val data = ds.toStrings
-    assertEquals(1, data(0).length)
-    assertEquals("6.6", data(2)(0))
-    assertEquals("C", data(3)(0))
-    assertEquals("1970/01/03", data(0)(0))
+    try{
+      val ds = Total()(TestDataset.canonical)
+      fail("Total should have failed on Text Variable")
+    }
+    catch{
+      case _: UnsupportedOperationException => //test passed
+    }
   }
   
   @Test
   def function_of_scalars {
-    val ds = Sum()(TestDataset.function_of_named_scalar)
+    val ds = Total()(TestDataset.function_of_named_scalar)
     val data = ds.toStrings
     assertEquals(2, data.length)
     assertEquals(1, data(0).length)
