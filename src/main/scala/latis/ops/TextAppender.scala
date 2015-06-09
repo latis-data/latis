8952101d06754a2103c3a2b16991091ad2cbe7c7
jast1399 <jast1399@lasp.colorado.edu>
2015-06-09 18:07:53 +0000

    change parameter name from 'app' to 'suffix'
    Former-commit-id: d0e8f9ae0e61886ad17dd3b2bc49308ed440da0f

--- src/main/scala/latis/ops/TextAppender.scala
+++ src/main/scala/latis/ops/TextAppender.scala
@@ -4,11 +4,11 @@ import latis.dm.Sample
 import latis.dm.Scalar
 import latis.dm.Text
 
-class TextAppender(name: String, app: String) extends Operation() {
+class TextAppender(name: String, suffix: String) extends Operation() {
   
   override def applyToScalar(s: Scalar) = s.getName match {
     case this.name => {
-      val t = s.getValue.toString + app
+      val t = s.getValue.toString + suffix
       val md = s.getMetadata + ("length", t.length.toString)
       Some(Text(md, t))
     }
@@ -29,5 +29,5 @@ class TextAppender(name: String, app: String) extends Operation() {
 object TextAppender extends OperationFactory {
   override def apply(args: Seq[String]) = new TextAppender(args(0), args(1))
   
-  def apply(name: String, app: String) = new TextAppender(name, app)
+  def apply(name: String, suffix: String) = new TextAppender(name, suffix)
 }
