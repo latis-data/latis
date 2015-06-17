2ed1851489ed582e118781330198c324ddc82034
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-06-17 17:51:18 -0600

    added tests for default time.scale.type
    Former-commit-id: 6071b982563a686631941968d24a311d4fc7078a

--- src/test/scala/latis/time/TestTimeScaleType.scala
+++ src/test/scala/latis/time/TestTimeScaleType.scala
@@ -6,6 +6,7 @@ import latis.util.LatisProperties
 import latis.ops.Operation
 import latis.dm.TestDataset
 import latis.writer.AsciiWriter
+import latis.metadata.Metadata
 
 class TestTimeScaleType {
 
@@ -35,4 +36,47 @@ class TestTimeScaleType {
     assertEquals(data("myTime").head, -24.0, 0)
   }
   
+  @Test @Ignore //passes in isolation but fails with others since the defaults are static (LATIS-290)
+  def formatted_time_with_utc_default = {
+    //diff should include 1 leap second
+    System.setProperty("time.scale.type", "UTC")
+    val md = Metadata("units" -> "yyyyMMdd")
+    val time1 = Time(md, "20150630")
+    val time2 = Time(md, "20150702")
+    //convert tp GPS time scale
+    val gps = TimeScale("1980-01-06", TimeUnit.MICROSECOND, TimeScaleType.TAI)
+    val gps1 = time1.convert(gps).getNumberData.doubleValue
+    val gps2 = time2.convert(gps).getNumberData.doubleValue
+    val dt = gps2 - gps1
+    assertEquals(1.72801e11, dt, 0.0)
+  }
+  
+  @Test
+  def formatted_time_with_tai_default = {
+    //both are TAI scales so no leap second diff
+    System.setProperty("time.scale.type", "TAI")
+    val md = Metadata("units" -> "yyyyMMdd")
+    val time1 = Time(md, "20150630")
+    val time2 = Time(md, "20150702")
+    //convert tp GPS time scale
+    val gps = TimeScale("1980-01-06", TimeUnit.MICROSECOND, TimeScaleType.TAI)
+    val gps1 = time1.convert(gps).getNumberData.doubleValue
+    val gps2 = time2.convert(gps).getNumberData.doubleValue
+    val dt = gps2 - gps1
+    assertEquals(1.72800e11, dt, 0.0)
+  }
+  
+  @Test
+  def formatted_time_with_native_default = {
+    //The native scale does not know about leap seconds so just plays along
+    val md = Metadata("units" -> "yyyyMMdd")
+    val time1 = Time(md, "20150630")
+    val time2 = Time(md, "20150702")
+    //convert tp GPS time scale
+    val gps = TimeScale("1980-01-06", TimeUnit.MICROSECOND, TimeScaleType.TAI)
+    val gps1 = time1.convert(gps).getNumberData.doubleValue
+    val gps2 = time2.convert(gps).getNumberData.doubleValue
+    val dt = gps2 - gps1
+    assertEquals(1.72800e11, dt, 0.0)
+  }
 }
