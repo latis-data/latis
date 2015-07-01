a6796e6a6d099fa0f3d9c766e4401a828445e5f7
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-07-01 15:45:42 -0600

    make sure leap seconds work with the TAI epoch
    Former-commit-id: 0475ea4fd68caace3aaf69b1a206bddf8f0ee836

--- src/test/scala/latis/time/TestTaiToUtcConverter.scala
+++ src/test/scala/latis/time/TestTaiToUtcConverter.scala
@@ -213,10 +213,15 @@ class TestTaiToUtcConverter {
   @Test
   def pre_utc_epoch = {
     val ts = TimeScale("TAI seconds since 1958-01-01")
-    val t = Time(ts, 1798761635)
+    val t = Time(ts, 1798761635) //confirmed by other source
     val ts2 = TimeScale("UTC milliseconds since 1970-01-01")
     val t2 = t.convert(ts2) 
     val s = t2.format("yyyy-MM-dd HH:mm:ss")
     assertEquals("2015-01-01 00:00:00", s)
+    
+    System.setProperty("time.scale.type", "UTC")
+    val t3 = Time.fromIso("2015-01-01")
+    val t4 = t3.convert(ts)
+    assertEquals(1798761635, t4.getNumberData.longValue)
   }
 }
