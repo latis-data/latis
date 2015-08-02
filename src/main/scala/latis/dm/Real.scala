5228a4ea978d2e779de56040f0a38fb9272dbac9
Doug Lindholm <dlindhol@gmail.com>
2015-08-02 10:04:17 -0600

    clean up Real and Integer construction, avoid AnyVal
    Former-commit-id: 302d008dbbbabaddcf4d3b08fbea4fcace2580e2

--- src/main/scala/latis/dm/Real.scala
+++ src/main/scala/latis/dm/Real.scala
@@ -11,34 +11,35 @@ trait Real extends Scalar with Number
 
 
 object Real {
-  def apply(): Real = new AbstractScalar() with Real
+  //needed, empty? def apply(): Real = new AbstractScalar() with Real
   
-  def fromDouble(v: Double): Real = new AbstractScalar(data = Data(v)) with Real
-  
-  def apply(v: AnyVal): Real = v match {
-    case d: Double => Real.fromDouble(d)
-    case i: Int    => Real.fromDouble(i.toDouble)
-    case l: Long   => Real.fromDouble(l.toDouble)
-    case f: Float  => Real.fromDouble(f.toDouble)
-    case s: Short  => Real.fromDouble(s.toDouble)
-    case st: scala.collection.immutable.StringOps => Real.fromDouble(st.toDouble)
+  def apply(data: Data): Real = data match {
+    case dv: DoubleValue => new AbstractScalar(data = dv) with Real
+    case _ => throw new UnsupportedOperationException("A Real must be constructed with a DoubleValue.")
   }
   
-  //def apply(md: Metadata, data: Data): Real = new AbstractScalar(md, data) with Real
+  def apply(v: String): Real = Real(DoubleValue(v.toDouble))
+  def apply(v: Double): Real = Real(DoubleValue(v))
+  def apply(v: Float):  Real = Real(DoubleValue(v.toDouble))
+  def apply(v: Long):   Real = Real(DoubleValue(v.toDouble))
+  def apply(v: Int):    Real = Real(DoubleValue(v.toDouble))
+  def apply(v: Short):  Real = Real(DoubleValue(v.toDouble))
+
   
-  def apply(md: Metadata, v: Any): Real = v match {
-    case d: Double => new AbstractScalar(md, data = DoubleValue(d)) with Real
-    case dv: DoubleValue => new AbstractScalar(md, dv) with Real
-    case i: Int    => Real(md,i.toDouble)
-    case l: Long   => Real(md,l.toDouble)
-    case f: Float  => Real(md,f.toDouble)
-    case s: Short  => Real(md,s.toDouble)
-    case st: scala.collection.immutable.StringOps => Real(md,st.toDouble)
+  def apply(md: Metadata, data: Data): Real = data match {
+    case dv: DoubleValue => new AbstractScalar(md, data) with Real
+    case _ => throw new UnsupportedOperationException("A Real must be constructed with a DoubleValue.")
   }
+  
+  def apply(md: Metadata, v: String): Real = Real(md, DoubleValue(v.toDouble))
+  def apply(md: Metadata, v: Double): Real = Real(md, DoubleValue(v))
+  def apply(md: Metadata, v: Float):  Real = Real(md, DoubleValue(v.toDouble))
+  def apply(md: Metadata, v: Long):   Real = Real(md, DoubleValue(v.toDouble))
+  def apply(md: Metadata, v: Int):    Real = Real(md, DoubleValue(v.toDouble))
+  def apply(md: Metadata, v: Short):  Real = Real(md, DoubleValue(v.toDouble))
   
   def apply(md: Metadata): Real = new AbstractScalar(md) with Real
 
-  def apply(md: Metadata, v: Double): Real = new AbstractScalar(md, Data(v)) with Real
   
   /**
    * Expose the double value represented by this Variable.
