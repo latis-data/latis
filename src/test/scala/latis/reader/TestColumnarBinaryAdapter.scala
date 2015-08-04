a9b8b5cbc99eb605a040f365eb94373dd9706d91
jast1399 <jast1399@lasp.colorado.edu>
2015-08-04 18:53:49 +0000

    create files for binary columnar test in test class
    Former-commit-id: af4ad5e919116b87b65ff1e66192ed9fdd52be11

--- src/test/scala/latis/reader/TestColumnarBinaryAdapter.scala
+++ src/test/scala/latis/reader/TestColumnarBinaryAdapter.scala
@@ -1,20 +1,72 @@
 package latis.reader
 
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.ByteBuffer
+
+import scala.Iterator
+import scala.collection.mutable.ArrayBuffer
+
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
-import latis.util.iterator.LoopIterator
-import latis.util.iterator.RepeatIterator
-import latis.reader.tsml.TsmlReader
-import latis.writer.AsciiWriter
+
 import latis.dm.Dataset
 import latis.dm.Function
-import latis.dm.Sample
 import latis.dm.Integer
-import org.junit.Ignore
+import latis.dm.Sample
+import latis.reader.tsml.TsmlReader
+import latis.util.FileUtils
+import latis.util.iterator.LoopIterator
+import latis.util.iterator.RepeatIterator
+import latis.writer.AsciiWriter
 
 class TestColumnarBinaryAdapter extends AdapterTests{
   
   def datasetName = "binary_columns"
+  
+  var files = ArrayBuffer[File]()
+  
+  @Before
+  def make_files {
+    val dir = FileUtils.getTmpDir
+    val myTime = Seq("1970/01/01","1970/01/02","1970/01/03")
+    val myInt = Seq(1,2,3)
+    val myReal = Seq(1.1,2.2,3.3)
+    val myText = Seq("A   ","B   ","C   ")
+    val timeBytes = ByteBuffer.allocate(60)
+    myTime.foreach(_.foreach(timeBytes.putChar(_)))
+    val intBytes = ByteBuffer.allocate(24)
+    myInt.foreach(intBytes.putLong(_))
+    val realBytes = ByteBuffer.allocate(24)
+    myReal.foreach(realBytes.putDouble(_))
+    val textBytes = ByteBuffer.allocate(24)
+    myText.foreach(_.foreach(textBytes.putChar(_)))
+    val timeFile = new File(dir, "myTime.bin")
+    val intFile = new File(dir, "myInt.bin")
+    val realFile = new File(dir, "myReal.bin")
+    val textFile = new File(dir, "myText.bin")
+    files += timeFile += intFile += realFile += textFile
+    val timeIS = new FileOutputStream(timeFile)
+    val intIS = new FileOutputStream(intFile)
+    val realIS = new FileOutputStream(realFile)
+    val textIS = new FileOutputStream(textFile)
+    timeIS.write(timeBytes.array)
+    intIS.write(intBytes.array)
+    realIS.write(realBytes.array)
+    textIS.write(textBytes.array)
+    timeIS.close
+    intIS.close
+    realIS.close
+    textIS.close
+  }
+  
+  @After
+  def close_files {
+    files.foreach(_.delete)
+  }
   
   @Test
   def rep_iterator {
