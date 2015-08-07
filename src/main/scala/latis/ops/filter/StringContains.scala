b5d4508133d67f69b3f35d73128bc2fb6d9e8572
jast1399 <jast1399@lasp.colorado.edu>
2015-08-07 18:15:18 +0000

    StringContains Selection
    Former-commit-id: d48cf1c3044e1b7c32f10668c189c839feff06a3

---
+++ src/main/scala/latis/ops/filter/StringContains.scala
@@ -0,0 +1,28 @@
+package latis.ops.filter
+
+import latis.dm.Text
+import latis.ops.OperationFactory
+
+/**
+ * Select values of 'name' that contain 'value'.
+ */
+class StringContains(name: String, value: String) extends Selection(name, "=~", value) {
+  
+  override def applyToText(text: Text): Option[Text] = {
+    if (text.hasName(vname)) text match {
+      case Text(text_value) if (text_value.contains(value)) => Some(text) //keep match
+      case _ => None
+    } else Some(text) //operation doesn't apply to this Scalar Variable, no-op
+  }
+  
+}
+
+object StringContains extends OperationFactory {
+  
+  def apply(name: String, value: String) = new StringContains(name, value)
+  
+  override def apply(args: Seq[String]) = StringContains(args(0), args(1))
+  
+  def unapply(sc: StringContains): Option[(String, String)] = Some((sc.vname, sc.value))
+  
+}
