94f62f87876c3f14fcc64c0aff27068a97c37d87
Nic Flores <flores314@yahoo.com>
2015-09-24 18:20:24 -0600

    Added tests for TakeRightOperation.
    Former-commit-id: 6b21d0fa5ed83c3285cc4695f8237b05dede0ccc

---
+++ src/test/scala/latis/ops/TestTakeRightOperation.scala
@@ -0,0 +1,246 @@
+package latis.ops
+
+import latis.dm.Function
+import latis.dm.implicits._
+import latis.dm.Real
+import latis.metadata.Metadata
+import latis.data.SampledData
+import org.junit._
+import Assert._
+import latis.ops.filter._
+import latis.dm.Dataset
+import latis.dm.TestDataset
+import latis.dm.Sample
+import latis.writer.Writer
+import latis.dm.Integer
+import latis.dm.Tuple
+import latis.dm.Text
+import latis.dm.Index
+import latis.time.Time
+import latis.reader.tsml.TsmlReader
+import latis.dm.Variable
+import latis.writer.AsciiWriter
+
+
+
+
+class TestTakeRightOperation {
+ 
+  @Test
+  def test_takeright_0 {
+    val ds = TakeRightOperation(0)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => assertEquals(true, s.isEmpty)
+    }
+  }
+  
+  @Test
+  def test_takeright_neg {
+    val ds = TakeRightOperation(-1)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => assertEquals(true, s.isEmpty)
+    }
+  }
+  
+  @Test
+  def test_takeright_1 {
+    val ds = TakeRightOperation(1)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(_,Tuple(x)) => x match {
+          case Seq(Integer(i),Real(r),Text(t)) => {
+            assertEquals(3,i)
+            assertEquals(3.3,r,0)
+            assertEquals("C",t)
+          }
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_takeright_2 {
+    val ds = TakeRightOperation(2)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(_,Tuple(x)) => x match {
+          case Seq(Integer(i),Real(r),Text(t)) => {
+            assertEquals(2,i)
+            assertEquals(2.2,r,0)
+            assertEquals("B",t)
+          }
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_takeright_empty_dataset {
+    val ds = TakeRightOperation(5)(Dataset.empty)
+    assertEquals(Dataset.empty, ds)
+  }
+  
+  @Test
+  def test_should_not_takeright_scalar {
+    val ds = TakeRightOperation(1)(TestDataset.integer)
+    ds match {
+      case Dataset(x) => x match {
+        case Integer(i) => assertEquals(42,i)
+        case _ => fail
+      }
+      case _ => fail
+    }
+  }
+  
+  @Test
+  def test_should_not_takeright_tuple_of_scalars {
+    val ds = TakeRightOperation(5)(TestDataset.tuple_of_scalars)
+    ds match {
+      case Dataset(x) => x match {
+        case Tuple(y) => y.head match {
+          case Integer(i) => assertEquals(0,i)
+          case _ => fail
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_function_of_scalar_with_takeright_0 {
+    val ds = TakeRightOperation(0)(TestDataset.function_of_scalar)
+    ds match {
+      case Dataset(Function(s)) => assertEquals(true, s.isEmpty)
+    }
+  }
+  
+  @Test
+  def test_function_of_scalar_with_takeright_1 {
+    val ds = TakeRightOperation(1)(TestDataset.function_of_scalar)
+    assertEquals(1, ds.getLength)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(Real(r1),Real(r2)) => {
+            assertEquals(2.0,r1,0)
+            assertEquals(2.0,r2,0)
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_function_of_scalar_with_takeright_2 {
+    val ds = TakeRightOperation(2)(TestDataset.function_of_scalar)
+    assertEquals(2, ds.getLength)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(Real(r1),Real(r2)) => {
+            assertEquals(1.0,r1,0)
+            assertEquals(1.0,r2,0)
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_function_of_scalar_with_takeright_5 {
+    val ds = TakeRightOperation(5)(TestDataset.function_of_scalar)
+    assertEquals(3, ds.getLength)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(Real(r1),Real(r2)) => {
+            assertEquals(0.0,r1,0)
+            assertEquals(0.0,r2,0)
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_function_of_function_with_takeright_1 {
+    val ds = TakeRightOperation(1)(TestDataset.function_of_functions)
+    ds match {
+      case Dataset(Function(f)) => f.toList.head match{
+        case Sample(Integer(i1),Function(f1)) => { 
+          assertEquals(3,i1)
+          f1.toList.head match {
+            case Sample(Integer(i2),Real(r2)) => {
+              assertEquals(10,i2)
+              assertEquals(30.0,r2,0)
+            }
+          }
+        }
+      }
+    }    
+  }
+  
+  @Test
+  def test_empty_function {
+    val ds = TakeRightOperation(5)(TestDataset.empty_function)
+    ds match {
+      case Dataset(x) => x match {
+        case Function(it) => {
+            assertEquals(true,it.isEmpty)
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_metadata_length {
+    val ds = TakeRightOperation(2)(TestDataset.function_of_scalar_with_length)
+    assertEquals(Some("2"), ds.unwrap.asInstanceOf[Function].getMetadata("length"))
+  }
+  
+  /*
+   * Would like to add something like this, where data is read from a tsml file. 
+   *
+  @Test
+  def test_tsml_data {
+    val data = TsmlReader("datasets/test/data_with_marker.tsml").getDataset
+    AsciiWriter.write(data)
+    AsciiWriter.write(TakeRightOperation(1)(data))
+    val ds = TakeRightOperation(1)(data)
+    ds match {
+      case Dataset(x) => {
+        System.out.println(x.toString())
+        x match {
+        case Function(f) => {
+          System.out.println(f.toList.toString())
+        }
+        }
+      }
+    }
+    assertEquals(data,data)
+  }
+  * 
+  */
+  
+  /*
+   * Should takeRight alter a tuple of functions?
+   * Why does "t" below print as a Vector and not a Seq of Samples?
+   * 
+   *
+  @Test
+  def test_takeright_tuple_of_functions {
+    AsciiWriter.write(TestDataset.tuple_of_functions)
+    val ds = TakeRightOperation(1)(TestDataset.tuple_of_functions)
+    AsciiWriter.write(ds)
+    ds match {
+      case Dataset(x) => x match {
+        case Tuple(t) => t match {
+          case Vector(s) => s match {
+            case Sample(Integer(i),Real(r)) => {
+              assertEquals(12,i)
+              assertEquals(2.0,r,0)
+            }
+          }
+        }
+      }
+    }
+    //assertEquals(TestDataset.tuple_of_functions, DropOperation(0)(TestDataset.tuple_of_functions))
+  }
+  * 
+  */
+
+  
+}
