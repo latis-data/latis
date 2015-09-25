5fd0c0e599cb6008753f7c6260c494172fe7762a
Nic Flores <flores314@yahoo.com>
2015-09-24 18:21:09 -0600

    Added test for DropOperation.
    Former-commit-id: 4ecf93c39a8c3462f7c1d57d489e8c35358c7252

---
+++ src/test/scala/latis/ops/TestDropOperation.scala
@@ -0,0 +1,211 @@
+package latis.ops
+
+import latis.dm.Function
+
+import latis.dm.implicits._
+import latis.dm.Real
+import latis.metadata.Metadata
+import latis.data.SampledData
+import org.junit._
+import Assert._
+import latis.ops.filter._
+import latis.dm.Dataset
+import latis.dm.TestDataset
+import latis.dm.Sample
+import latis.writer.Writer
+import latis.dm.Integer
+import latis.dm.Tuple
+import latis.dm.Text
+import latis.dm.Index
+import latis.time.Time
+import latis.reader.tsml.TsmlReader
+import latis.writer.AsciiWriter
+
+
+class TestDropOperation {
+  
+  @Test
+  def test_drop_0 {
+    val ds = DropOperation(0)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => assertEquals(3,s.toList.length)
+    }
+  }
+  
+  @Test
+  def test_drop_1 {
+    val ds = DropOperation(1)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(_,Tuple(x)) => x match {
+          case Seq(Integer(i),Real(r),Text(t)) => {
+            assertEquals(2,i)
+            assertEquals(2.2,r,0)
+            assertEquals("B",t)
+          }
+          case _ => fail
+        }
+        case _ => fail
+      }
+      case _ => fail
+    }
+  }
+  
+  @Test
+  def test_drop_2 {
+    val ds = DropOperation(2)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => s.toList.head match {
+        case Sample(_,Tuple(x)) => x match {
+          case Seq(Integer(i),Real(r),Text(t)) => {
+            assertEquals(3,i)
+            assertEquals(3.3,r,0)
+            assertEquals("C",t)
+          }
+          case _ => fail
+        }
+        case _ => fail
+      }
+      case _ => fail
+    }
+  }
+  
+  @Test
+  def test_drop_3 {
+    val ds = DropOperation(3)(TestDataset.canonical)
+    ds match {
+      case Dataset(Function(s)) => assertEquals(0,s.toList.length)
+      case _ => fail
+    }
+  }
+  
+  @Test
+  def dropping_empty_dataset {
+    val ds = DropOperation(5)(Dataset.empty)
+    assertEquals(Dataset.empty, ds)
+  }
+  
+  @Test
+  def test_should_not_drop_scalar {
+    val ds = DropOperation(1)(TestDataset.integer)
+    ds match {
+      case Dataset(x) => x match {
+        case Integer(i) => assertEquals(42,i)
+        case _ => fail
+      }
+      case _ => fail
+    }
+  }
+  
+  @Test
+  def test_should_not_drop_tuple_of_scalars {
+    val ds = DropOperation(5)(TestDataset.tuple_of_scalars)
+    ds match {
+      case Dataset(x) => x match {
+        case Tuple(y) => y.head match {
+          case Integer(i) => assertEquals(0,i)
+          case _ => fail
+        }
+      }
+    }
+  }
+  
+  @Test
+  def test_function_of_scalar_with_drop_0 {
+    val ds = DropOperation(0)(TestDataset.function_of_scalar)
+    assertEquals(3, ds.getLength)
+  }
+  
+  @Test
+  def test_function_of_scalar_with_drop_1 {
+    val ds = DropOperation(1)(TestDataset.function_of_scalar)
+    assertEquals(2, ds.getLength)
+  }
+  
+  @Test
+  def test_function_of_scalar_with_drop_3 {
+    val ds = DropOperation(3)(TestDataset.function_of_scalar)
+    assertEquals(0, ds.getLength)
+  }
+  
+  @Test
+  def test_function_of_scalar_with_drop_5 {
+    val ds = DropOperation(5)(TestDataset.function_of_scalar)
+    assertEquals(0, ds.getLength)
+  }
+  
+  @Test
+  def test_function_of_function_with_drop_1 {
+    val exp = Sample(Integer(Metadata("x"), 0), Function((0 until 3).map(j => Sample(Integer(Metadata("y"), 10 + j), Real(Metadata("z"), 10 * 0 + j)))))
+
+    AsciiWriter.write(exp)
+    System.out.println("f_of_f")
+    AsciiWriter.write(TestDataset.function_of_functions)
+    System.out.println("takeright(1)(f_of_f)")
+    AsciiWriter.write(DropOperation(1)(TestDataset.function_of_functions))
+    
+    assertEquals(exp, DropOperation(1)(TestDataset.function_of_functions).unwrap.asInstanceOf[Function].iterator.next)
+  }
+  
+  @Test
+  def test_empty_function {
+    val emptyfunc = Dataset(Function(Real(Metadata("domain")), Real(Metadata("range")), Iterator.empty, Metadata(Map("length" -> "0"))), Metadata("empty_function"))
+    val ds = DropOperation(5)(TestDataset.empty_function)
+    ds match {
+      case Dataset(x) => x match{
+        case Function(it) => assertEquals(true,it.isEmpty)
+      }
+    }
+  }
+  
+  /*
+   * Would like to add test that reads data from tsml file.
+   *
+  @Test
+  def test_tsml_data {
+    val data = TsmlReader("datasets/test/data_with_marker.tsml").getDataset
+    AsciiWriter.write(data)
+    AsciiWriter.write(DropOperation(1)(data))
+    val ds = DropOperation(1)(data)
+    ds match {
+      case Dataset(x) => {
+        System.out.println(x.toString())
+        x match {
+        case Function(f) => {
+          System.out.println(f.toList.toString())
+        }
+        }
+      }
+    }
+    assertEquals(data,ds)
+  }
+  * 
+  */
+  
+  
+  /*
+   * How should drop behave with a tuple of functions?
+   * 
+  @Test
+  def test_should_drop_tuple_of_functions {
+    //AsciiWriter.write(TestDataset.tuple_of_functions)
+    //System.out.println(TestDataset.tuple_of_functions)
+    
+    val ds = DropOperation(1)(TestDataset.tuple_of_functions)
+    AsciiWriter.write(ds)
+    ds match {
+      case Dataset(x) => x match {
+        case Tuple(t) => {
+            AsciiWriter.write(t)
+            System.out.println(t.toList.head)
+            assertEquals(8,t.head.getSize)
+        }
+      }
+    }
+    
+    assertEquals(TestDataset.tuple_of_functions, DropOperation(0)(TestDataset.tuple_of_functions))
+  }
+  *
+  */
+  
+}
