9cd9ca26ad3ccc0019c9167a57bcd0cb77cf3bd5
Nic Flores <flores314@yahoo.com>
2015-09-28 16:46:18 -0600

    moved TakeOperation to latis.ops.filter since it is idepmpotent
    Former-commit-id: 0aef0ad813b807e26c8a8329e5d7d8f8432ced82

---
+++ src/main/scala/latis/ops/filter/TakeOperation.scala
@@ -0,0 +1,38 @@
+package latis.ops.filter
+
+import latis.dm.Function
+import latis.metadata.Metadata
+import latis.ops.OperationFactory
+
+
+class TakeOperation(val n: Int) extends Filter {
+  override def applyToFunction(function: Function) = {
+    //Assume we can hold this all in memory.
+    
+    //get the first 'limit' samples, or all if we had less
+    val samples = function.iterator.take(n).toList
+    
+    //change length of Function in metadata
+    val md = Metadata(function.getMetadata.getProperties + ("length" -> samples.length.toString))
+    
+    //make the new function with the updated metadata
+    samples.length match {
+      case 0 => Some(Function(function.getDomain, function.getRange, Iterator.empty, md)) //empty Function with type of original
+      case _ => Some(Function(samples, md))
+    }
+  }
+}
+
+object TakeOperation extends OperationFactory {
+  
+  override def apply(args: Seq[String]): TakeOperation = {
+    if (args.length > 1) throw new UnsupportedOperationException("The TakeOperation accepts only one argument")
+    try {
+      TakeOperation(args.head.toInt)
+    } catch {
+      case e: NumberFormatException => throw new UnsupportedOperationException("The TakeOperation requires an integer argument")
+    }
+  }
+    
+  def apply(n: Int): TakeOperation = new TakeOperation(n)
+}
