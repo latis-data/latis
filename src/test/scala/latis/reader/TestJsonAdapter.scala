e799d06553d3f9dcacbc5865995a7b766ef03a61
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-10-01 15:05:07 -0600

    test error with json array modeled as function of index
    Former-commit-id: ccd53b324ab2a741fb0ff2a6159970139d193591

--- src/test/scala/latis/reader/TestJsonAdapter.scala
+++ src/test/scala/latis/reader/TestJsonAdapter.scala
@@ -1,6 +1,6 @@
 package latis.reader
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert._
 import org.junit.Test
 
 import latis.reader.tsml.TsmlReader
@@ -14,11 +14,29 @@ class TestJsonAdapter {
     assertEquals(4, data.length)
     assertEquals(3, data(2).length)
     assertEquals("2", data(1)(1))
+  }  
+  
+  @Test
+  def test_adapter_array {
+    val ds = TsmlReader("datasets/test/json_array.tsml").getDataset
+    val data = ds.toStrings
+    assertEquals(5, data.length)
+    assertEquals(3, data(2).length)
+    assertEquals("2", data(1)(1))
   }
   
   @Test
   def test_reader {
     val ds = JsonReader("datasets/test/mixed.json").getDataset
+    val data = ds.toStrings
+    assertEquals(4, data.length)
+    assertEquals(3, data(2).length)
+    assertEquals("2", data(1)(1))
+  }
+  
+  @Test
+  def test_reader_array {
+    val ds = JsonReader("datasets/test/mixed_array.json").getDataset
     val data = ds.toStrings
     assertEquals(4, data.length)
     assertEquals(3, data(2).length)
