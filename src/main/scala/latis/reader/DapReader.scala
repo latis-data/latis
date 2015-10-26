8d782aaed0d77884e2c27db3ea815aed9b45aaee
jast1399 <jast1399@lasp.colorado.edu>
2015-10-26 18:58:05 +0000

    improve DapReader and test
    Former-commit-id: ed3ede778587bb4f3c993270e728d901aa76d7a0

--- src/main/scala/latis/reader/DapReader.scala
+++ src/main/scala/latis/reader/DapReader.scala
@@ -1,33 +1,82 @@
 package latis.reader
 
-import scala.util.parsing.combinator.JavaTokenParsers
-import latis.dm._
-import latis.dm.Binary
-import latis.metadata.Metadata
+import scala.annotation.migration
 import scala.io.Source
+import scala.util.parsing.combinator.JavaTokenParsers
+import latis.dm.Binary
+import latis.dm.Dataset
+import latis.dm.Function
+import latis.dm.Index
+import latis.dm.Integer
+import latis.dm.Real
+import latis.dm.Sample
+import latis.dm.Scalar
+import latis.dm.Text
+import latis.dm.Tuple
+import latis.dm.Variable
+import latis.metadata.Metadata
+import scala.collection.mutable.MapBuilder
+import latis.ops.Operation
+import java.net.URL
 
-class DapReader(url: String) {
+class DapReader(url: String) extends DatasetAccessor{
   
-  val dds = Source.fromURL(url + ".dds").getLines.mkString(" ")
-  val data = {
+  override def close = {}
+  
+  lazy val dds = Source.fromURL(url + ".dds").getLines.mkString(" ")
+  lazy val das = Source.fromURL(url + ".das").getLines.mkString(" ")
+  lazy val data = {
     val chars = Source.fromURL(url + ".dods").drop(dds.length + "\nData:\n".length)
-    //The data should be in 4-byte chunks, but Source reads in 1-byte chunks.
+    //The data should be formatted in 4-byte chunks, but Source reads one byte at a time.
     //Each char read corresponds to one byte. Combine 16-bit Chars into 32-bit Ints. 
     chars.grouped(4).map(cs => (cs(0).toInt << 24) + (cs(1).toInt << 16) + (cs(2).toInt << 8) + (cs(3).toInt))
   }
+  val metadata = collection.mutable.Map[String, Metadata]()
   
-  lazy val origDataset = DdsParser.getDataset(dds)
+  lazy val origDataset = {
+    DasParser.getOrigMetadata
+    DdsParser.getOrigDataset
+  }
   def getOrigDataset: Dataset = origDataset
   
-  def getDataset: Dataset = {
+  override def getDataset(ops: Seq[Operation]): Dataset = {
     val md = getOrigDataset.getMetadata
-    val ov = getOrigDataset.unwrap
+    val od = ops.foldLeft(getOrigDataset)((ds,op) => op(ds))
+    val ov = od.unwrap
     
-    makeVariable(ov) match {
-      case Some(v) => Dataset(v, md)
-      case None => Dataset.empty
+    val v = ov match {
+      //Handle the first function iteratively. The rest of the functions 
+      //can't be iterative because the data must be read when the variable
+      //is first seen. i.e. we can't keep an iterator that hasn't been read.
+      case f: Function => { 
+        val START_OF_INSTANCE = 0x5a000000
+        val END_OF_SEQUENCE = 0xa5000000
+        
+        val it = new Iterator[Sample] {
+          def hasNext = {
+            val n = data.next
+            (n != END_OF_SEQUENCE) && (n == START_OF_INSTANCE)
+          }
+          def next = makeVariable(f.getSample) match {
+            case Some(s: Sample) => s
+            case _ => next
+          }
+        }
+        
+        Function(f, it)
+      }
+      
+      case _ => makeVariable(ov) match {
+        case Some(v) => v
+        case None => ???
+      }
     }
-    
+    val ds = Dataset(v, md)
+    ops.foldLeft(Dataset(v, md))((ds,op) => op(ds))
+  }
+  
+  override def getDataset: Dataset = {
+    getDataset(Seq())
   }
   
   def makeVariable(v: Variable): Option[Variable] = v match {
@@ -42,13 +91,14 @@ class DapReader(url: String) {
       case Some("Int16") => Some(Integer(i.getMetadata, data.next.toShort))
       case Some("UInt16") => Some(Integer(i.getMetadata, data.next))
       case Some("Int32") => Some(Integer(i.getMetadata, data.next))
-      case Some("UInt32") => Some(Integer(i.getMetadata, data.next.toLong))
+      case Some("UInt32") => Some(Integer(i.getMetadata, data.next.toLong & 0xffffffffl))
       case _ => ???
     }
     case r: Real => r.getMetadata("type") match {
       case Some("Float32") => Some(Real(r.getMetadata, java.lang.Float.intBitsToFloat(data.next)))
       case Some("Float64") => {
-        val long = (data.next.toLong << 32) + data.next.toLong
+        val long = (data.next.toLong << 32) + 
+          (data.next.toLong & 0xffffffffl) //remove leading bits for negative numbers
         Some(Real(r.getMetadata, java.lang.Double.longBitsToDouble(long)))
       }
       case _ => ???
@@ -82,7 +132,6 @@ class DapReader(url: String) {
   }
   
   def makeFunction(f: Function): Option[Function] = {
-    //TODO: Array
     val START_OF_INSTANCE = 0x5a000000
     val END_OF_SEQUENCE = 0xa5000000
     
@@ -97,7 +146,7 @@ class DapReader(url: String) {
       }
     }
     
-    Some(Function(f, it))
+    Some(Function(f, it.toArray.iterator))//force reading of iterator
   }
   
   
@@ -113,67 +162,104 @@ class DapReader(url: String) {
         }
     }
     def type_decl: Parser[Variable] = 
-      atomic_decl  | array_decl | structure_decl | 
+      atomic_decl  | /*array_decl |*/ structure_decl | 
       sequence_decl //| grid_decl
     
     //scalar
     def atomic_decl: Parser[Scalar] = atomic_type ~ ident <~ ";" ^^ {
-      case t ~ name => t match {
-        case "Byte" => Binary(Metadata(name))
-        case "Int16" => Integer(Metadata("name"->name, "type"->t))
-        case "UInt16" => Integer(Metadata("name"->name, "type"->t))
-        case "Int32" => Integer(Metadata("name"->name, "type"->t))
-        case "UInt32" => Integer(Metadata("name"->name, "type"->t))
-        case "Float32" => Real(Metadata("name"->name, "type"->t))
-        case "Float64" => Real(Metadata("name"->name, "type"->t))
-        case "String" => Text(Metadata(name))
-        case "Url" => Text(Metadata(name))
+      case t ~ name => {
+        val md = metadata.getOrElse(name, Metadata.empty)
+        t match {
+          case "Byte" => Binary(md)
+          case "Int16" => Integer(md + ("type"->t))
+          case "UInt16" => Integer(md + ("type"->t))
+          case "Int32" => Integer(md + ("type"->t))
+          case "UInt32" => Integer(md + ("type"->t))
+          case "Float32" => Real(md + ("type"->t))
+          case "Float64" => Real(md + ("type"->t))
+          case "String" => Text(md)
+          case "Url" => Text(md)
+        }
       }
     }
     def atomic_type = 
       "Byte" | "Int16" | "UInt16" | "Int32" | "UInt32" | 
       "Float32" | "Float64" | "String" | "Url"
     
-    //index function
-    def array_decl: Parser[Function] = 
-      array_types ~ ident ~ array_dims <~ ";" ^^ {
-      case v ~ name ~ length => {
-        val md = Metadata(Map("name" -> name, "length" -> length))
-        Function(Index(), v, md)
-      }
-    }
-    def array_types = atomic_decl | structure_decl | sequence_decl //| grid_decl
-    def array_dims = array_dim //| array_dim ~ array_dims // only 1D arrays for now
-    def array_dim = "[" ~ ( ident ~ "=" ).? ~> wholeNumber <~ "]"
+    //index function not yet supported
+//    def array_decl: Parser[Function] = 
+//      array_types ~ ident ~ array_dims <~ ";" ^^ {
+//      case v ~ name ~ length => {
+//        val md = metadata.getOrElse(name, Metadata.empty) + ( "length" -> length)
+//        Function(Index(), v, md)
+//      }
+//    }
+//    def array_types = atomic_decl | structure_decl | sequence_decl //| grid_decl
+//    def array_dims = array_dim //| array_dim ~ array_dims // only 1D arrays for now
+//    def array_dim = "[" ~ ( ident ~ "=" ).? ~> wholeNumber <~ "]"
     
     //tuple
     def structure_decl: Parser[Tuple] = 
       "Structure" ~ "{" ~> structure_types.* ~ ("}" ~> ident <~ ";") ^^ {
-      case vars ~ name => Tuple(vars, Metadata(name))
+      case vars ~ name => Tuple(vars, metadata.getOrElse(name, Metadata.empty))
     }
-    def structure_types = atomic_decl | array_decl | structure_decl | sequence_decl //| grid_decl
+    def structure_types = atomic_decl | /*array_decl |*/ structure_decl | sequence_decl //| grid_decl
     
     //function
     def sequence_decl: Parser[Function] = 
       "Sequence" ~ "{" ~> sequence_types.* ~ (sequence_decl).? ~ ("}" ~> ident <~ ";") ^^ {
-      case vars ~ Some(f) ~ name => vars.length match {
-        case 1 => Function(vars.head, f, Metadata(name))
-        case _ => Function(vars.head, Tuple(vars.tail :+ f), Metadata(name))
+      case vars ~ Some(f) ~ name => {
+        val md = metadata.getOrElse(name, Metadata.empty)
+        vars.length match {
+          case 1 => Function(vars.head, f, md)
+          case _ => Function(vars.head, Tuple(vars.tail :+ f), md)
+        }
       }
-      case vars ~ None ~ name => vars.length match {
-        case 1 => Function(Index(), vars.head, Metadata(name))
-        case 2 => Function(vars.head, vars.last, Metadata(name))
-        case _ => Function(vars.head, Tuple(vars.tail), Metadata(name))
+      case vars ~ None ~ name => {
+        val md = metadata.getOrElse(name, Metadata.empty)
+        vars.length match {
+          case 1 => Function(Index(), vars.head, md)
+          case 2 => Function(vars.head, vars.last, md)
+          case _ => Function(vars.head, Tuple(vars.tail), md)
+        }
       }
     }
-    def sequence_types = atomic_decl | array_decl | structure_decl //| grid_decl
+    def sequence_types = atomic_decl | /*array_decl |*/ structure_decl //| grid_decl
     
     //not yet supported
     //def grid_decl = "grid" ~ "{" ~ "array:" ~ array_decl ~ "maps:" ~ array_decl.* ~ "}" ~ ";"
     
-    def getDataset(dds: String) = 
+    def getOrigDataset = 
       parse(dds_doc, dds).get
     
   }
   
+  object DasParser extends JavaTokenParsers {
+
+    def das_doc: Parser[Unit]= "attributes {" ~ attribute_cont.* ~ "}" ^^ {case _ => }
+
+    def attribute_decl = attribute_cont | attribute
+    
+    def attribute_cont: Parser[Unit] = (ident <~ "{") ~ (attribute_decl.* <~ "}") ^^ {
+      case name ~ vals => {
+        var md = Metadata(name)
+        vals.foreach(_ match {
+          case (k: String, v: String) => md += (k -> v)
+          case _ => 
+        })
+        metadata += (name -> md)
+      }
+    }
+    
+    def attribute: Parser[(String, String)] = "string" ~> ident ~ stringLiteral <~ ";" ^^ {
+      case name ~ str => (name -> str.stripPrefix("\"").stripSuffix("\""))
+    }
+    
+    def getOrigMetadata = parse(das_doc, das).get
+  }
+  
+}
+
+object DapReader {
+  def apply(url: String) = new DapReader(url)
 }
