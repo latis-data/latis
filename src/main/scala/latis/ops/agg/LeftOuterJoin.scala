729089ec6e2050293b3614ca658a29c31bc4304d
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-10-30 17:26:03 -0600

    Add a LeftOuterJoin operation to perform a union that results in a sample for every sample in the first (left) dataset.
    Former-commit-id: 7a5675447a981640ce94a903807c7b16c25b792f

---
+++ src/main/scala/latis/ops/agg/LeftOuterJoin.scala
@@ -0,0 +1,97 @@
+package latis.ops.agg
+
+import latis.dm.Dataset
+import latis.dm.Sample
+import latis.util.iterator.PeekIterator
+import latis.dm.Scalar
+import scala.collection.mutable.ArrayBuffer
+import latis.dm.Tuple
+import latis.dm.Function
+import latis.ops.Reduction
+import latis.metadata.EmptyMetadata
+import latis.metadata.Metadata
+import latis.dm.Variable
+
+class LeftOuterJoin extends Aggregation { 
+  //use case: x -> (a,b) LeftOuterJoin x -> (c,d) => x -> (a,b,c,d) 
+  //  where all samples of the initial dataset are kept
+  //  using fill values for the second dataset
+  /*
+   * TODO: implement with resampling? but traversable once problem
+   * this could use assumption that both are sorted
+   * resampling in general would violate FP since it depends on state of iterator
+   * maybe some common chunk of code?
+   */
+  
+  def aggregate(ds1: Dataset, ds2: Dataset) = {    
+    val (f1, f2, it1, it2) = (ds1, ds2) match {
+      case(Dataset(f1 @ Function(it1)), Dataset(f2 @ Function(it2))) => (f1, f2, it1, it2)
+      case _ => throw new UnsupportedOperationException("LeftOuterJoin expects a Function in each of the Datasets it aggregates.")
+    }
+    
+    val reduction = new Reduction
+    
+    //need domain and range types for new Function
+    val dtype = f1.getDomain
+    val rtype = reduction.applyToTuple(Tuple(f1.getRange, f2.getRange)).get //flatten, consistent with below
+    
+    //Make PeekIterator for the second dataset so we can look ahead
+    val pit2 = PeekIterator(it2)
+    
+    //make iterator to return joined samples
+    val samples = new PeekIterator[Sample]() {
+      def getNext = {
+        if (it1.hasNext) {
+          val s1 = it1.next  //next sample from first dataset
+          val d1 = s1.domain.asInstanceOf[Scalar]
+          //val s2 = pit2.peek //next sample from second dataset
+          
+          def getNextMatchingSample: Option[Sample] = {
+            //try the next sample without advancing the iterator
+            val sample = pit2.peek
+            if (sample != null) {
+              val d2 = sample.domain.asInstanceOf[Scalar]
+              d1 compare d2 match {
+                case 0 => Some(sample) //found a match
+                case n: Int if (n < 0) => None  //the other is already ahead
+                case n: Int if (n > 0) => {
+                  //we are ahead of the other, try the next one
+                  pit2.next //advance the iterator
+                  getNextMatchingSample
+                }
+              }
+            } else None //there are no more samples
+          }
+          
+          val range2 = getNextMatchingSample match {
+            case Some(sample) => sample.range
+            case None => makeFillVariable(f2.getRange)
+          }
+          
+          val range = reduction.applyToTuple(Tuple(s1.range, range2)).get //flatten
+          Sample(d1,range)
+          
+        } else null
+      }
+    }
+    
+    //TODO: update metadata
+    //use metadata from first Dataset for new Dataset
+    //Note, dataset metadata must be passed in via the super apply(ds1, ds2, md)
+    val fmd = f1.getMetadata
+    
+    Dataset(Function(dtype, rtype, samples, fmd))
+  }
+  
+
+  //TODO: util method
+  def makeFillVariable(v: Variable): Variable = v match {
+    case s: Scalar => Scalar(s.getMetadata, s.getFillValue)
+  }
+}
+
+object LeftOuterJoin {
+  
+  def apply() = new LeftOuterJoin()
+  def apply(ds1: Dataset, ds2: Dataset, md: Metadata = EmptyMetadata) = new LeftOuterJoin()(ds1, ds2, md)
+}
---
+++ src/test/scala/latis/ops/TestLeftOuterJoin.scala
@@ -0,0 +1,145 @@
+package latis.ops
+
+import org.junit._
+import Assert._
+import latis.reader.tsml.TsmlReader
+import latis.writer._
+import latis.ops.agg.CollectionAggregation
+import latis.ops.agg.TileAggregation
+import latis.dm._
+import latis.ops.agg.Intersection
+import latis.metadata.Metadata
+import latis.ops.agg.LeftOuterJoin
+
+class TestLeftOuterJoin {
+  
+  @Test
+  def same_in_both = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_second_in_middle = {
+    val samples1 = List(1,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_second_at_end = {
+    val samples1 = List(1,2).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_second_at_start = {
+    val samples1 = List(2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_first_in_middle = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2), (t,a)); assert(b.isNaN())}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_first_at_end = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3), (t,a)); assert(b.isNaN())}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_first_at_start = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new LeftOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1), (t,a)); assert(b.isNaN())}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+}
