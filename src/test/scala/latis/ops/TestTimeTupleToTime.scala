746c0dfb57b5da1c9a0f696400283c1db87ecab7
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-11-11 13:00:59 -0700

    test new TimeTupleToTime operation
    Former-commit-id: 06728ea43182f5902d139ffd20f4685971a13874

---
+++ src/test/scala/latis/ops/TestTimeTupleToTime.scala
@@ -0,0 +1,39 @@
+package latis.ops
+
+import org.junit._
+import org.junit.Assert._
+import latis.dm._
+import latis.time._
+import latis.metadata.Metadata
+
+class TestTimeTupleToTime {
+  
+  @Test
+  def date_only = {
+    val year = Text(Metadata("units" -> "yyyy"), "2015")
+    val mon = Text(Metadata("units" -> "MM"), "11")
+    val day = Text(Metadata("units" -> "dd"), "01")
+    val ds = Dataset(Tuple(List(year, mon, day), Metadata("time"))) //Tuple must have name="time"
+    val op = TimeTupleToTime()
+    val ds2 = op(ds)
+    ds2 match {
+      case Dataset(t: Time) => assertEquals(1446336000000l, t.getJavaTime)
+    }
+  }
+  
+  @Test
+  def date_and_time = {
+    val year = Text(Metadata("units" -> "yyyy"), "1970")
+    val mon  = Text(Metadata("units" -> "MM"), "1")
+    val day  = Text(Metadata("units" -> "dd"), "01")
+    val hour = Text(Metadata("units" -> "HH"), "0")
+    val min  = Text(Metadata("units" -> "mm"), "0")
+    val sec  = Text(Metadata("units" -> "ss"), "01")
+    val ds = Dataset(Tuple(List(year, mon, day, hour, min, sec), Metadata("time")))
+    val op = TimeTupleToTime()
+    val ds2 = op(ds)
+    ds2 match {
+      case Dataset(t: Time) => assertEquals(1000l, t.getJavaTime)
+    }
+  }
+}
