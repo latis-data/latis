a657199df5c17070c3dd70c30a37991df39a1312
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2015-12-23 16:33:16 -0700

    base trait for interpolation strategies
    Former-commit-id: 912a1631a3a42d6da1d77656ba3618ea213d5500

--- src/main/scala/latis/ops/resample/Interpolation.scala
+++ src/main/scala/latis/ops/resample/Interpolation.scala
@@ -2,6 +2,16 @@ package latis.ops.resample
 
 trait Interpolation { 
   
+  /**
+   * Given ordered pairs of x and y, return a function that 
+   * when given an x value it will optionally return a corresponding y value.
+   */
   def interpolator(xs: Array[Double], ys: Array[Double]): Double => Option[Double]
   
+  /**
+   * Number of samples needed to perform an interpolation
+   * assuming the interpolate is in the middle?
+   * Default to 2. Can be overridden. Should be even.
+   */
+  val interpolationWindowSize: Int = 2
 }
