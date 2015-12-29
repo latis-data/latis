c9ebe80c5050a811a9ed986351c93c9f8d0ba456
Doug Lindholm <doug.lindholm@gmail.com>
2015-12-29 16:48:49 -0700

    FullOuterJoin guts in place and ready for testing
    Former-commit-id: e1d153810939b7f74f36c065d7c70e9e58c0b7d0

--- src/main/scala/latis/ops/agg/FullOuterJoin.scala
+++ src/main/scala/latis/ops/agg/FullOuterJoin.scala
@@ -6,19 +6,124 @@ import latis.ops.resample.NoInterpolation
 import latis.dm.Function
 import latis.dm.Sample
 import latis.dm.Scalar
+import scala.collection.mutable.ArrayBuffer
+import latis.ops.resample.NoExtrapolation
 
 /**
  * Given two Datasets that each contain a Function with the same domain Variable,
- * create a single Function that contains ALL samples of both Functions using 
+ * create a single Function that contains ALL samples of both Functions using
  * fill values as needed.
  */
-class FullOuterJoin extends BinaryOperation with NoInterpolation { //TODO: with NoExtrapolation {
+class FullOuterJoin extends Join with NoInterpolation with NoExtrapolation {
   //e.g. new FullOuterJoin with LinearInterpolation
   //TODO: diff interpolation for each dataset? from metadata
-  
+
+  private def makeSampleIterator(f1: Function, f2: Function) = new Iterator[Sample]() {
+    //define indices of the inner pair of samples in a sliding window
+    //used to align each dataset for interpolation
+    val upper = interpolationWindowSize / 2
+    val lower = upper - 1
+
+    //create the sliding iterators so we can interpolate
+    //  within sliding windows
+    val it1 = f1.iterator.sliding(interpolationWindowSize).withPartial(false)
+    val it2 = f2.iterator.sliding(interpolationWindowSize).withPartial(false)
+
+    //temporary vars for the sliding windows
+    var as: Array[Sample] = null //left dataset window
+    var bs: Array[Sample] = null //right dataset window
+
+    //hang on to first domain variables so we know when we are done extrapolating
+    var firsta1: Scalar = null
+    var firstb1: Scalar = null
+
+    //trigger staging the first set of samples on the first pass
+    var first = true
+    //trigger using extrapolation before interpolation is possible
+    var beforeFirstInterp = true
+
+    def hasNext = it1.hasNext || it2.hasNext
+
+    def next: Sample = {
+//TODO: premature to test hasNext since we end each iteration with a next
+      val oSample: Option[Sample] = if (!it1.hasNext) {
+        //no more samples in ds1, extrapolate
+        bs = it2.next.toArray
+        extrapolate(as, bs(lower).domain.asInstanceOf[Scalar]) match {
+          case Some(sample) => joinSamples(sample, bs(lower))
+          case None => ??? //fill
+        }
+      } else if (!it2.hasNext) {
+        //no more samples in ds2, extrapolate
+        as = it1.next.toArray
+        extrapolate(bs, as(lower).domain.asInstanceOf[Scalar]) match {
+          case Some(sample) => joinSamples(as(lower), sample)
+          case None => ??? 
+        }
+
+      } else {
+        if (first) {
+          //stage the first samples
+          as = it1.next.toArray
+          bs = it2.next.toArray
+          //get 1st values for extrapolation logic
+          firsta1 = as(lower).domain.asInstanceOf[Scalar]
+          firstb1 = bs(lower).domain.asInstanceOf[Scalar]
+          //done with the first pass
+          first = false
+        }
+        //Get the domain variable of the samples to compare
+        //Assumes scalar domains, for now
+        var a1 = as(lower).domain.asInstanceOf[Scalar]
+        var b1 = bs(lower).domain.asInstanceOf[Scalar]
+        /*
+               * TODO: if beforeFirst then preExtrapolate
+               * how to know when to allow interp?
+               * sign of a1,b1 compare changes or goes to 0
+               * but neither needs to increment
+               * a2 > b1
+               * a2 = b1 need to inc a
+               * use first samples from first block?
+               */
+        //TODO: Deal with extrapolation until both datasets overlap
+        //if (beforeFirstInterp) 
+        /*
+ * can we increment first then interp
+ * allow us to end block with a sample
+ * help with "first" logic?
+ * OR inc after all cases, should be same logic?
+ */
+        val joinedSample = if (a1 < b1) interpolate(as, bs(lower).domain.asInstanceOf[Scalar]) match {
+          case Some(sample) => joinSamples(sample, bs(lower))
+          case None => ??? //fill
+        }
+        else if (a1 > b1) interpolate(bs, as(lower).domain.asInstanceOf[Scalar]) match {
+          case Some(sample) => joinSamples(as(lower), sample)
+          case None => ???
+        }
+        else joinSamples(as(lower), bs(lower))
+
+        //increment iterators as needed
+        val a2 = as(upper).domain.asInstanceOf[Scalar]
+        val b2 = bs(upper).domain.asInstanceOf[Scalar]
+        //Note, a2, b2 are the basis of the next comparison
+        if (a2 <= b2 && it1.hasNext) as = it1.next.toArray
+        if (a2 >= b2 && it2.hasNext) bs = it2.next.toArray
+
+        joinedSample
+      }
+      
+      oSample match {
+        case Some(sample) => sample
+        case None => ??? //TODO: next? but may not be a next - orig need for peekIterator
+      }
+        
+    }
+  }
+
   def apply(ds1: Dataset, ds2: Dataset): Dataset = {
     (ds1, ds2) match {
-      case(Dataset(f1: Function), Dataset(f2: Function)) => {
+      case (Dataset(f1: Function), Dataset(f2: Function)) => {
         //make sure the domains are consistent, match on name for now
         if (f1.getDomain.getName != f2.getDomain.getName) {
           val msg = s"Can't join Functions with different domains."
@@ -29,51 +134,20 @@ class FullOuterJoin extends BinaryOperation with NoInterpolation { //TODO: with 
           val msg = s"Can't join Functions with non Scalar domains, for now."
           throw new UnsupportedOperationException(msg)
         }
+
+        //make Iterator of new Samples
+        //try without PeekIterator so we don't open data source too soon
+ //TODO: can PeekIterator leave data source alone with appropriate use of lazy?
+        val samples = makeSampleIterator(f1, f2)
+
+ //TODO: can we get new function type without peeking (i.e. opening data source)?
         
-        //TODO: factor up common join stuff
-        
-        //define indices of the inner pair of samples in a sliding window
-        //used to align each dataset for interpolation
-        val upper = interpolationWindowSize / 2
-        val lower = upper - 1
-        
-        //create the sliding iterators so we can interpolate
-        //  within sliding windows
-        val it1 = f1.iterator.sliding(interpolationWindowSize)
-        val it2 = f2.iterator.sliding(interpolationWindowSize)
-        
-        //loop for each sample we need to produce
-        var notDone = it1.hasNext && it2.hasNext
-        while (notDone) {
-          var as: List[Sample] = it1.next
-          var bs: List[Sample] = it2.next
-          //if starting with a1 < b1 extrap b and inc a until a1 >= b1
-          //assuming Scala domain for now //TODO: need to compare ordering of domain Tuples
-          var a1 = as(lower).domain.asInstanceOf[Scalar]
-          var b1 = bs(lower).domain.asInstanceOf[Scalar]
-          while (a1 < b1) {
-            //TODO: extrapolate f2
-            if (it1.hasNext) {
-              as = it1.next
-              a1 = as(lower).domain.asInstanceOf[Scalar]
-            } 
-            else notDone = false //hit end of one before it ever got caught up  //TODO: need to skip some stuff
-          }
-          //TODO: b1 < a1
-          
-          //we should now have a set of as and bs to do interp
-          //strip down to doubles
-          //interp for each parameter in range
-         // val interp = interpolator()
-          
-        }
-        
-        ???
+        //TODO: make Function and Dataset metadata
+        Dataset(Function(samples.toSeq)) //memoizing samples for now
       }
-      case _ => throw new UnsupportedOperationException("FullOuterJoin expects a Function in each of the Datasets it joins.")
     }
   }
-  
+
   /*
    * TODO: how could we do this in the context of resampling
    * Discrete telemetry should be filled with previous value: FloorResampling
@@ -110,11 +184,11 @@ class FullOuterJoin extends BinaryOperation with NoInterpolation { //TODO: with 
    *      a2 = b2 => inc a,b (join in next iteration)
    *      a2 < b2 => inc a
    *      a2 > b2 => inc b
-   * a1 > b1 => interp a at b1
+   * a1 < b1 => interp a at b1
    *      a2 = b2 => inc a,b
    *      a2 < b2 => inc a
    *      a2 > b2 => inc b
-   * a1 < b1 => interp b at a1
+   * a1 > b1 => interp b at a1
    *      a2 = b2 => inc a,b
    *      a2 < b2 => inc a
    *      a2 > b2 => inc b
@@ -130,5 +204,4 @@ class FullOuterJoin extends BinaryOperation with NoInterpolation { //TODO: with 
    *   assume even?
    * will sign just work
    */
-  
 }
---
+++ src/test/scala/latis/ops/TestFullOuterJoin.scala
@@ -0,0 +1,171 @@
+package latis.ops
+
+import org.junit._
+import Assert._
+import latis.reader.tsml.TsmlReader
+import latis.writer._
+import latis.ops.agg.CollectionAggregation
+import latis.ops.agg.TileAggregation
+import latis.dm._
+import latis.ops.agg.Intersection
+import latis.metadata.Metadata
+import latis.ops.agg.FullOuterJoin
+
+class TestFullOuterJoin {
+  
+  @Test
+  def same_in_both = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+   AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_second_in_middle = {
+    val samples1 = List(1,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_second_at_end = {
+    val samples1 = List(1,2).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_second_at_start = {
+    val samples1 = List(2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_first_in_middle = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2), (t,a)); assert(b.isNaN())}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_first_at_end = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1,2), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3), (t,a)); assert(b.isNaN())}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def extra_in_first_at_start = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    //AsciiWriter.write(ds)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((1,1), (t,a)); assert(b.isNaN())}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((2,2,4), (t,a,b))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b)))) => assertEquals((3,3,6), (t,a,b))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test
+  def empty_second = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List[Sample]()
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(Real(Metadata("t")), Real(Metadata("b")), Iterator.empty, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+    latis.writer.AsciiWriter.write(ds)
+    val data = ds.toDoubles
+    assertEquals(3, data(0).length) //3 samples
+    assertEquals(2, data.length) //only 2 parameters
+  }
+  
+  @Test(expected=classOf[UnsupportedOperationException])
+  def mismatched_domain = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(Metadata("t"), i), Real(Metadata("a"), i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(Metadata("x"), i), Real(Metadata("b"), i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin()
+    val ds = op(ds1, ds2)
+  }
+  
+  //TODO: same range variable
+}
