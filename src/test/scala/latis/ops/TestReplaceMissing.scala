aa6503fc088234e415d54d492b190b7ed8378e32
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-01-29 10:05:38 -0700

    Test the ReplaceMissingOperation
    Former-commit-id: 1054b9758afc1f886f217c559beb2f311ad149f3

---
+++ src/test/scala/latis/ops/TestReplaceMissing.scala
@@ -0,0 +1,70 @@
+package latis.ops
+
+import latis.dm._
+import latis.dm.implicits._
+import latis.writer._
+import org.junit._
+import Assert._
+import latis.reader.tsml.TsmlReader
+import latis.metadata.Metadata
+import latis.data.SampledData
+
+class TestReplaceMissing {
+
+  @Test
+  def replace_with_number = {
+    val ds1 = TestDataset.function_of_scalar_with_nan
+    val ds2 = ReplaceMissingOperation(-999)(ds1)
+    latis.writer.AsciiWriter.write(ds2)
+    ds2 match {
+      case Dataset(Function(it)) => it.next; it.next match {
+        case Sample(_, Real(d)) => assertEquals(-999.0, d, 0.0)
+      }
+    }
+  }
+  
+  @Test
+  def replace_with_number_as_string = {
+    val ds1 = TestDataset.function_of_scalar_with_nan
+    val ds2 = ReplaceMissingOperation(List("-999"))(ds1)
+    //latis.writer.AsciiWriter.write(ds2)
+    ds2 match {
+      case Dataset(Function(it)) => it.next; it.next match {
+        case Sample(_, Real(d)) => assertEquals(-999.0, d, 0.0)
+      }
+    }
+  }
+  
+  @Test
+  def replace_missing_value_from_metadata = {
+    val domain = Real(Metadata("name" -> "foo"))
+    val range = Real(Metadata("missing_value" -> "0"))
+    val data = SampledData.fromValues(List(0,1,2), List(3,0,5))
+    val ds = Dataset(Function(domain, range, data = data))
+    val ds2 = ReplaceMissingOperation(-999)(ds)
+    ds2 match {
+      case Dataset(Function(it)) => it.next; it.next match {
+        case Sample(_, Real(d)) => assertEquals(-999.0, d, 0.0)
+      }
+    }
+  }
+  
+  @Test
+  def replace_integer_missing_value_from_metadata = {
+    val domain = Real(Metadata("name" -> "foo"))
+    val range = Integer(Metadata("missing_value" -> "0"))
+    val data = SampledData.fromValues(List(0,1,2), List(3,0,5))
+    val ds = Dataset(Function(domain, range, data = data))
+    val ds2 = ReplaceMissingOperation(-999)(ds)
+    ds2 match {
+      case Dataset(Function(it)) => it.next; it.next match {
+        case Sample(_, Integer(d)) => assertEquals(-999, d)
+      }
+    }
+  }
+
+}
+
+
+
+
