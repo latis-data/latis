a9bbc48968384e3c44a4db7baaf413fa8f5d163b
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-03-17 16:32:42 -0600

    implement "last" using LastFilter and interpret "project(expression)" so we can use csvs
    Former-commit-id: dadb513ab2a3786968affcbd3fe8cfe14f480570

--- src/main/scala/latis/dm/Dataset.scala
+++ src/main/scala/latis/dm/Dataset.scala
@@ -5,6 +5,7 @@ import latis.data.EmptyData
 import latis.metadata.Metadata
 import latis.metadata.EmptyMetadata
 import latis.ops.Projection
+import latis.ops.filter.LastFilter
 import latis.ops.filter.Selection
 import latis.ops.math.BasicMath
 import latis.util.DataMap
@@ -57,7 +58,7 @@ class Dataset(variable: Variable, metadata: Metadata = EmptyMetadata) extends Ba
   
   def project(proj: Projection): Dataset = proj(this)
   def project(varNames: Seq[String]): Dataset = Projection(varNames)(this)
-  def project(vname: String): Dataset = Projection(Seq(vname))(this)
+  def project(expression: String): Dataset = Projection(expression)(this)
   
   def select(expression: String): Dataset = Selection(expression)(this)
   
@@ -100,12 +101,7 @@ class Dataset(variable: Variable, metadata: Metadata = EmptyMetadata) extends Ba
     }
   }
   
-  def last: Dataset = variable match {
-    //TODO: use Last filter
-    case Function(samples) => {
-      Dataset(Function(List(samples.toSeq.last)))
-    }
-  }
+  def last: Dataset = LastFilter()(this)
     
   /**
    * Realize the Data for this Dataset so we can close the Reader.
