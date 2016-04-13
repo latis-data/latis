add3e863a4e3719bd43027ea0aca3fe379b04705
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-04-13 17:06:38 -0600

    add tests related to middle names
    Former-commit-id: 980f39c0014e4846fcbbd7e7b46020192e5dae9d

--- src/test/scala/latis/dm/TestVariable.scala
+++ src/test/scala/latis/dm/TestVariable.scala
@@ -38,11 +38,12 @@ class TestVariable {
   
   @Test 
   def findAllVariables {
+    //foo: (bar: (a), bar: (b))
     val v = Tuple(Seq(
         Tuple(Real(Metadata("a")), Metadata("bar")), 
         Tuple(Real(Metadata("b")), Metadata("bar"))
         ), Metadata("foo"))
-    
+        
     v.findAllVariablesByName("foo.bar") match {
       case Seq(v1, v2) => 
       case _ => fail
@@ -53,4 +54,33 @@ class TestVariable {
     }
   }
   
+  @Test
+  def find_all_variables_by_middle_name = {
+    //foo: (bar: (a, b))
+    val v = Tuple(Seq(
+        Tuple(Seq(Real(Metadata("a")), Real(Metadata("b"))), Metadata("bar"))
+        ), Metadata("foo"))
+    
+    v.findAllVariablesByName("bar") match {
+      case Seq(Tuple(vars)) => assertEquals(2, vars.length)
+      case _ => fail
+    }
+    v.findAllVariablesByName("bar.b") match {
+      case Seq(v1: Real) => assertEquals("b", v1.getName)
+      case _ => fail
+    }
+  }
+  
+  @Test
+  def find_variable_without_middle_name = {
+    //foo: (bar: (a, b))
+    val v = Tuple(Seq(
+        Tuple(Seq(Real(Metadata("a")), Real(Metadata("b"))), Metadata("bar"))
+        ), Metadata("foo"))
+        
+    v.findVariableByName("foo.a") match {
+      case None => fail
+      case Some(v1: Variable) => assertEquals("a", v1.getName)
+    }
+  }
 }
