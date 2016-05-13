eedce8e4ce0e0e22ab7ebfc7ada9f6389631d634
Doug Lindholm <doug.lindholm@gmail.com>
2016-05-13 09:10:02 -0600

    notes
    Former-commit-id: 546098d32f75fabe2c75d7412ff52c637723c34e

--- src/main/scala/latis/ops/Evaluation.scala
+++ src/main/scala/latis/ops/Evaluation.scala
@@ -10,7 +10,20 @@ class Evaluation extends Operation {
    * function.apply(arg, strategy)
    *   strategy optional, otherwise use the one tied to the function?
    *   but can only apply operation to dataset
-   *   
+   * 
+   * should the resampling/interpolation strategy be a property/metadata of the Dataset?
+   * 
+   * think of function evaluation like Map access by key with interpolation (and extrapolation) mixed in
+   *   f:F[D,C] 
+   *   f(d:D): C
+   * need to be able to interpolate data of type C (Interpolation[C]?)
+   *   akin to Ordering
+   *   and likewise for D?
+   *   probably just need to support interp for any Variable
+   * how should this operate of Scalars and Tuples?
+   *   should it even be an Operation?
+   *   part of Resampling or such?
+   *   do we really ever want to get just one new sample back as a dataset  
    */
   
   override def applyToFunction(function: Function): Option[Variable] = {
