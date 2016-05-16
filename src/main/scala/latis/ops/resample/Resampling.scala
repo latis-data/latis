fd29fc4f0244473517ef09afd0ddd37d3ab1a228
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-05-16 16:09:18 -0600

    clean up
    Former-commit-id: 928c72664193f754c9e36d753b326f83ea74c9db

--- src/main/scala/latis/ops/resample/Resampling.scala
+++ src/main/scala/latis/ops/resample/Resampling.scala
@@ -15,21 +15,8 @@ import latis.ops.agg.BasicJoin
 import latis.time.Time
 
 class Resampling(domainSet: Iterable[Variable]) extends Operation with NoInterpolation {
-  /*
-   * TODO: construct with DomainSet?
-   * currently DomainSet is just Data
-   * need to be able to match type, name?
-   *   convert units...
-   *   add metadata "template" to it?
-   *   def apply(index: Int): Data
-   *   def indexOf(data: Data): Int
-   * needs to be iterable
-   * 
-   *   
-   */
-  
+
   val dname = domainSet.head.getName
-   
 
   override def applyToFunction(function: Function): Option[Function] = {
     val newSamples = if(function.getDomain.hasName(dname)) { 
@@ -61,52 +48,21 @@ class Resampling(domainSet: Iterable[Variable]) extends Operation with NoInterpo
 
     Some(Function(newSamples.toSeq, function.getMetadata + ("length" -> newSamples.size.toString)))
   }
-//  
-//  /**
-//   * Split a Function with a Tuple range into a Seq of scalar Functions, one 
-//   * for each Variable in the Tuple.
-//   */
-//  def split(function: Function): Seq[Function] = function.getRange match {
-//    case Tuple(vars) => {
-//      val samples = function.iterator.toSeq
-//      val dom = samples.map(_.domain)
-//      val ran = samples.map(_.range.asInstanceOf[Tuple].getVariables).transpose
-//      ran.map(Function(dom, _))
-//    }
-//  }
-//  
-//  /**
-//   * Combine a Seq of Functions into a single Function by wrapping the ranges of
-//   * each individual Function into a single Tuple. Assumes each function has the 
-//   * same domain.
-//   */
-//  def splice(fs: Seq[Function]): Function = {
-//    val samples = fs.map(_.iterator.toSeq)
-//    val dom = samples(0).map(_.domain)
-//    val ran = samples.map(_.map(_.range)).transpose
-//    val newSamples = dom.zip(ran).map(p => Sample(p._1, Tuple(p._2)))
-//    Function(newSamples)
-//  }
-//  
+
   /**
    * Gets the interpolator for a scalar function of Numbers. Otherwise defaults
    * a NaN interpolator. 
    */
   def getInterpolator(function: Function): Double => Option[Double] = {
     val samples = function.iterator.toArray
-    samples(0) match {
+    samples.head match {
       case Sample(d: Number, r: Number) => {
         val xs = samples.map(_.domain.getNumberData.doubleValue)
-        val ys = samples.map(_.range.getNumberData.doubleValue)
-        interpolator(xs,ys)
-      }
-      case Sample(t: Time, r: Number) => {
-        val xs = samples.map(_.domain.asInstanceOf[Time].getJavaTime.doubleValue)
         val ys = samples.map(_.range.getNumberData.doubleValue)
         interpolator(xs,ys)
       }
       case _ => (d: Double) => Some(Double.NaN)
     }
   }
-
+  
 }
