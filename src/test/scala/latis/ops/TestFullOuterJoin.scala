6c9476ff01fdbd98aa2175328fb5f52a48b6a279
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-05-16 14:18:18 -0600

    more tests
    Former-commit-id: f5033461ef0a4db8844792a49b0f38e5ca73d121

--- src/test/scala/latis/ops/TestFullOuterJoin.scala
+++ src/test/scala/latis/ops/TestFullOuterJoin.scala
@@ -16,6 +16,8 @@ class TestFullOuterJoin {
   val mdt = Metadata("name" -> "t")
   val mda = Metadata("name" -> "a", "missing_value" -> "0")
   val mdb = Metadata("name" -> "b", "missing_value" -> "0")
+  val mdc = Metadata("name" -> "c", "missing_value" -> "0")
+  val mdd = Metadata("name" -> "d", "missing_value" -> "0")
   
   @Test
   def same_in_both = {
@@ -239,7 +241,53 @@ class TestFullOuterJoin {
     val ds = op(ds1, ds2)
   }
   
-  //TODO: test with tuple ranges
+  @Test
+  def tuple_codomains = {
+    val samples1 = List(2,3).map(i => Sample(Real(mdt, i), Tuple(Real(mda, i), Real(mdc, i+2))))
+    val samples2 = List(1,3).map(i => Sample(Real(mdt, i), Tuple(Real(mdb, i*2), Real(mdd, i*3))))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val op = new FullOuterJoin2()
+    val ds = op(ds1, ds2)
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b), Real(c), Real(d)))) => assertEquals((1,0,0,2,3), (t,a,b,c,d))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b), Real(c), Real(d)))) => assertEquals((1,2,4,0,0), (t,a,b,c,d))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b), Real(c), Real(d)))) => assertEquals((1,3,5,6,9), (t,a,b,c,d))}
+        assert(!it.hasNext)
+      }
+    }
+  }
   
-  //TODO: same range variable
+  @Test
+  def three_datasets = {
+    val samples1 = List(1,2).map(i => Sample(Real(mdt, i), Real(mda, i)))
+    val samples2 = List(1,3).map(i => Sample(Real(mdt, i), Real(mdb, i*2)))
+    val samples3 = List(2,3).map(i => Sample(Real(mdt, i), Real(mdc, i+2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1"))
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2"))
+    val ds3 = Dataset(Function(samples3, Metadata("function3")), Metadata("dataset3"))
+    val op = new FullOuterJoin2()
+    val ds = op(Seq(ds1, ds2, ds3))
+    ds match {
+      case Dataset(Function(it)) => {
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b), Real(c)))) => assertEquals((1,1,2,0), (t,a,b,c))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b), Real(c)))) => assertEquals((2,2,0,4), (t,a,b,c))}
+        it.next match {case Sample(Real(t), Tuple(Seq(Real(a), Real(b), Real(c)))) => assertEquals((3,0,6,5), (t,a,b,c))}
+        assert(!it.hasNext)
+      }
+    }
+  }
+  
+  @Test @Ignore //TODO: deal with duplicate names in join
+  def duplicate_codomain_variable = {
+    val samples1 = List(1,2,3).map(i => Sample(Real(mdt, i), Real(mda, i)))
+    val samples2 = List(1,2,3).map(i => Sample(Real(mdt, i), Real(mda, i*2)))
+    val ds1 = Dataset(Function(samples1, Metadata("function1")), Metadata("dataset1")) //.force
+    val ds2 = Dataset(Function(samples2, Metadata("function2")), Metadata("dataset2")) //.force
+    
+    val op = new FullOuterJoin2()
+    val ds = op(ds1, ds2)
+    latis.writer.AsciiWriter.write(ds)
+  }
 }
