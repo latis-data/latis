03e5cf69b8292d7948bc82b8e0c517b95e0e5ae4
Dylan Nguyen <Dylan.Nguyen@lasp.colorado.edu>
2016-05-17 12:56:07 -0600

    finished refactoring rounding as op and unit testing
    Former-commit-id: fcc571f32c92ef8373799cf7975b5d8e268c5afa

--- src/main/scala/latis/ops/RoundWithSigfigs.scala
+++ src/main/scala/latis/ops/RoundWithSigfigs.scala
@@ -5,12 +5,15 @@ import latis.dm._
 
 import scala.math.BigDecimal.RoundingMode
 
-class RoundWithSigfigs(name: String) extends Operation {
+class RoundWithSigfigs(name: String, digits: Option[Int] = None) extends Operation {
+  val errorMessage: String = "Sigfigs must be greater than zero"
   
   override def applyToScalar(scalar: Scalar): Option[Scalar] = {
-    val roundTo = scalar.getMetadata("sigfigs") match {
-      case None => return Some(scalar)
-      case Some(precision) => precision.toInt
+    val roundTo = (digits, scalar.getMetadata("sigfigs")) match {
+      case (Some(i), _) if i.toInt <= 0 => throw new Error(errorMessage)
+      case (Some(i), _) => i.toInt
+      case (None, Some(sigfigs)) => sigfigs.toInt
+      case (_, _) => return Some(scalar)
     }
     (scalar.hasName(name), scalar) match {
       case (true, Real(d)) => {
@@ -21,6 +24,7 @@ class RoundWithSigfigs(name: String) extends Operation {
         val s = scala.math.pow(10, roundTo - scala.math.log10(scala.math.abs(l)).ceil)
         Some(Scalar(scalar.getMetadata(), ((scala.math.rint(l * s)) / s).toLong))
       }
+      case (_, _) => Some(scalar)
     }
   }
 }
@@ -28,8 +32,8 @@ class RoundWithSigfigs(name: String) extends Operation {
 object RoundWithSigfigs extends OperationFactory {
 
   override def apply(args: Seq[String]): RoundWithSigfigs = args match {
-    case Seq(n: String) => new RoundWithSigfigs(n)
+    case Seq(n: String, d: String) => new RoundWithSigfigs(n, Some(d.toInt))
   }
   
-  def apply(n: String): RoundWithSigfigs = new RoundWithSigfigs(n)
+  def apply(n: String, d: Option[Int] = None ): RoundWithSigfigs = new RoundWithSigfigs(n, d)
 }
