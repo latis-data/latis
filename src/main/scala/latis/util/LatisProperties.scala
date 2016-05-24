571480a44f31ef3aae1886cfd52eb6d2c70bfad7
Dylan Nguyen <dyng9409@colorado.edu>
2016-05-24 14:29:58 -0600

    fixed my fix
    Former-commit-id: 7cc4d6eda35b5906f18bca65702da3790d3dfee1

--- src/main/scala/latis/util/LatisProperties.scala
+++ src/main/scala/latis/util/LatisProperties.scala
@@ -33,17 +33,10 @@ class LatisProperties extends Properties with LazyLogging {
   } catch {
     case e: Exception => {
       //logger.warn("Unable to load properties file: " + file)
-      //throw new RuntimeException("Unable to load properties file: " + file, e)
-      //I've added the below so that the scala REPL will play nice with the
-      //latis.properties. I can remove this when merging if necessary
+      ///I've commented the below so that the scala REPL will play nice with the
+      //latis.properties. I will remove this when merging with dev
       //~Dylan Nguyen 5/23
-      try {
-        val in = new FileInputStream("~/git/latis/src/main/resources/latis.properties")
-        load(in)
-        in.close
-      } catch {
-        case e: Exception => 
-      }
+      //throw new RuntimeException("Unable to load properties file: " + file, e)
     }
   }
 
