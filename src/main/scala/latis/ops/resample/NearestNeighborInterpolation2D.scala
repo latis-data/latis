12d46a868fa2389da0c8fd6493c1831cf425e0df
Dylan Nguyen <Dylan.Nguyen@lasp.colorado.edu>
2016-06-14 15:29:36 -0600

    basic implementation of 2D plot interpolation using the nearest neighbor strategy
    Former-commit-id: 91923d902688ab918a3654fcda6ab03dfd63825a

--- src/main/scala/latis/ops/resample/NearestNeighborInterpolation2D.scala
+++ src/main/scala/latis/ops/resample/NearestNeighborInterpolation2D.scala
@@ -7,7 +7,50 @@ package latis.ops.resample
 trait NearestNeighborInterpolation2D extends Interpolation {
   
   def interpolator(domain: Array[Array[Double]], range: Array[Double]) = (x: Double, y: Double) => {
+    val xs = domain(0).distinct
+    val ys = domain(1).distinct
 
+    val xcoord = xs.find( _ > x) match {
+      case Some(upperBound) => {
+        //some upper bound to the x value exists
+        //so find a lower bound
+        val i = xs.indexOf(upperBound)
+        if(i == 0) i
+        else {
+          val lowerBound = xs(i-1)
+          if((x-lowerBound) <= (upperBound-x)) (i-1)
+          else i
+        }
+      }
+      case None => {
+        //no upper bound to the x value exists
+        //use last x vlaue as lower bound
+        xs.length-1
+      }
+    }
+
+    val ycoord = ys.find( _ > y) match {
+      //this part here is heavily reliant on a cartesian grid
+      //may need to refactor in the future to be more general
+      case Some(upperBound) => {
+        //some upper bound to the y value exists
+        //find lower bound
+        val i = ys.indexOf(upperBound)
+        if(i == 0) i
+        else {
+          val lowerBound = ys(i-1)
+          if((y-lowerBound) <= (upperBound-y)) (i-1)
+          else i
+        }
+      }
+      case None => {
+        //no upper bound to the y value exists
+        //use last y value as lower bound
+        ys.length-1
+      }
+    }
+
+    //hooray index magic/math!
+    Some(range(xcoord*xs.length+ycoord))
   }
-  
 }
