02f4f765f51903a6a40812d7f0cfe5614b45c7d7
Nicandro Flores <nicandro.flores@lasp.colorado.edu>
2016-06-23 14:19:57 -0600

    fixed zipWriter to gradefully deal with no data in inputstream
    Former-commit-id: de5fc01f6007c5a8bbea1ac8da53451ef80994af

--- src/main/scala/latis/writer/ZipWriter2.scala
+++ src/main/scala/latis/writer/ZipWriter2.scala
@@ -5,16 +5,18 @@ import java.io.File
 import java.net.URL
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
-
 import latis.dm.Dataset
 import latis.dm.Function
 import latis.dm.Text
+import java.io.InputStream
+import com.typesafe.scalalogging.LazyLogging
+import java.io.IOException
 
 /**
  * Write a zip file of the URLs contained in a URL list dataset.
  * This is designed to supersede ZipWriter.
  */
-class ZipWriter2 extends Writer {
+class ZipWriter2 extends Writer with LazyLogging {
   //TODO: provide option to specify prefix for zip entries
   
   def write(dataset: Dataset) = {
@@ -39,17 +41,20 @@ class ZipWriter2 extends Writer {
     try {
       for (f <- files) {
         //add zip entry to output stream
-        zip.putNextEntry(new ZipEntry(f))
-
+        //zip.putNextEntry(new ZipEntry(f))
+        println(baseUrl)
         val url = new URL(baseUrl + f)
-        val bis = new BufferedInputStream(url.openStream())
+        var bis: InputStream = null
         try {
+          bis = new BufferedInputStream(url.openStream())
+          zip.putNextEntry(new ZipEntry(f))
           Stream.continually(bis.read).takeWhile(-1 !=).foreach(zip.write(_))
+        } catch {
+          case e: IOException => logger.warn("No data found at: " + url, e)
         }
         finally {
-          bis.close
+          if (bis != null) bis.close
         }
-
         zip.closeEntry
       }
     }
