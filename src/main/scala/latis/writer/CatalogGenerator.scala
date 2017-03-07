1279bfcc6bdf27cb157f2b4d7151311da453192d
Shawn <sapols@yahoo.com>
2017-03-07 12:02:08 -0700

    Starting to create a CatalogGenerator class.

---
+++ src/main/scala/latis/writer/CatalogGenerator.scala
@@ -0,0 +1,26 @@
+package latis.writer
+
+import latis.dm.Dataset
+import latis.dm.Binary
+import latis.dm.Dataset
+import latis.dm.Function
+import latis.dm.Index
+import latis.dm.Integer
+import latis.dm.Real
+import latis.dm.Sample
+import latis.dm.Scalar
+import latis.dm.Text
+import latis.dm.Tuple
+import latis.dm.Variable
+import java.io.PrintWriter
+import latis.dm.TupleMatch
+import scala.annotation.meta.companionObject
+
+class CatalogGenerator extends Writer {
+  
+  def write(dataset: Dataset) {
+    ???
+  }
+  
+
+}
