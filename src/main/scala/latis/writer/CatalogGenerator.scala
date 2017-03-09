12d563b21c56f703f472b024b40faf35495dd51f
shpo9723 <shpo9723@lasp.colorado.edu>
2017-03-09 12:04:48 -0700

    CatalogGenerator can now get a list of datasets for a given mission.

--- src/main/scala/latis/writer/CatalogGenerator.scala
+++ src/main/scala/latis/writer/CatalogGenerator.scala
@@ -13,14 +13,43 @@ import latis.dm.Text
 import latis.dm.Tuple
 import latis.dm.Variable
 import java.io.PrintWriter
+import java.io._
 import latis.dm.TupleMatch
 import scala.annotation.meta.companionObject
+import latis.util.LatisProperties
+import java.net.URL
+import java.io.File
+import scala.collection.mutable.ArrayBuffer
 
 class CatalogGenerator extends Writer {
+  
+  private lazy val datasetNames = getListOfDatasets  
   
   def write(dataset: Dataset) {
     ???
   }
   
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+  
+  def getListOfDatasets: List[String] = {
+    val dsdir: String = LatisProperties.getOrElse("dataset.dir", "datasets")
+    val fileList = getListOfFiles(dsdir) 
+    val fileNames = ArrayBuffer[String]()
+    
+    for (x <- fileList) {
+      fileNames += x.toString.substring(dsdir.length()+1)
+    }
+    
+    fileNames.toList
+  }
+  
+  def getDatasetNames = datasetNames
 
 }
