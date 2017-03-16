fb9379a0c6c9658408f295501998d31b8bd55192
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2017-03-16 17:34:19 -0600

    implement NoExtrapolation by using fill value

--- src/main/scala/latis/ops/resample/NoExtrapolation.scala
+++ src/main/scala/latis/ops/resample/NoExtrapolation.scala
@@ -1,9 +1,23 @@
 package latis.ops.resample
 
+import latis.dm.Sample
+import latis.dm.Scalar
+import latis.data.Data
+
 trait NoExtrapolation extends Extrapolation {
   
   def extrapolator(xs: Array[Double], ys: Array[Double]): Double => Option[Double] = {
     (d: Double) => None
   }
   
+  //TODO: generalize use of extrapolator function (see Interpolation)
+  override def extrapolate(samples: Array[Sample], domain: Scalar): Option[Sample] = {
+    //copy range scalar with fill value
+    val range = samples.head.range match {
+      case s: Scalar => s(Data(s.getFillValue))
+      case _ => throw new UnsupportedOperationException("Extrapolation requires a Scalar domain for now.")
+    }
+    
+    Some(Sample(domain, range))
+  }
 }
