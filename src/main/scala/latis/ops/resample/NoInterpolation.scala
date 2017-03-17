a1abd2d8cc4c6d47be7d5ddde22af45a3e8a0d63
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2017-03-17 12:50:56 -0600

    override interpolate so we can join tuple ranges including Text

--- src/main/scala/latis/ops/resample/NoInterpolation.scala
+++ src/main/scala/latis/ops/resample/NoInterpolation.scala
@@ -1,5 +1,11 @@
 package latis.ops.resample
 
+import latis.dm.Sample
+import latis.dm.Scalar
+import latis.dm.Number
+import latis.dm.Tuple
+import latis.data.Data
+
 trait NoInterpolation extends Interpolation { 
   
   def interpolator(xs: Array[Double], ys: Array[Double]): Double => Option[Double] = {
@@ -10,4 +16,41 @@ trait NoInterpolation extends Interpolation {
     }
   }
   
+  /**
+   * Since there is no interpolation, we can support any type of Variable.
+   */
+  override def interpolate(samples: Array[Sample], domain: Scalar): Option[Sample] = {
+    //TODO: assumes scalar or flat tuple for range for the purpose of getting fill values
+    
+    //Get domain values from the samples
+    val xs: Array[Double] = samples.map(s => s match {
+      case Sample(Number(x), _) => x
+    })
+    
+    //interpolate value
+    val x: Double = domain match {case Number(x) => x}
+    
+    val range = xs.indexOf(x) match {
+      case -1 => {
+        //need fill values
+        //use first sample as a template
+        val temps = samples.head.range match {
+          case s: Scalar => Seq(s)
+          case Tuple(vars) => vars
+        }
+        val filledVars = temps.map(v => v match {
+          case s: Scalar => s(Data(s.getFillValue))
+          case _ => throw new UnsupportedOperationException("NoInterpolation supports only scalars and flat tuples of scalars in the range, for now.")
+        })
+    
+        filledVars.length match {
+          case 1 => filledVars.head
+          case _ => Tuple(filledVars)
+        }
+      }
+      case index: Int => samples(index)
+    }
+    
+    Some(Sample(domain, range))
+  }
 }
