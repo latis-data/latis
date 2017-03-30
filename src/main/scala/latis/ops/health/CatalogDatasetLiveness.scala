9e94163ad2dc07d40e842d031c1ebeeb7f262462
Shawn <sapols@yahoo.com>
2017-03-30 11:40:06 -0600

    Method "urlIsAlive" is now "dsIsAlive". (Check through code not service)

--- src/main/scala/latis/ops/health/CatalogDatasetLiveness.scala
+++ src/main/scala/latis/ops/health/CatalogDatasetLiveness.scala
@@ -1,21 +1,19 @@
 package latis.ops.health
 
-import com.typesafe.scalalogging.LazyLogging
-import latis.ops.Operation
-import latis.metadata.Metadata
-import latis.dm._
-import latis.reader.CatalogReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.InputStream
-import latis.ops.OperationFactory
 import scala.collection.mutable.ArrayBuffer
+import latis.dm.Dataset
+import latis.dm.Function
+import latis.dm.Sample
+import latis.dm.Text
+import latis.metadata.Metadata
+import latis.ops.Operation
+import latis.ops.OperationFactory
+import latis.ops.filter.FirstFilter
+import latis.reader.DatasetAccessor
+import com.typesafe.scalalogging.LazyLogging
 
-/* TODO: Finalize this description/name of the class itself
- * 
- * Given a catalog dataset ("following the DCAT ontology"?), describe 
+/* 
+ * Given a catalog dataset following the DCAT ontology, describe 
  * whether the datasets in the catalog are "alive" or not.
  * This means checking to see if a dataset can return a first record. 
  */
@@ -25,7 +23,7 @@ class CatalogDatasetLiveness extends Operation with LazyLogging {
    * Apply this Operation to the given Dataset.
    * (Overriden to change the dataset's name)
    */
-  override def apply(dataset: Dataset): Dataset = dataset match { //TODO: Is there an easier way to change ds's name?
+  override def apply(dataset: Dataset): Dataset = dataset match { 
     case Dataset(variable) => {
       val md = dataset.getMetadata
       applyToVariable(variable) match {
@@ -48,7 +46,7 @@ class CatalogDatasetLiveness extends Operation with LazyLogging {
         it.foreach { s => s match {
           case Sample(Text(name), _) => 
             samples += Sample(Text(Metadata("ds_name"), name), 
-                         Text(Metadata("alive"), urlIsAlive(name + ".txt?&first()").toString)) 
+                         Text(Metadata("alive"), dsIsAlive(name).toString)) 
                          
           case _ => samples += Sample(Text(Metadata("ds_name"), "[INVALID RECORD]"), 
                                  Text(Metadata("alive"), "N/A"))              
@@ -61,42 +59,23 @@ class CatalogDatasetLiveness extends Operation with LazyLogging {
   
   /*
    * Given a dataset name, return true if that dataset serves a first record.
-   * Else, false, and log the error.  
+   * Else, false.  
    */
-  def urlIsAlive(name: String): Boolean = {
-    val baseUrl = "http://lasp.colorado.edu/lisird3/latis/" //TODO: generalize this 
-    
-    val url = new URL(baseUrl + name)
-    val con = url.openConnection.asInstanceOf[HttpURLConnection]
-    logger.info("Testing url: " + url.toString)
-    val is = {
-      try {
-        con.getInputStream
-      }
-      catch {
-        case e: Exception => con.getErrorStream
-      }
-    }
-    
-    var br: BufferedReader = null
-    
+  def dsIsAlive(name: String): Boolean = {
     try {
-      br = new BufferedReader(new InputStreamReader(is))
-      
-      br.readLine match {
-        case "" => logger.info("Empty data found for " + url); false
-        case "LaTiS Error: {" => logger.info("Dataset access failed for " + url + " with LaTiS Error: " + br.readLine); false
-        case _ => logger.info(url.toString + " passed"); true
+      val ds = DatasetAccessor.fromName(name).getDataset(Seq(FirstFilter()))
+      ds match {
+        case Dataset(Function(it)) if (!it.isEmpty) => it.next match {
+          case s: Sample => true
+          case _ => false
+        }
+        case _ => false
       }
-      
-    }
-    catch {
-      case e: java.lang.Exception => logger.info("Dataset access failed for " + url + ". Unable to read from LaTiS: " + e.getMessage); false
     } 
-    finally {
-      try { is.close } catch {case _: Exception => }
-      try { br.close } catch {case _: Exception => }
-    }
+    catch {
+      case e: Throwable => 
+        logger.info("Dataset access failed for " + name + ". Unable to read from LaTiS: " + e.getMessage); false
+    } 
   }
   
 }
