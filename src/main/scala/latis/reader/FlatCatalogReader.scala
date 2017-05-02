37c71123e591de66360a2b6106e04fea0e7abff6
Shawn <sapols@yahoo.com>
2017-05-02 14:00:04 -0600

    Allow FlatCatalogReader to getDataset with operations.

--- src/main/scala/latis/reader/FlatCatalogReader.scala
+++ src/main/scala/latis/reader/FlatCatalogReader.scala
@@ -38,8 +38,6 @@ class FlatCatalogReader extends DatasetAccessor {
   lazy val template = Function(Text(Metadata("name")), Tuple(Text(Metadata("description")),
       Tuple(Text(Metadata("accessURL")), Metadata("distribution"))))
   
-  def getDataset(operations: Seq[Operation]) = getDataset
-  
   override def getDataset = {
     val files = FileUtilsNio.listAllFilesWithSize(dir).map(_.takeWhile(_ != ','))
     val names = files.filter(_.endsWith(".tsml")).map(_.stripSuffix(".tsml"))
@@ -50,6 +48,19 @@ class FlatCatalogReader extends DatasetAccessor {
                                                      "description" -> StringValue("Hello world!")))
     val f = DataMapUtils.dataMapsToFunction(dataMap.iterator, template)
     Dataset(f, Metadata("catalog"))
+  }
+  
+  def getDataset(operations: Seq[Operation]): Dataset = {
+    val files = FileUtilsNio.listAllFilesWithSize(dir).map(_.takeWhile(_ != ','))
+    val names = files.filter(_.endsWith(".tsml")).map(_.stripSuffix(".tsml"))
+    val accessUrls = names
+
+    val dataMap = names.zip(accessUrls).map(p => Map("name" -> StringValue(p._1), 
+                                                     "accessURL" -> StringValue(p._2),
+                                                     "description" -> StringValue("")))
+    val f = DataMapUtils.dataMapsToFunction(dataMap.iterator, template)
+    val dataset = Dataset(f, Metadata("catalog"))
+    operations.foldLeft(dataset)((ds,op) => op(ds))
   }
   
   def close = {}
