00918a67c80a2b4d2ab600f33723329e7df9581a
Shawn <sapols@yahoo.com>
2018-01-28 14:00:30 -0700

    Manually pull the version from the main latis.properties (never test).

--- src/test/scala/latis/TestVersion.scala
+++ src/test/scala/latis/TestVersion.scala
@@ -11,7 +11,7 @@ class TestVersion {
   @Test
   def checkForVersionMatch = {
     val sbtVersion = getSbtVersionWithoutSNAPSHOT
-    LatisProperties.get("version") match {
+    getMainPropertiesVersion match {
       case Some(propertiesVersion) => assertEquals(sbtVersion, propertiesVersion)
       case _ => fail("The 'version' property could not be found.")
     }
@@ -19,10 +19,25 @@ class TestVersion {
   
   def getSbtVersionWithoutSNAPSHOT: String = {
     val versionFile: String = new String(Files.readAllBytes(Paths.get("./version.sbt")))
-    val version = versionFile.split(" := ")(1).replaceAll("\"", "").trim
+    val version = versionFile.split(":=")(1).replaceAll("\"", "").trim
     val versionNum = version.split("-")(0)
     versionNum
   }
   
+  /*
+   * Used to pull the "version" property from the main latis.properties (not test).
+   */
+  def getMainPropertiesVersion: Option[String] = {
+    val propertiesFile: String = new String(Files.readAllBytes(Paths.get("./src/main/resources/latis.properties")))
+    propertiesFile contains "version" match {
+      case true => {
+        val versionIndex = propertiesFile.indexOf("version")
+        val version = propertiesFile.substring(versionIndex, propertiesFile.indexOf("\n", versionIndex))
+        val versionNum = version.split("=")(1).trim
+        Some(versionNum)
+      }
+      case false => None
+    }
+  }
   
 }
