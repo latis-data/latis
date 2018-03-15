1d63c7952f895f3f60fc13abd9a1847e311f6433
Peter Pfister <Peter.Pfister@lasp.colorado.edu>
2018-03-15 16:34:44 -0600

    Create new class to query system memory (LATIS-668)

---
+++ src/main/scala/latis/reader/SystemReader.scala
@@ -0,0 +1,46 @@
+package latis.reader
+
+import latis.dm.Dataset
+import latis.dm._
+import latis.metadata.Metadata
+import latis.ops.Operation
+import latis.dm.Variable
+
+/**
+   * Construct a Dataset by querying the JVM.
+   *   (free, used, total, percentUsed, max)
+   * Where
+   *   free:				 is the current amount of memory in Mb available
+   *   used:        is the amount of memory in Mb computed from total - free.
+   *   total:       the current memory in Mb allocated on the heap
+   *   percentUsed: the ratio of used / total in percent
+   *   max:         the maximum memory in Mb availble to the current JVM (-Xmx config setting)
+   */
+class SystemReader private() extends DatasetAccessor { 
+  
+  private val Mb = 1024 * 1024
+  private val metaData =  Metadata("memoryProperties")
+  private lazy val jvm = Runtime.getRuntime
+  
+  override def getDataset(): Dataset = Dataset(Tuple(getFree, getUsed, getTotal, getPercentUsed, getMax), metaData)
+  
+  def getDataset(operations: Seq[Operation]): Dataset = getDataset
+  
+  def close: Unit = {}
+  
+  def getFree: Integer = Integer(jvm.freeMemory / Mb)
+  
+  def getUsed: Integer = Integer((jvm.totalMemory() - jvm.freeMemory) / Mb)
+  
+  def getTotal: Integer = Integer(jvm.totalMemory() / Mb)
+  
+  def getPercentUsed: Integer = Integer(100 * (jvm.totalMemory() - jvm.freeMemory) / jvm.totalMemory())
+  
+  def getMax: Integer = Integer(jvm.maxMemory() / Mb)
+  
+}
+
+object SystemReader {
+  def apply(): SystemReader = new SystemReader
+}
+
