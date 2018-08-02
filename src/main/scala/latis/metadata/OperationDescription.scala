40efa3c401269c8caeb4e44aca601e0039f592b9
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2018-08-02 09:57:19 -0600

    Put parameters on multiple lines

--- src/main/scala/latis/metadata/OperationDescription.scala
+++ src/main/scala/latis/metadata/OperationDescription.scala
@@ -1,7 +1,16 @@
 package latis.metadata
 
-class OperationDescription(val name: String, val className: String, val description: String, val usage: String) { }
+class OperationDescription(
+    val name: String, 
+    val className: String, 
+    val description: String, 
+    val usage: String) { }
 
 object OperationDescription {
-  def apply(name: String, className: String, description: String, usage: String): OperationDescription = new OperationDescription(name, className, description, usage)
+  def apply(
+      name: String, 
+      className: String, 
+      description: String, 
+      usage: String): OperationDescription = 
+        new OperationDescription(name, className, description, usage)
 }
