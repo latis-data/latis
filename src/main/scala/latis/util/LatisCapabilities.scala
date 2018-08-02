b0c1c5cd6e34c20e4b3815d5b7ef097a4105c4e7
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2018-08-02 17:22:45 -0600

    Start to get func of id -> dataset_name from catalog (doesn't work yet)

--- src/main/scala/latis/util/LatisCapabilities.scala
+++ src/main/scala/latis/util/LatisCapabilities.scala
@@ -4,11 +4,14 @@ import latis.dm.Dataset
 import latis.dm.Function
 import latis.dm.Text
 import latis.dm.Tuple
+import latis.dm._
 import latis.metadata.Metadata
 import latis.metadata.ServerMetadata
 import latis.ops.Operation
+import latis.ops.Projection
 import latis.reader.DatasetAccessor
 import latis.util.FileUtils.getListOfFiles
+//TODO: organize imports
 
 /**
  * Construct a Dataset of LaTiS capabilities:
@@ -29,35 +32,38 @@ class LatisCapabilities extends DatasetAccessor {
    */
   def getCapabilities: Dataset = {
     val datasets: Function = {
-      val dir = LatisProperties.getOrElse("dataset.dir", "src/main/resources/datasets") //TODO: make sure this works in dev and prod
-      Function(
-        getListOfFiles(dir).map { f => 
-          val n = f.getName
-          val name = n.substring(0, n.lastIndexOf('.')) //drop the file extension
+      val catalogDs = DatasetAccessor.fromName("catalog").getDataset() //TODO: this doesn't seem to actually get the catalog dataset
+    
+      catalogDs match {
+        case Dataset(Function(it)) => {
           val md: Metadata = Metadata().addName("dataset_name") 
-          Text(md, name)
+          Function(
+            it.toSeq.map { 
+              case Sample(name, _) => Text(md, name) //TODO: make sure this works...
+            }
+          )
         }
-      )
+      }
     }
     
     val outputOptions: Function = {
       val md1: Metadata = Metadata().addName("output_option")
-      val md2: Metadata = Metadata().addName("output_description") //TODO: is there a cleaner way to name these scalars than using "md1", "md2"?
+      val md2: Metadata = Metadata().addName("output_description") 
       Function(
         ServerMetadata.availableSuffixes.
         sortBy(suffixInfo => suffixInfo.suffix). //put in alphabetical order
-        map(suffixInfo => (Tuple(Text(md1, suffixInfo.suffix), Text(md2, suffixInfo.description)))) //TODO: breaks if a value is null; I changed ServerMetadata to "getOrElse" "" instead of null. Was that okay?
+        map(suffixInfo => (Tuple(Text(md1, suffixInfo.suffix), Text(md2, suffixInfo.description)))) 
       )
     }
     
     val filterOptions: Function = { 
       val md1: Metadata = Metadata().addName("operation_option")
       val md2: Metadata = Metadata().addName("operation_description")
-      val md3: Metadata = Metadata().addName("operation_usage") //TODO: is there a cleaner way to name these scalars than using "md1", "md2", "md3"?
+      val md3: Metadata = Metadata().addName("operation_usage") 
       Function(  
         ServerMetadata.availableOperations.
         sortBy(opInfo => opInfo.name). //put in alphabetical order
-        map(opInfo => (Tuple(Text(md1, opInfo.name), Text(md2, opInfo.description), Text(md3, opInfo.usage)))) //TODO: breaks if a value is null; I changed ServerMetadata to "getOrElse" "" instead of null. Was that okay?
+        map(opInfo => (Tuple(Text(md1, opInfo.name), Text(md2, opInfo.description), Text(md3, opInfo.usage))))
       )
     } 
     
