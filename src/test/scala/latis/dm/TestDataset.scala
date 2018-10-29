efc5f3d8b0e2ff4c9cfcd30f78230e3b0ac2c83a
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2018-10-29 09:37:32 -0600

    Clean up imports

--- src/test/scala/latis/dm/TestDataset.scala
+++ src/test/scala/latis/dm/TestDataset.scala
@@ -1,17 +1,14 @@
 package latis.dm
 
-import latis.dm.implicits._
-import org.junit._
-import Assert._
-import com.typesafe.scalalogging.LazyLogging
-import latis.dm._
-import latis.metadata.Metadata
-import latis.time.Time
-import latis.writer.AsciiWriter
-import java.nio.ByteBuffer
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Test
+
 import latis.data.value.DoubleValue
+import latis.dm.implicits.doubleToDataset
+import latis.metadata.Metadata
 import latis.reader.tsml.TsmlReader
-import sun.awt.resources.awt
+import latis.time.Time
 
 class TestDataset {
 
