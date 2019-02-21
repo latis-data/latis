7861b8d7f123579ed0f834afb3eff49bfa103d61
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2019-02-21 12:42:27 -0700

    Add support for reordering capture groups in RegexAdapter
    This was something that was a potential solution for modeling a
    particular dataset that we ended up not using. However, it sounds like
    a good idea in general, so here it is.
    
    This works pretty much the same way that ColumnarAdapter works except
    that the 'columns' attribute is optional.

--- src/test/scala/latis/reader/TestRegexAdapter.scala
+++ src/test/scala/latis/reader/TestRegexAdapter.scala
@@ -5,6 +5,7 @@ import Assert._
 import latis.reader.tsml.TsmlReader
 import latis.writer.Writer
 import latis.ops._
+import latis.dm._
 
 class TestRegexAdapter extends AdapterTests {
 
@@ -12,4 +13,16 @@ class TestRegexAdapter extends AdapterTests {
   
   //@Test
   def test = writeDataset
+
+  @Test
+  def cols(): Unit =
+    DatasetAccessor.fromName("regex_col").getDataset() match {
+      case Dataset(Function(it)) => it.next match {
+        case Sample(Text(t), TupleMatch(Text(c), Real(b), Integer(a))) =>
+          assertEquals("1970/01/01", t)
+          assertEquals("A", c)
+          assertEquals(1.1, b, 0)
+          assertEquals(1, a)
+      }
+    }
 }
