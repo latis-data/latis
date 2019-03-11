d757a27257837e8644660a06f7f8afe6cb1815ae
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-03-11 16:03:27 -0600

    Enable ZipWriter2 to handle either http or file URLs

--- src/main/scala/latis/writer/ZipWriter2.scala
+++ src/main/scala/latis/writer/ZipWriter2.scala
@@ -15,18 +15,30 @@ import com.typesafe.scalalogging.LazyLogging
 import latis.dm.Dataset
 
 /**
- * Write a zip file of the URLs contained in a URL list dataset.
- * This is designed to supersede ZipWriter.
+ * Write a zip file of the files contained in a granule list dataset.
+ * This depends on some naming conventions. There must be a variable
+ * named "file" in the range of the Dataset's Function. Metadata may
+ * include:
+ *   baseUrl: Base URL to be prepended to file names.
+ *   srcDir: Base directory to be prepended to file names.
+ * If neither is present then it is assumed that the "file" names are
+ * complete URLs.
  */
 class ZipWriter2 extends Writer with LazyLogging {
-  //TODO: provide option to specify prefix for zip entries
+  //TODO: provide option to specify root directory for zip entries
   
   def write(dataset: Dataset): Unit = {
     
     lazy val baseUrl = dataset.getMetadata.get("baseUrl") match {
       case Some(b) => b + File.separator
-      case None => "" //URLs are already complete
+      case None => dataset.getMetadata.get("srcDir") match {
+        case Some(dir) => "file://" + dir + File.separator
+        case None => "" //URLs are already complete
+      }
     }
+    
+    //Hack to keep full URL out of zip file. Assumes no baseUrl.
+    val removePrefix = dataset.getMetadata.getOrElse("removePrefix", "")
     
     //Get the name of each file as it appears in the file list.
     //Throw an exception if a 'file' Variable cannot be found in the Dataset.
@@ -37,14 +49,12 @@ class ZipWriter2 extends Writer with LazyLogging {
     val zip = new ZipOutputStream(getOutputStream)
     try {
       for (f <- files) {
-        //add zip entry to output stream
-        //zip.putNextEntry(new ZipEntry(f))
-        println(baseUrl)
+        val zipEntry = dataset.getName + File.separator + f.replaceFirst(removePrefix, "")
         val url = new URL(baseUrl + f)
         var bis: InputStream = null
         try {
           bis = new BufferedInputStream(url.openStream())
-          zip.putNextEntry(new ZipEntry(f))
+          zip.putNextEntry(new ZipEntry(zipEntry))
           Stream.continually(bis.read).takeWhile(-1 !=).foreach(zip.write(_))
         } catch {
           case e: IOException => logger.warn("No data found at: " + url, e)
