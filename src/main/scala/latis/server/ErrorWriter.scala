21bb4dd2ff920cdcc973c54ccd7691fa65434c33
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2019-07-31 12:39:51 -0600

    Use 400 status code for UnsupportedOperationExceptions

--- src/main/scala/latis/server/ErrorWriter.scala
+++ src/main/scala/latis/server/ErrorWriter.scala
@@ -12,7 +12,7 @@ class ErrorWriter(response: HttpServletResponse) {
     //case httpe: HTTPException => response.sendError(httpe.getStatusCode, httpe.getMessage)
     
     case uoe: UnsupportedOperationException => {
-      writeWithStatusCode(uoe, HttpServletResponse.SC_INTERNAL_SERVER_ERROR) //500 (will change to 400 in LATIS-802)
+      writeWithStatusCode(uoe, HttpServletResponse.SC_BAD_REQUEST) //400
     }
     case _ => {
       writeWithStatusCode(e, HttpServletResponse.SC_INTERNAL_SERVER_ERROR) //500
