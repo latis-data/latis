4c56a3424002caa35c1d2b55efa7f4e1c979165d
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2019-08-02 14:10:16 -0600

    400 when an invalid variable name is given to the rename operation

--- src/main/scala/latis/ops/RenameOperation.scala
+++ src/main/scala/latis/ops/RenameOperation.scala
@@ -7,6 +7,7 @@ import latis.dm.Scalar
 import latis.dm.Tuple
 import latis.dm.Function
 import latis.dm.Sample
+import latis.util.LatisServerException
 
 class RenameOperation(val origName: String, val newName: String) extends Operation {
   //TODO: this could use some clean up
@@ -25,7 +26,7 @@ class RenameOperation(val origName: String, val newName: String) extends Operati
       val v = dataset match {
         case Dataset(v) => applyToVariable(v) match {
           case Some(v) => v
-          case None => throw new UnsupportedOperationException("No variable found with name: " + origName)
+          case None => throw new LatisServerException("No variable found with name: " + origName)
           //TODO: error or no-op?
         }
         case _ => null
