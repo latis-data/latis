c1fb83b6ad5386b8a3b1349a37bb3f663f9c7cb4
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2019-08-07 15:32:24 -0600

    Enclose Scalar ranges (with index domain) in makeSample() with curlies{}
    This fixes a bug that appeared in SWP-375 where the Capabilities dataset
    was not properly formatted JSON.

--- src/test/scala/latis/writer/TestJsonWriter.scala
+++ src/test/scala/latis/writer/TestJsonWriter.scala
@@ -7,6 +7,7 @@ import latis.metadata.Metadata
 import latis.data.SampledData
 import java.io.ByteArrayOutputStream
 import latis.reader.DatasetAccessor
+import latis.util.LatisCapabilities
 
 class TestJsonWriter extends WriterTest {
 
@@ -127,4 +128,18 @@ class TestJsonWriter extends WriterTest {
     writer.write(ds)
     assertTrue(baos.toString.startsWith("{"))
   }
+  
+  @Test // SWP-375 bug fix
+  def json2_capabilities = {
+    val ds = LatisCapabilities().getDataset 
+    val writer = new JsonWriter2
+    var baos = new ByteArrayOutputStream()
+    writer.setOutputStream(baos)
+    writer.write(ds)
+    val rows = baos.toString.split("\n")
+    assertEquals(rows(0), "{\"datasets\": [{\"dataset_name\": \"agg\"},")
+    assertNotEquals(-1, rows.indexOf("{\"output_option\": \"bin\", \"output_description\": \"Binary stream in network byte order (big-endian)\"},"))
+    assertNotEquals(-1, rows.indexOf("{\"operation_option\": \"contains\", \"operation_description\": \"Return samples where the given variable has one of the given values\", \"operation_usage\": \"contains(name, v1, v2, ...)\"},"))
+  }
+  
 }
