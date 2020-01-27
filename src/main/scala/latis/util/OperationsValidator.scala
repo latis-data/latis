1f0f78b7eeb684d5b56e0c56e15a8979b07e34be
Shawn Polson <sapols@yahoo.com>
2020-01-27 12:04:46 -0700

    Add crude support for First and Last filter short circuiting

--- src/main/scala/latis/util/OperationsValidator.scala
+++ src/main/scala/latis/util/OperationsValidator.scala
@@ -2,6 +2,8 @@ package latis.util
 
 import latis.ops.Operation
 import latis.ops.filter.Selection
+import latis.ops.filter.FirstFilter
+import latis.ops.filter.LastFilter
 import java.util.Date
 import latis.time.Time
 
@@ -16,15 +18,19 @@ object OperationsValidator {
     
     var min: Option[Long] = None
     var max: Long = new Date().getTime  //default to now
+    var hasFirst: Boolean = false
+    var hasLast: Boolean = false
     
     ops foreach {
       case Selection("time", op, time) => 
         if (op.contains(">")) min = Some(Time.isoToJava(time))
         if (op.contains("<")) max = Time.isoToJava(time)
+      case ff: FirstFilter => hasFirst = true
+      case lf: LastFilter => hasLast = true
       case _ =>
     }
     
-    min match {
+    if (!(hasFirst || hasLast)) min match {
       case None => throw new UnsupportedOperationException("This request requires a minimum time selection.")
       case Some(t0) => if ((max - t0) > maxTimeRange)
         throw new UnsupportedOperationException(s"The time selection exceeds the time range limit of ${maxTimeRange} ms.")
