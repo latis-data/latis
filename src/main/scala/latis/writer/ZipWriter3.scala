3b7ce2fda7eec5ce10817999e2aaa58e1cf3139f
sapols <sapols@yahoo.com>
2021-12-08 12:14:05 -0700

    New ZipWriter3 implementation

--- src/main/scala/latis/writer/ZipWriter3.scala
+++ src/main/scala/latis/writer/ZipWriter3.scala
@@ -14,29 +14,28 @@ import latis.time.Time
 /**
  * Proposed replacement for ZipWriter1 and ZipWriter2 (LATIS-476).
  * Use operations to transform binary, "file", or "url" list datasets into
- * "zip list" datasets (zipEntry -> binary/url) which this writer writes. 
+ * "zip list" datasets (domain -> (zipEntry, resource)) which this writer writes. 
  * Datasets will be searched for a Binary variable first, then a "url" variable, 
  * then a "file" one. The first variable found will be used. The Operations deal 
  * with the different logic for each type.
  * 
  * Duplicate zip entries will be disambiguated by appending
- * a counter with an underscore (e.g. "_1"). TODO: disambiguate with domain values.
+ * domain values with an underscore (e.g. "_2001-01-01"). 
  */
 class ZipWriter3 extends Writer with LazyLogging {
   //TODO: add manifest
-  //TODO: add disambiguating "_1" before file extension
+  //TODO: add file extension
 
   def write(dataset: Dataset): Unit = {
-    val zds: Dataset = toZipListDs(dataset)
-
+    val zds: Dataset = toZipListDs(dataset) //domain -> (zipEntry, resource)
     val zip = new ZipOutputStream(getOutputStream)
     try {
       zds match {
         case DatasetSamples(it) => it foreach {
-          case Sample(Text(zipEntry), Binary(bytes)) =>
-            zipFromBytes(zip, disambiguate(zipEntry), bytes)
-          case Sample(Text(zipEntry), Text(url)) =>
-            zipFromUrl(zip, disambiguate(zipEntry), url)
+          case Sample(d, TupleMatch(Text(zipEntry), Binary(bytes))) =>
+            zipFromBytes(zip, disambiguate(zipEntry, d), bytes)
+          case Sample(d, TupleMatch(Text(zipEntry), Text(url))) =>
+            zipFromUrl(zip, disambiguate(zipEntry, d), url)
           case _ => throw new RuntimeException("Not a valid zip dataset")
         }
         case _ => throw new RuntimeException("Empty dataset")
@@ -57,7 +56,7 @@ class ZipWriter3 extends Writer with LazyLogging {
       new UrlListToZipList()(ds)
     else if (ds.findVariableByName("file").nonEmpty)
       new FileListToZipList()(ds)
-    else throw new RuntimeException("No 'url', 'file', nor binary variable found in dataset.")
+    else throw new RuntimeException("No binary, 'url', nor 'file' variable found in dataset")
   
 
   /**
@@ -70,7 +69,7 @@ class ZipWriter3 extends Writer with LazyLogging {
     } catch {
       case e: IOException =>
         // Log a warning, leave empty entry 
-        val msg = "Failed to write bytes to the zip entry."
+        val msg = "Failed to write bytes to the zip entry"
         logger.warn(msg, e)
     }
     zip.closeEntry //finalize the zip entry
@@ -88,7 +87,6 @@ class ZipWriter3 extends Writer with LazyLogging {
         val msg = s"Failed to open the URL: $url"
         throw new RuntimeException(msg, e)
     }
-
     // Write the zip entry
     try {
       zip.putNextEntry(new ZipEntry(name))
@@ -113,31 +111,27 @@ class ZipWriter3 extends Writer with LazyLogging {
   }
 
   /**
-   * Keeps a counter so we can disambiguate duplicate zip entry names.
+   * Keeps a list of names so we can disambiguate duplicate zip entry names.
    * We do it this way so we can stream samples.
    */
-  private val counter = scala.collection.mutable.Map[String, Int]() //TODO: will need to be Map[String, String]
+  private val entries = scala.collection.mutable.MutableList[String]()
 
   /**
    * Makes sure that we don't duplicate zip entry names.
    * This will append "_d" for names that have already been used
    * where "d" is the stringified form of the given domain value.
-   * 
-   * TODO: Can't access domain variable because we get rid of it in *ListToZipList ops. 
-   *       Will have to refactor "zip list" datasets to: domain -> (zipEntry, serializable).
    */
-  private def disambiguate(name: String/*, domain: Variable*/): String = counter.get(name) match {
-    case Some(n) =>
-      counter += ((name, n + 1))
-      name + "_" + n
-    case None =>
-      counter += ((name, 1))
+  private def disambiguate(name: String, domain: Variable): String = entries.find(_ == name) match {
+    case Some(_) => 
+      name + "_" + domainToString(domain)
+    case None => 
+      entries += name
       name
   }
-
+  
   /**
    * Returns a String representation of a domain variable
-   * that is acceptable as a zip entry name.
+   * that is acceptable to disambiguate a zip entry.
    */
   private def domainToString(domain: Variable): String = domain match {
     case time: Time => time match {
