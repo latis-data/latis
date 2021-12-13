536b05db1483123bb2295786bb21584630bc4e59
sapols <sapols@yahoo.com>
2021-12-13 14:06:17 -0700

    Combine pattern match logic

--- src/main/scala/latis/ops/BinaryListToZipList.scala
+++ src/main/scala/latis/ops/BinaryListToZipList.scala
@@ -20,15 +20,12 @@ class BinaryListToZipList extends Operation {
    * Turns the Sample into a zip entry of the form: domain -> (dsName, binary),
    * if it has a Binary Variable. 
    */
-  override def applyToSample(sample: Sample): Option[Sample] = sample match {
-    case Sample(domain, _) => sample.toSeq.find {
-      case Binary(_) => true
-      case _ => false
-    } match {
-      case Some(bv) =>
-        Some(Sample(domain, Tuple(List(Text(Metadata("zipEntry"), dsName), bv))))
-      case _ => throw new UnsupportedOperationException("No Binary variable found in sample")
-    }
-    case _ => throw new UnsupportedOperationException("Invalid sample")
+  override def applyToSample(sample: Sample): Option[Sample] = sample.toSeq.find {
+    case Binary(_) => true
+    case _ => false
+  } match {
+    case Some(bv) =>
+      Some(Sample(sample.domain, Tuple(List(Text(Metadata("zipEntry"), dsName), bv))))
+    case _ => throw new UnsupportedOperationException("No Binary variable found in sample")
   }
 }
