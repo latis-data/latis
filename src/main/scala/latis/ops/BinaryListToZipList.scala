7632b28cae47256d9124d1286f189177c30d127c
sapols <sapols@yahoo.com>
2021-12-14 10:42:01 -0700

    Refactor BinaryListToZipList

--- src/main/scala/latis/ops/BinaryListToZipList.scala
+++ src/main/scala/latis/ops/BinaryListToZipList.scala
@@ -2,10 +2,11 @@ package latis.ops
 
 import latis.dm._
 import latis.metadata.Metadata
+import latis.time.Time
 
 /**
  * Convert a Dataset with a binary variable into a Dataset 
- * specialized for writing to a zip file: domain -> (dsName, binary).
+ * specialized for writing to a zip file: zipEntry -> binary.
  */
 class BinaryListToZipList extends Operation {
   
@@ -17,12 +18,31 @@ class BinaryListToZipList extends Operation {
   }
 
   /**
-   * Turns the Sample into a zip entry of the form: domain -> (dsName, binary),
+   * Turns the Sample into a zip entry of the form: zipEntry -> binary,
    * if it has a Binary Variable. 
    */
   override def applyToSample(sample: Sample): Option[Sample] = sample.toSeq.find(_.isInstanceOf[Binary]) match {
     case Some(bv) =>
-      Some(Sample(sample.domain, Tuple(List(Text(Metadata("zipEntry"), dsName), bv))))
+      Some(Sample(Text(Metadata("zipEntry"), disambiguate(dsName, sample.domain)), bv))
     case _ => throw new UnsupportedOperationException("No Binary variable found in sample")
+  }
+
+  /**
+   * Appends "_d" to zip entry names, where "d" is the stringified form of the given domain value.
+   */
+  private def disambiguate(name: String, domain: Variable): String = name + "_" + domainToString(domain)
+
+  /**
+   * Returns a String representation of a domain variable
+   * that is acceptable to disambiguate a zip entry.
+   */
+  private def domainToString(domain: Variable): String = domain match {
+    case time: Time => time match {
+      case Text(t)   => t //keep formatting
+      case _: Number => time.toIso //TODO: consider stripping special characters like '-' and ':'
+    }
+    case tup: Tuple => tup.getVariables.map(domainToString).mkString("_")
+    case d: Scalar  => d.stringValue
+    case _ => throw new UnsupportedOperationException(s"Unsupported data type for domain variable '$domain'")
   }
 }
